b218ae7fdcfa6251deb8ef2949966e9d79143a6a
jenslehmann <jenslehmann@af9c125c-f337-0410-9a96-b85bd07cbc14>
2007-08-20 15:23:17 +0000

    web service implementation added

---
+++ trunk/src/dl-learner/org/dllearner/server/LearnMonitor.java
@@ -0,0 +1,81 @@
+package org.dllearner.server;
+
+import org.dllearner.Config;
+import org.dllearner.LearningProblem;
+import org.dllearner.Main;
+import org.dllearner.algorithms.refinement.ROLearner;
+import org.dllearner.reasoning.ReasoningMethodUnsupportedException;
+
+public class LearnMonitor extends Thread {
+
+	
+	private ClientState c;
+	private boolean active = false;
+	
+	//private ROLearner ROL;
+	
+	public LearnMonitor(ClientState c){
+		this.c=c;
+	}
+	
+	
+	public  void end(){
+		
+		System.out.println("trying to end");
+		System.out.println("ROL2"+c.ROL);
+		this.c.ROL.stop();
+		c.setLastResult(c.ROL.getBestSolution().toString());
+		c.setStatus("stopped");
+	}
+	
+	public void learn(){
+		
+		this.start();
+		
+	}
+	
+	public void run(){
+		try{
+		c.setStatus("still running");
+		active=true;
+		c.setStatus("running");
+		
+		//		 notwendige Vorverarbeitungsschritte für den Lernalgorithmus
+		// - es müssen ein paar Konzepte, die ev. von Jena generiert wurden ignoriert
+		//   werden
+		// - die Subsumptionhierarchie muss erstellt werden
+		// - die Subsumptionhierarchie wird verbessert um das Lernen effizienter zu machen
+		Main.autoDetectConceptsAndRoles(c.getRs());
+		c.getReasoner().prepareSubsumptionHierarchy();
+		if (Config.Refinement.improveSubsumptionHierarchy) {
+			try {
+				c.getReasoner().getSubsumptionHierarchy().improveSubsumptionHierarchy();
+			} catch (ReasoningMethodUnsupportedException e) {
+				// solange DIG-Reasoner eingestellt ist, schlägt diese Operation nie fehl
+				e.printStackTrace();
+			}
+		}
+		c.p("learning started");
+		LearningProblem learningProblem = new LearningProblem(c.getRs(), c.getPosExamples(), c.getNegExamples());
+		// erstmal wird nur der Refinement-Learner als Web-Service angeboten
+		//System.out.println("aaaa");
+		c.ROL = new ROLearner(learningProblem);
+		
+		c.ROL.start();
+		//new ROLearner();
+		//c.p(("ROL1"+ROL));
+		
+		
+		
+		
+		 
+		
+		//c.setLastResult(c.ROL.getBestSolution().toString());
+		c.setLastResult(c.ROL.getBestSolution().toString());
+		c.setStatus("finished");
+		
+		}catch (Exception e) {e.printStackTrace();}
+		finally{active=false;};
+	}
+	
+}
