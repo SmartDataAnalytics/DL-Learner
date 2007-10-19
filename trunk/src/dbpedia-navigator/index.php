63244e1c73af2e27d26142a5e4c8f0de799e7420
sknappe <sknappe@af9c125c-f337-0410-9a96-b85bd07cbc14>
2007-10-19 09:52:23 +0000

    new version

--- trunk/src/dbpedia-navigator/index.php
+++ trunk/src/dbpedia-navigator/index.php
@@ -7,37 +7,66 @@ include_once("SparqlConnection.php");
  ini_set('error_reporting',E_ALL);
  ini_set('max_execution_time',200);
 
-$lastAction="View";
 $content="";
-$possibleActions="";
-$instances=" ";
-$left="";
-$right="";
-$middle="";
 $search="";
-$system="";
+$examples="";
+$positiveAdd="";
+$negativeAdd="";
 
 $settings=new Settings();
 
 echo "<a href='index.php?clearsession=clear'>start from scratch</a>";
-if(isset($_GET['clearsession']))$_SESSION['State_ID'] =false;
+if(isset($_GET['clearsession'])){
+	unset($_SESSION['positive']);
+	unset($_SESSION['negative']);
+}
 
+$sc=new SparqlConnection($settings->dbpediauri,$settings->wsdluri);
 
+//add positives or negatives to session
+if (@$_GET['action']=='AddPositive'){
+	$_GET=$_SESSION["lastGet"];
+	if (!isset($_SESSION['positive'])){
+		$array=array($_GET['subject']);
+		$_SESSION['positive']=$array;
+	}
+	else{
+		$array=$_SESSION['positive'];
+		$array[]=$_GET['subject'];
+		$_SESSION['positive']=$array;
+	}
+}
+if (@$_GET['action']=='AddNegative'){
+	$_GET=$_SESSION["lastGet"];
+	if (!isset($_SESSION['negative'])){
+		$array=array($_GET['subject']);
+		$_SESSION['negative']=$array;
+	}
+	else{
+		$array=$_SESSION['negative'];
+		$array[]=$_GET['subject'];
+		$_SESSION['negative']=$array;
+	}
+}
+if (@$_GET['action']=='ClearPositive'){
+	$_GET=$_SESSION["lastGet"];
+	unset($_SESSION['positive']);
+}
+if (@$_GET['action']=='ClearNegative'){
+	$_GET=$_SESSION["lastGet"];
+	unset($_SESSION['negative']);
+}
 
-if( (!isset($_SESSION['State_ID'] ) )	|| ($_SESSION['State_ID']==""))
+//learn a concept
+if (@$_GET['action']=="GetConcept")
 {
-	//get new ID
-	$sc=new SparqlConnection($settings->dbpediauri,$settings->wsdluri);
-	$_SESSION['State_ID']=$sc->getID();
-	
+	$_GET=$_SESSION["lastGet"];
+	$conc=$sc->getConceptFromExamples($_SESSION['positive'],$_SESSION['negative']);
+	$_SESSION['learnedConcept']=$conc;
 }
-	
-else{
-	//echo "Current ID is: ".$_SESSION['Learner_ID']."<br>";
-	$sc=new SparqlConnection($settings->dbpediauri,$settings->wsdluri,$_SESSION['State_ID']);
-}
-	
-	
+if (isset($_SESSION['learnedConcept'])) $learnedConcept="<br/>Last learned Concept: ".$_SESSION['learnedConcept'];
+else $learnedConcept="";
+
 //SearchBox
 	$search="<form action=\"index.php\" method=\"GET\">\n".
 			"<table border=\"0\">\n".
@@ -70,17 +99,51 @@ else{
 		foreach ($triples as $triple){
 			$content.="Subject: ".urldecode($triple[0])."<br/>Predicate: ".urldecode($triple[1])."<br/>Object: ".urldecode($triple[2])."<br/><br/>\n";
 		}
+		$positiveAdd="<input type=\"submit\" name=\"action\" value=\"AddPositive\">";
+		$negativeAdd="<input type=\"submit\" name=\"action\" value=\"AddNegative\">";
 	}
 	
+//add box for adding articles to positive or negative
+	$examples.="<form action=\"index.php\" method=\"GET\">\n".
+			   "Positive:<br/>\n";
+	if (!isset($_SESSION['positive'])) $examples.="No positive example picked yet.<br/>\n";
+	else{
+		$pos=$_SESSION['positive'];
+		foreach ($pos as $p)
+		{
+			$examples.=$p."<br/>\n";
+		}
+	}
+	$examples.="<br/>".$positiveAdd."&nbsp;<input type=\"submit\" name=\"action\" value=\"ClearPositive\">\n".
+			   "</form>\n";
+		
+	$examples.="<form action=\"index.php\" method=\"GET\">\n".
+			   "<br/>Negative:<br/>\n";
+	if (!isset($_SESSION['negative'])) $examples.="No negative example picked yet.<br/>\n";
+	else{
+		$neg=$_SESSION['negative'];
+		foreach ($neg as $n)
+		{
+			$examples.=$n."<br/>\n";
+		}
+	}
+	$examples.="<br/>".$negativeAdd."&nbsp;<input type=\"submit\" name=\"action\" value=\"ClearNegative\">\n".
+			   "</form>\n";	
+
+//fill concept box
+	$concept="<form action=\"index.php\" method=\"GET\">\n".
+			 "<input type=\"submit\" name=\"action\" value=\"GetConcept\">\n".
+			 "</form>\n";
+	$concept.=$learnedConcept;
 	
+//include master
+	$_SESSION['lastGet']=$_GET;
 	include("master.php");
 	
 	echo $masterContent;
+
 	
-	
-	
-	
-	
+	//make a box
 	function makeBox($title,$content,$toggleBoxContent=true)
 	{
 		if($toggleBoxContent)
@@ -97,14 +160,4 @@ else{
 		</div> <!-- box -->";
 		return $ret;
 	}
-
-
-	function shorten($a)
-	{
-	    if(($strpos=strpos($a,'#'))>=4){
-			return substr($a,$strpos);
-		}
-		else {return $a;}
-	}
-
 ?>
