f363ca67db06e3b70ffb26efab589ec576fa6831
Jens Lehmann <JensLehmann@users.noreply.github.com>
2008-01-09 11:25:03 +0000

    - changed calls in DBpedia navigator and DLLearner web service to new SPARQL component - basic search functionality working (learning not working yet) - code improvements

--- src/dbpedia-navigator/index.php
+++ src/dbpedia-navigator/index.php
@@ -1,12 +1,14 @@
 <?php
+
 ini_set('error_reporting',E_ALL);
 ini_set('max_execution_time',200);
 ini_set("soap.wsdl_cache_enabled","1");
+
 session_start();
 require_once('Settings.php');
-require_once('SparqlConnection.php');
+require_once('DLLearnerConnection.php');
 $settings=new Settings();
-$sc=new SparqlConnection($settings->dbpediauri,$settings->wsdluri);
+$sc=new DLLearnerConnection($settings->dbpediauri,$settings->wsdluri);
 $ids=$sc->getIDs();
 $_SESSION['id']=$ids[0];
 $_SESSION['ksID']=$ids[1];
@@ -20,7 +22,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <title>DL Learner</title>
+    <title>DBpedia Navigator</title>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
     <link rel="stylesheet" href="default.css"/>
     <?php $xajax->printJavascript('xajax/'); ?>
@@ -60,95 +62,99 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
   </script>
   </head>
   <body>
+ 
 <p><a href='clearsession.php'>start from scratch</a></p>
-<h3>DBPedia-Navigator-Test</h3>
-<div id="layer" style="display:none"><div id="layerContent" style="display:none"></div></div>
-<div id="wrapper">
-<div id="leftSidebar">
-
-<div class="box">
-  <div class="boxtitle">Search</div>
-  <div class="boxcontent" id="search">
-	Search:<br/>
-	<input type="text" name="label" id="label" /><br/>
-	<input type="button" value="Article" class="button" onclick="xajax_searchAndShowArticle(document.getElementById('label').value);return false;" />&nbsp;&nbsp;&nbsp;
-	<input type="button" value="Fulltext" class="button" onclick=""/>
-  </div> <!-- boxcontent -->
-</div> <!-- box -->
-
-<div class="box">
-  <div class="boxtitle">Searchresults</div>
-  <div class="boxcontent">
-  <div id="searchcontent" style="display:block"></div>
-  <div id="loadingSubject" style="display:none"><img src="ajax-loader.gif" alt="Loading..."/></div>
-  </div> <!-- boxcontent -->
-</div> <!-- box -->
-
-<div class="box" id="concept">
-  <div class="boxtitlewithbutton"><table border="0" class="titletable"><tr><td class="left">Learned Concept</td><td class="right"><input type="button" value="Learn" class="button" onclick="xajax_learnConcept();return false;" /></td></tr></table></div>
-  <div class="boxcontent">
-  <div id="conceptcontent" style="display:none"></div>
-  <div id="loadingConcept" style="display:none"><img src="ajax-loader.gif" alt="Loading..."/></div>
-  </div> <!-- boxcontent -->
-</div> <!-- box -->
-
-<div class="box" id="conceptSubjects">
-  <div class="boxtitlewithbutton"><table border="0" class="titletable"><tr><td class="left">Subjects From Concept</td><td class="right"><input type="button" value="Show" class="button" onclick="xajax_getSubjectsFromConcept();return false;" /></td></tr></table></div>
-  <div class="boxcontent">
-  <div id="conceptsubjectcontent" style="display:none"></div>
-  <div id="loadingConceptSubjects" style="display:none"><img src="ajax-loader.gif" alt="Loading..."/></div>
-  </div> <!-- boxcontent -->
-</div> <!-- box -->
-
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-</div><!-- END leftSidebar -->
-
-<div id="content">
-<div class="box">
-  <div class="boxtitlewithbutton"><table border="0" class="titletable"><tr><td class="left" id="ArticleTitle">Article</td><td class="right"><span id="contentbuttons"></span></td></tr></table></div>
-  <div class="boxcontent" id="article">
-  <div id="articlecontent" style="display:block"></div>
-  <div id="loadingArticle" style="display:none"><img src="ajax-loader.gif" alt="Loading..."/></div>
-  </div> <!-- boxcontent -->
-</div> <!-- box -->
-
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-</div><!-- content -->
-<div id="rightSidebar">
-
-<div class="box">
-  <div class="boxtitlewithbutton"><table border="0" class="titletable"><tr><td class="left">Positives</td><td class="right"><input type="button" value="Clear" class="button" onclick="xajax_clearPositives();return false;" /></td></tr></table></div>
-  <div class="boxcontent" id="Positives">
-  </div> <!-- boxcontent -->
-</div> <!-- box -->
-
-<div class="box">
-  <div class="boxtitlewithbutton"><table border="0" class="titletable"><tr><td class="left">Negatives</td><td class="right"><input type="button" value="Clear" class="button" onclick="xajax_clearNegatives();return false;" /></td></tr></table></div>
-  <div class="boxcontent" id="Negatives">
-  </div> <!-- boxcontent -->
-</div> <!-- box -->
-
-<div class="box">
-  <div class="boxtitle">Last Articles</div>
-  <div class="boxcontent" id="lastarticles">
-  </div> <!-- boxcontent -->
-</div> <!-- box -->
-
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />      
-</div><!-- rightSidebar -->
-<div id="clear"></div>
+<h1>DBPedia Navigator</h1>
+<div id="layer" style="display:none">
+	<div id="layerContent" style="display:none"></div>
 </div>
-<?php
+<div id="wrapper">
+	<div id="leftSidebar">
 
-$uri = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+		<div class="box">
+		  <div class="boxtitle">Search DBpedia</div>
+		  <div class="boxcontent" id="search">
+			<!-- Search:<br/> -->
+			<input type="text" name="label" id="label" /><br/>
+			<input type="button" value="Search" class="button" onclick="xajax_searchAndShowArticle(document.getElementById('label').value);return false;" />&nbsp;&nbsp;&nbsp;
+			<!--  <input type="button" value="Fulltext" class="button" onclick=""/> -->
+		  </div> <!-- boxcontent -->
+		</div> <!-- box -->
 
-echo '<div><a href="http://validator.w3.org/check?uri='.$uri.'"';
-echo '><img src="images/valid-xhtml10.png" alt="valid XHTML 1.0" /></a>'."\n";
-echo '<a href="http://jigsaw.w3.org/css-validator/validator?uri='.$uri.'"';
-echo '><img src="images/valid-css.png" alt="valid CSS" /></a></div>'."\n";
+		<div class="box">
+		  <div class="boxtitle">Search Results</div>
+		  <div class="boxcontent">
+		  <div id="searchcontent" style="display:block"></div>
+		  <div id="loadingSubject" style="display:none"><img src="ajax-loader.gif" alt="Loading..."/></div>
+		  </div> <!-- boxcontent -->
+		</div> <!-- box -->
 
-?>
+		<div class="box" id="concept">
+		  <div class="boxtitlewithbutton"><table border="0" class="titletable"><tr><td class="left">Learned Concept</td><td class="right"><input type="button" value="Learn" class="button" onclick="xajax_learnConcept();return false;" /></td></tr></table></div>
+		  <div class="boxcontent">
+		  <div id="conceptcontent" style="display:none"></div>
+		  <div id="loadingConcept" style="display:none"><img src="ajax-loader.gif" alt="Loading..."/></div>
+		  </div> <!-- boxcontent -->
+		</div> <!-- box -->
 
+		<div class="box" id="conceptSubjects">
+		  <div class="boxtitlewithbutton"><table border="0" class="titletable"><tr><td class="left">Subjects From Concept</td><td class="right"><input type="button" value="Show" class="button" onclick="xajax_getSubjectsFromConcept();return false;" /></td></tr></table></div>
+		  <div class="boxcontent">
+		  <div id="conceptsubjectcontent" style="display:none"></div>
+		  <div id="loadingConceptSubjects" style="display:none"><img src="ajax-loader.gif" alt="Loading..."/></div>
+		  </div> <!-- boxcontent -->
+		</div> <!-- box -->
+		
+		<div id="validation">
+			<?php
+			$uri = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+			
+			echo '<div><a href="http://validator.w3.org/check?uri='.$uri.'"';
+			echo '><img src="images/valid-xhtml10.png" alt="valid XHTML 1.0" /></a>'."\n";
+			echo '<a href="http://jigsaw.w3.org/css-validator/validator?uri='.$uri.'"';
+			echo '><img src="images/valid-css.png" alt="valid CSS" /></a></div>'."\n";
+			?>	
+		</div>		
+		
+	</div><!-- END leftSidebar -->
+
+	<div id="content">
+		<div class="box">
+		  <div class="boxtitlewithbutton"><table border="0" class="titletable"><tr><td class="left" id="ArticleTitle">Article</td><td class="right"><span id="contentbuttons"></span></td></tr></table></div>
+		  <div class="boxcontent" id="article">
+		  <div id="articlecontent" style="display:block"></div>
+		  <div id="loadingArticle" style="display:none"><img src="ajax-loader.gif" alt="Loading..."/></div>
+		  </div> <!-- boxcontent -->
+		</div> <!-- box -->
+	</div><!-- content -->
+	
+	<div id="rightSidebar">
+
+		<div class="box">
+		  <div class="boxtitlewithbutton"><table border="0" class="titletable"><tr><td class="left">Positives</td><td class="right"><input type="button" value="Clear" class="button" onclick="xajax_clearPositives();return false;" /></td></tr></table></div>
+		  <div class="boxcontent" id="Positives">
+		  </div> <!-- boxcontent -->
+		</div> <!-- box -->
+
+		<div class="box">
+		  <div class="boxtitlewithbutton"><table border="0" class="titletable"><tr><td class="left">Negatives</td><td class="right"><input type="button" value="Clear" class="button" onclick="xajax_clearNegatives();return false;" /></td></tr></table></div>
+		  <div class="boxcontent" id="Negatives">
+		  </div> <!-- boxcontent -->
+		</div> <!-- box -->
+
+		<div class="box">
+		  <div class="boxtitle">Last Articles</div>
+		  <div class="boxcontent" id="lastarticles">
+		  </div> <!-- boxcontent -->
+		</div> <!-- box -->
+
+	</div><!-- rightSidebar -->
+	
+	<!--  <div id="clear"></div> -->
+	
+
+	
+</div><!--  wrapper -->
   </body>
 </html>
 			
