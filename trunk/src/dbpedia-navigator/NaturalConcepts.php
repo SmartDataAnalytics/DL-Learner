67c45d0099f6ad95f01211ba406c291d192f7cd7
sknappe <sknappe@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-01-16 15:42:53 +0000

    the uris are now identified out of the concept

--- trunk/src/dbpedia-navigator/NaturalConcepts.php
+++ trunk/src/dbpedia-navigator/NaturalConcepts.php
@@ -15,17 +15,30 @@ class NaturalConcepts
 	
 	function identifyConcepts()
 	{
+		$temp=$this->concept;
 		$ret=array();
+		$offset=0;
 		while (true){
-			$nextpos=strpos("http",$this->concept);
-			$nextend=strpos()
+			$nextpos=strpos($temp,"http",$offset);
+			if (!$nextpos) break;
+			$nextend=preg_match("/\040|.TOP|.BOTTOM|.EXISTS|.ALL|.\(/",$temp,$treffer,PREG_OFFSET_CAPTURE,$nextpos);
+			if (!$nextend){
+				$uri=substr($temp,$nextpos,strlen($temp)-$nextpos);
+				$ret[]=$uri;
+				break;
+			}
+			$uri=substr($temp,$nextpos,$treffer[0][1]-$nextpos);
+			$ret[]=$uri;
+			$offset=$treffer[0][1];
 		}
+		
+		print_r($ret);
 		return $ret;
 	}
 }
 
-$conc="EXISTS http://dbpedia.org/property/website AND http://dbpedia.org/resource/Berlin";
+$conc="EXISTS http://dbpedia.org/property/website.TOP AND http://dbpedia.org/resource/Berlin";
 $nc=new NaturalConcepts($conc);
 $ic=$nc->getNaturalConcept();
-print_r($ic);
+//print_r($ic);
 ?>
