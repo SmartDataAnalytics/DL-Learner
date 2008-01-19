a6b1d865f66a6c5f4684b4044003b0e67bcc49fb
jenslehmann <jenslehmann@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-01-19 10:16:30 +0000

    - added JUnit library for unit tests (please update your classpath) - implemented first unit test - added missing getName() methods

---
+++ trunk/src/dl-learner/org/dllearner/test/AllTestsRunner.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2007-2008, Jens Lehmann
+ *
+ * This file is part of DL-Learner.
+ * 
+ * DL-Learner is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DL-Learner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.dllearner.test;
+
+import org.junit.runner.JUnitCore;
+
+/**
+ * Class designed to run all DL-Learner component tests. Note,
+ * that in Eclipse (and similar in other IDEs) you can run 
+ * JUnit tests by clicking on a file containing methods annotated
+ * with @Test and "Run As JUnit Test".
+ * 
+ * @author Jens Lehmann
+ * 
+ */
+public class AllTestsRunner {
+
+	public static void main(String[] args) {
+		JUnitCore.main("org.dllearner.test.ComponentTests");
+	}
+
+}
