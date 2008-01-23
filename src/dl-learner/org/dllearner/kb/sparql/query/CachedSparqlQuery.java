883b37f453ccf6dec00012fc48097052097a3c9a
Sebastian Hellmann <kurzum@users.noreply.github.com>
2008-01-23 11:15:31 +0000

    cache integrated

--- src/dl-learner/org/dllearner/kb/sparql/query/CachedSparqlQuery.java
+++ src/dl-learner/org/dllearner/kb/sparql/query/CachedSparqlQuery.java
@@ -2,6 +2,8 @@ package org.dllearner.kb.sparql.query;
 
 import org.dllearner.kb.sparql.configuration.SparqlEndpoint;
 
+import com.hp.hpl.jena.query.ResultSet;
+
 /**
  * Does the same as SparqlQuery, but uses the cache. key should be an uri or
  * something that can be mapped to a file see cache
@@ -40,6 +42,7 @@ public class CachedSparqlQuery {
 	 * 
 	 * @return String xml
 	 */
+	@Deprecated
 	public String getAsXMLString() {
 		String FromCache = cache.get(key, queryString);
 		if (debug_no_cache) {
@@ -49,10 +52,8 @@ public class CachedSparqlQuery {
 		// if not in cache get it from EndPoint
 		if (FromCache == null) {
 			// configuration.increaseNumberOfuncachedSparqlQueries();
-
 			xml = this.sparqlQuery.getAsXMLString();
 			// sendAndReceiveSPARQL(sparql);
-
 			// p(sparql);
 			// System.out.println(xml);
 			if (!debug_no_cache) {
@@ -66,6 +67,45 @@ public class CachedSparqlQuery {
 		}
 
 		return xml;
+	}
+	
+	/**
+	 * TODO can further be optimized
+	 * @return a jena ResultSet
+	 */
+	public ResultSet getAsResultSet(){
+		return SparqlQuery.JSONtoResultSet(getAsJSON());
+	}
+	
+	/**
+	 * sends a query and returns JSON using cache
+	 * 
+	 * @return String JSON
+	 */	
+	public String getAsJSON() {
+		String FromCache = cache.get(key, queryString);
+		if (debug_no_cache) {
+			FromCache = null;
+		}
+		String JSON = null;
+		
+		// if not in cache get it from EndPoint
+		if (FromCache == null) {
+			// configuration.increaseNumberOfuncachedSparqlQueries();
+			JSON = this.sparqlQuery.getAsJSON();
+			// sendAndReceiveSPARQL(sparql);
+			// p(sparql);
+			// System.out.println(xml);
+			if (!debug_no_cache) {
+				cache.put(key, queryString, JSON);
+			}
+			// System.out.print("\n");
+		} else {
+			// configuration.increaseNumberOfCachedSparqlQueries();
+			JSON = FromCache;
+			// System.out.println("FROM CACHE");
+		}
+		return JSON;
 	}
 
 	public void stop() {
