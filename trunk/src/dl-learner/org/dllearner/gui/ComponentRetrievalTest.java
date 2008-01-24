83e4cfe8efc5920f2d785fefd7e70be188dbc00e
jenslehmann <jenslehmann@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-01-24 08:57:25 +0000

    added example for Tilo

--- trunk/src/dl-learner/org/dllearner/gui/ComponentRetrievalTest.java
+++ trunk/src/dl-learner/org/dllearner/gui/ComponentRetrievalTest.java
@@ -23,6 +23,10 @@ import java.util.List;
 
 import org.dllearner.core.ComponentManager;
 import org.dllearner.core.KnowledgeSource;
+import org.dllearner.core.config.ConfigEntry;
+import org.dllearner.core.config.InvalidConfigOptionValueException;
+import org.dllearner.core.config.StringConfigOption;
+import org.dllearner.kb.OWLFile;
 
 /**
  * @author Jens Lehmann
@@ -34,10 +38,23 @@ public class ComponentRetrievalTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// example 1: getting all components of a given type
 		ComponentManager cm = ComponentManager.getInstance();
 		List<Class<? extends KnowledgeSource>> sources = cm.getKnowledgeSources();
 		cm.knowledgeSource(sources.get(0));
 		System.out.println(sources.get(1).toString());
+		
+		// example 2: setting a config value using a ConfigEntry object
+		KnowledgeSource owlFile = cm.knowledgeSource(OWLFile.class);
+		StringConfigOption urlOption = (StringConfigOption) cm.getConfigOption(OWLFile.class, "url");
+		ConfigEntry<String> urlEntry;
+		try {
+			urlEntry = new ConfigEntry<String>(urlOption, "http://example.com");
+			cm.applyConfigEntry(owlFile, urlEntry);
+		} catch (InvalidConfigOptionValueException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
