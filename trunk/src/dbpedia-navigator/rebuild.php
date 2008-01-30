fd54227a55bd66978945f88c1b27106b1a285b7e
sknappe <sknappe@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-01-30 10:26:38 +0000

    changed for new Web-Service interface

--- trunk/src/dbpedia-navigator/rebuild.php
+++ trunk/src/dbpedia-navigator/rebuild.php
@@ -14,11 +14,6 @@ DLLearnerConnection::loadWSDLfiles($settings->wsdluri);
 // we need to make sure that PHP really uses the new WSDL file
 // and does not cache, so we disable the cache and load it
 ini_set("soap.wsdl_cache_enabled","0");
-$sc=new DLLearnerConnection($settings->dbpediauri,$settings->wsdluri);
-//$sc->getIDs();
-// TODO: does not work; 
-// maybe try to set ttl of cache to 1 second,
-// wait for 1 second and then redirect to start page
 
 // redirect to index page
 $index_uri = 'http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']).'/index.php';
