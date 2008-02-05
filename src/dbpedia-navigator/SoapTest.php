dc2d1927089ff37c5a6061c7dd7b7434753103a9
sknappe <sknappe@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-02-05 15:39:08 +0000

    deleted test files, no longer necessary

--- src/dbpedia-navigator/SoapTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$start=microtime(true);
-	$client = new SoapClient("http://www.foreclosuredatabank.com/soapserver.php?wsdl");
-	print($client->latestProperties('33149'));
-	$stop=microtime(true)-$start;
-	print "<br/>".$stop;
-?>
