56e3cdce1fc44bb248d3a1ed50aae41786ca9c96
jenslehmann <jenslehmann@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-02-08 09:04:00 +0000

    wrote Prolog parser test (found problems with negative numbers)

---
+++ trunk/src/dl-learner/org/dllearner/examples/Carcinogenesis.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (C) 2007-2008, Jens Lehmann
+ *
+ * This file is part of DL-Learner.
+ * 
+ * DL-Learner is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DL-Learner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.dllearner.examples;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.dllearner.parser.ParseException;
+import org.dllearner.parser.PrologParser;
+import org.dllearner.utilities.Files;
+
+/**
+ * This class maps the carcinogenesis Prolog files to an OWL file. In a first
+ * step, a Prolog parser is used to read all files. The main step involves
+ * applying mapping Prolog clauses to OWL axioms through domain specific mapping
+ * rules.
+ * 
+ * @author Jens Lehmann
+ * 
+ */
+public class Carcinogenesis {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		try {
+			String prologFile = "examples/carcinogenesis/prolog/atoms.pl";
+			File file = new File(prologFile);
+			String content = Files.readFile(file);
+			PrologParser pp = new PrologParser();
+			pp.parseProgram(content);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
