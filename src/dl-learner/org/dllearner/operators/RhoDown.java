419d899f5fe7856e72ea918756e07efbf620eb8d
Jens Lehmann <JensLehmann@users.noreply.github.com>
2008-02-27 11:37:51 +0000

    Subclipse Bug I

--- src/dl-learner/org/dllearner/operators/RhoDown.java
+++ src/dl-learner/org/dllearner/operators/RhoDown.java
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
-package org.dllearner.refinementoperators;
+package org.dllearner.operators;
 
 import java.util.HashMap;
 import java.util.HashSet;
