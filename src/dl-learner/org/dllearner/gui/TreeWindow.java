60c1a627473aca8944aac1285a6a679790169d13
tonytacker <tonytacker@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-03-09 02:16:01 +0000

    add an example tree in new window for tests

---
+++ src/dl-learner/org/dllearner/gui/TreeWindow.java
@@ -0,0 +1,65 @@
+package org.dllearner.gui;
+
+/**
+ * Copyright (C) 2007-2008, Jens Lehmann
+ *
+ * This file is part of DL-Learner.
+ * 
+ * DL-Learner is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DL-Learner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import javax.swing.*;
+import javax.swing.tree.*;
+
+/**
+ * TreeWindow
+ * 
+ * @author Tilo Hielscher
+ */
+public class TreeWindow extends JFrame {
+
+	private static final long serialVersionUID = -5807192061389763835L;
+
+	@SuppressWarnings("unused")
+	private Config config;
+
+	public TreeWindow(Config config) {
+		this.config = config;
+		this.setTitle("DL-Learner Tree");
+		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		this.setLocationByPlatform(true);
+		this.setSize(300, 400);
+
+		// set icon
+		setIconImage(java.awt.Toolkit.getDefaultToolkit().getImage(
+				this.getClass().getResource("icon.gif")));
+	
+		// tree
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode( "root" ); 
+		for ( int knot = 0; knot < 4; knot++ ) 
+		{ 
+		  DefaultMutableTreeNode node = new DefaultMutableTreeNode( "knot " + knot ); 
+		  root.add( node ); 
+		  for ( int leaf = 1; leaf < 4; leaf++ ) 
+		    node.add( new DefaultMutableTreeNode("leaf " + (knot*3+leaf )) ); 
+		} 
+		JTree tree = new JTree( root );
+		this.add( new JScrollPane( tree ) ); 
+
+		this.setVisible(true);
+	}
+	
+	
+}
