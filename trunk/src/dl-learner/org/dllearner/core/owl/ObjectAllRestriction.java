2f04863b776580702add1fbbdd6aafc4ebd613c8
kurzum <kurzum@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-04-03 11:55:26 +0000

    owl api partial fix for #1931147
    http://sourceforge.net/tracker/index.php?func=detail&aid=1931147&group_id=203619&atid=986319

--- trunk/src/dl-learner/org/dllearner/core/owl/ObjectAllRestriction.java
+++ trunk/src/dl-learner/org/dllearner/core/owl/ObjectAllRestriction.java
@@ -40,7 +40,7 @@ public class ObjectAllRestriction extends ObjectQuantorRestriction {
       
 	@Override
 	public String toManchesterSyntaxString(String baseURI, Map<String, String> prefixes) {
-	    return restrictedPropertyExpression.toString(baseURI, prefixes) + " some " + children.get(0).toManchesterSyntaxString(baseURI, prefixes);
+	    return restrictedPropertyExpression.toString(baseURI, prefixes) + " only " + children.get(0).toManchesterSyntaxString(baseURI, prefixes);
 	}	   
     
 	/* (non-Javadoc)
