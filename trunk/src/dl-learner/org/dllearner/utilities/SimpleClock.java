043fab23463cee0903e397b022235a0f223a204e
kurzum <kurzum@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-04-07 10:07:41 +0000

    more comments

--- trunk/src/dl-learner/org/dllearner/utilities/SimpleClock.java
+++ trunk/src/dl-learner/org/dllearner/utilities/SimpleClock.java
@@ -1,5 +1,9 @@
 package org.dllearner.utilities;
 
+/**
+ * class for counting time and output it
+ *
+ */
 public class SimpleClock {
 	private long time;
 	
@@ -7,9 +11,25 @@ public class SimpleClock {
 		time=System.currentTimeMillis();
 	}
 	
+	/**
+	 *  prints time needed
+	 *  and resets the clock 
+	 */
 	public void printAndSet() {
 		long now=System.currentTimeMillis();
 		System.out.println("needed "+(now-time)+" ms");
+		time=now;
+	}
+	
+	
+	/**
+	 * prints time needed
+	 *  and resets the clock 
+	 * @param s String for printing
+	 */
+	public void printAndSet(String s) {
+		long now=System.currentTimeMillis();
+		System.out.println(s+" needed "+(now-time)+" ms");
 		time=now;
 	}
 	
