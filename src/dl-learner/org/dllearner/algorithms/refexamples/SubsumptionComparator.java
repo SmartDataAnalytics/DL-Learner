5ecf32a74ee3a689ff7c3afe6e06b0e9c3e5c9f2
JensLehmann <JensLehmann@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-05-13 13:45:10 +0000

    comparator finished

--- src/dl-learner/org/dllearner/algorithms/refexamples/SubsumptionComparator.java
+++ src/dl-learner/org/dllearner/algorithms/refexamples/SubsumptionComparator.java
@@ -4,10 +4,12 @@ import java.util.Comparator;
 
 import org.dllearner.core.ReasoningService;
 import org.dllearner.core.owl.Description;
+import org.dllearner.utilities.ConceptComparator;
 
-public class SubsumptionComparator implements Comparator<ExampleBasedNode> {
+public class SubsumptionComparator implements Comparator<Description> {
 
-	public ReasoningService rs;
+	private ReasoningService rs;
+	private ConceptComparator cc = new ConceptComparator();
 	
 	public SubsumptionComparator(ReasoningService rs) {
 		this.rs = rs;
@@ -16,6 +18,10 @@ public class SubsumptionComparator implements Comparator<ExampleBasedNode> {
 	public int compare(ExampleBasedNode arg0, ExampleBasedNode arg1) {
 		Description concept1 = arg0.getConcept();
 		Description concept2 = arg1.getConcept();
+		return compare(concept1, concept2);
+	}
+
+	public int compare(Description concept1, Description concept2) {
 		// return true if concept1 is a super concept of concept2
 		boolean value1 = rs.subsumes(concept1, concept2);
 		if(value1)
@@ -25,8 +31,10 @@ public class SubsumptionComparator implements Comparator<ExampleBasedNode> {
 		if(value2)
 			return -1;
 		
-		// both concepts are equal
-		return 0;
+//		System.out.println("Incomparable: " + concept1 + " " + concept2);
+		
+		// both concepts are incomparable => order them syntactically
+		return cc.compare(concept1, concept2);
 	}
 
 }
