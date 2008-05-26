a54b2b9648cf7ecd62d517f771160acbe730fbf4
kurzum <kurzum@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-05-26 09:46:46 +0000

    fixed warnings

--- trunk/src/dl-learner/org/dllearner/kb/extraction/TypedSparqlQuery.java
+++ trunk/src/dl-learner/org/dllearner/kb/extraction/TypedSparqlQuery.java
@@ -103,7 +103,7 @@ public class TypedSparqlQuery implements TypedSparqlQueryInterface {
 		
 		List<ResultBinding> l = ResultSetFormatter.toList(rs);
 		
-		p(l.toString());
+		logger.trace(l.toString());
 		for (ResultBinding resultBinding : l) {
 					
 			s.add(new StringTuple(resultBinding.get(a).toString(),
@@ -112,137 +112,13 @@ public class TypedSparqlQuery implements TypedSparqlQueryInterface {
 		return s;
 	}
 
-	/*@Deprecated
-	private Set<StringTuple> cachedSparql(URI uri, String sparqlQueryString,
-			String a, String b) {
-		return null;
-		/*
-		 * OLD CODE FOLLOWING keep until Jena is working String FromCache =
-		 * cache.get(u.toString(), sparqlQueryString); if (debug_no_cache) {
-		 * //FromCache = null; } String xml = null; // if not in cache get it
-		 * from EndPoint if (FromCache == null) {
-		 * configuration.increaseNumberOfuncachedSparqlQueries(); // try { xml =
-		 * sendAndReceiveSPARQL(sparqlQueryString);
-		 * 
-		 * //} catch (IOException e) {e.printStackTrace();}
-		 * 
-		 * p(sparqlQueryString); // System.out.println(xml); if
-		 * (!debug_no_cache) { cache.put(uri.toString(), sparqlQueryString,
-		 * xml); } // System.out.print("\n"); } else {
-		 * configuration.increaseNumberOfCachedSparqlQueries(); xml = FromCache; //
-		 * System.out.println("FROM CACHE"); }
-		 */
-		// System.out.println(sparql);
-		// System.out.println(xml);
-		// process XML
-	//}
+	
 
-	/**
-	 * TODO old XML processing, can be removed, once Jena is done
-	 * 
-	 * @param xml
-	 * @param a
-	 * @param b
-	 * @return a Set of Tuples <a|b>
-	 */
-	@Deprecated
-	public Set<StringTuple> processResult(String xml, String a, String b) {
+	
 
-		Set<StringTuple> ret = new HashSet<StringTuple>();
-		// TODO if result is empty, catch exceptions
-		String resEnd = "</result>";
-		String one = "binding name=\"" + a + "\"";
-		String two = "binding name=\"" + b + "\"";
-		String endbinding = "binding";
-		String uri = "uri";
-		// String uridel = "<uri>";
-		String bnode = "<bnode>";
-		// String uriend = "</uri>";
-		String predtmp = "";
-		String objtmp = "";
-		// System.out.println(getNextResult(xml));
-		String nextResult = "";
-		while ((nextResult = getNextResult(xml)) != null) {
-			// System.out.println(xml.indexOf(resEnd));
-			// System.out.println(xml);
-			if (nextResult.indexOf(bnode) != -1) {
-				xml = xml.substring(xml.indexOf(resEnd) + resEnd.length());
-				continue;
-			}
-			// get pred
-			// predtmp = nextResult.substring(nextResult.indexOf(one) +
-			// one.length());
-			predtmp = getinTag(nextResult, one, endbinding);
-			predtmp = getinTag(predtmp, uri, uri);
-			// System.out.println(predtmp);
+	
+	
 
-			// getobj
-			objtmp = getinTag(nextResult, two, endbinding);
-			objtmp = getinTag(objtmp, uri, uri);
-			// System.out.println(objtmp);
 
-			StringTuple st = new StringTuple(predtmp, objtmp);
-			// System.out.println(st);
-			ret.add(st);
-			xml = xml.substring(xml.indexOf(resEnd) + resEnd.length());
-
-		}
-		/*
-		 * while (xml.indexOf(one) != -1) {
-		 * 
-		 * 
-		 *  // System.out.println(new Tupel(predtmp,objtmp)); }
-		 */
-
-		return ret;
-
-	}
-
-	/**
-	 * TODO used by old XML processing, can be removed once Jena is done
-	 * 
-	 * @param xml
-	 * @return
-	 */
-	@Deprecated
-	private String getNextResult(String xml) {
-		String res1 = "<result>";
-		String res2 = "</result>";
-		if (xml.indexOf(res1) == -1)
-			return null;
-		xml = xml.substring(xml.indexOf(res1) + res1.length());
-		xml = xml.substring(0, xml.indexOf(res2));
-		// System.out.println(xml);
-		return xml;
-	}
-
-	/**
-	 * TODO used by old XML processing, can be removed once Jena is done
-	 * 
-	 * @param xml
-	 * @param starttag
-	 * @param endtag
-	 * @return
-	 */
-	@Deprecated
-	private String getinTag(String xml, String starttag, String endtag) {
-		String res1 = "<" + starttag + ">";
-		// System.out.println(res1);
-		String res2 = "</" + endtag + ">";
-		if (xml.indexOf(res1) == -1)
-			return null;
-		xml = xml.substring(xml.indexOf(res1) + res1.length());
-		// System.out.println(xml);
-		xml = xml.substring(0, xml.indexOf(res2));
-		// System.out.println(xml);
-
-		return xml;
-	}
-
-	public void p(String str) {
-		if (print_flag) {
-			System.out.println(str);
-		}
-	}
 
 }
