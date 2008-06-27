c7654622ded7242a97d840eaa180c07b91291540
kurzum <kurzum@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-06-27 13:44:33 +0000

    comments and formatting

--- trunk/src/dl-learner/org/dllearner/kb/sparql/SparqlQuery.java
+++ trunk/src/dl-learner/org/dllearner/kb/sparql/SparqlQuery.java
@@ -44,16 +44,18 @@ import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
 public class SparqlQuery {
 
 	private static Logger logger = Logger.getLogger(SparqlQuery.class);
-	
 
 	private boolean isRunning = false;
+
 	private boolean wasExecuted = false;
+
 	private String queryString;
+
 	private QueryEngineHTTP queryExecution;
+
 	private SparqlEndpoint endpoint;
-	//private ResultSet rs;
+
 	private String json = null;
-//	private SparqlQueryException sendException=null;
 
 	/**
 	 * Standard constructor.
@@ -62,32 +64,29 @@ public class SparqlQuery {
 	 * @param endpoint
 	 */
 	public SparqlQuery(String queryString, SparqlEndpoint endpoint) {
-	    //	QUALITY there seems to be a bug in ontowiki
-	    	this.queryString = queryString.replaceAll("\n", " ");
+		// QUALITY there seems to be a bug in ontowiki
+		this.queryString = queryString.replaceAll("\n", " ");
 		this.endpoint = endpoint;
 	}
 
-	
-	
-	
-	
 	/**
-	 * Sends a SPARQL query using the Jena library.
-	 * main format is JSON use method getasjson
+	 * Sends a SPARQL query using the Jena library. main format is JSON, use
+	 * method getasjson
 	 * 
 	 * @return nothing
 	 */
-	
+
 	public void send() {
-	    	wasExecuted = true;
+		wasExecuted = true;
+		// isRunning = true;
 		ResultSet rs;
-		//isRunning = true;
+
 		writeToSparqlLog("***********\nNew Query:");
 		writeToSparqlLog(queryString);
 		writeToSparqlLog(endpoint.getURL().toString());
-		
+
 		String service = endpoint.getURL().toString();
-		
+
 		// Jena access to SPARQL endpoint
 		queryExecution = new QueryEngineHTTP(service, queryString);
 		for (String dgu : endpoint.getDefaultGraphURIs()) {
@@ -96,26 +95,25 @@ public class SparqlQuery {
 		for (String ngu : endpoint.getNamedGraphURIs()) {
 			queryExecution.addNamedGraph(ngu);
 		}
-		
-		
-		//TODO remove after overnext Jena release
-		HttpQuery.urlLimit = 3*1024 ;
-		JamonMonitorLogger.getTimeMonitor(SparqlQuery.class, "httpTime").start();
+
+		// TODO remove after overnext Jena release
+		HttpQuery.urlLimit = 3 * 1024;
+		JamonMonitorLogger.getTimeMonitor(SparqlQuery.class, "httpTime")
+				.start();
 		rs = queryExecution.execSelect();
 		JamonMonitorLogger.getTimeMonitor(SparqlQuery.class, "httpTime").stop();
-				
-		
-		logger.debug("query length: "+queryString.length()+" | ENDPOINT: "+endpoint.getURL().toString());
-		//writeToSparqlLog("query: "+queryString+ " | ENDPOINT: "+endpoint.getURL().toString());
-		
-		
-		
+
+		logger.debug("query length: " + queryString.length() + " | ENDPOINT: "
+				+ endpoint.getURL().toString());
+		// writeToSparqlLog("query: "+queryString+ " | ENDPOINT:
+		// "+endpoint.getURL().toString());
+
 		json = SparqlQuery.getAsJSON(rs);
-		
-		writeToSparqlLog("JSON: "+json);
-		
+
+		writeToSparqlLog("JSON: " + json);
+
 		isRunning = false;
-		
+
 	}
 
 	public void stop() {
@@ -126,22 +124,75 @@ public class SparqlQuery {
 	public String getQueryString() {
 		return queryString;
 	}
-	
+
 	public SparqlEndpoint getEndpoint() {
-	    return endpoint;
+		return endpoint;
 	}
-	
+
 	public boolean isRunning() {
 		return isRunning;
 	}
-	
-	public QueryEngineHTTP getExecution(){
+
+	public QueryEngineHTTP getExecution() {
 		return queryExecution;
 	}
-	
-//	public SparqlQueryException getException(){
-//		return sendException;
-//	}
+
+	public void setJson(String json) {
+		this.wasExecuted = true;
+		this.json = json;
+	}
+
+	public void setRunning(boolean running) {
+		this.isRunning = running;
+	}
+
+	/**
+	 * returns the Result of the query as JSON string executes the query if it
+	 * wasn't executed before
+	 * 
+	 * @return
+	 */
+	public String getJson() {
+		if (wasExecuted == false) {
+			this.send();
+		}
+		return json;
+	}
+
+	/**
+	 * makes a ResultSet from the Json String, depends on getJSON
+	 * 
+	 * @return
+	 */
+	public ResultSet getResultSet() {
+		if (getJson() == null) {
+			return null;
+		} else
+			return JSONtoResultSet(json);
+	}
+
+	/**
+	 * makes an XML String from the Json String, depends on getJSON
+	 * 
+	 * @return
+	 */
+	public String getXMLString() {
+		if (getJson() == null) {
+			return null;
+		} else
+			return JSONtoXML(json);
+	}
+
+	public static void writeToSparqlLog(String s) {
+		try {
+			FileWriter fw = new FileWriter("log/sparql.txt", true);
+			fw.write(s + "\n");
+			fw.flush();
+			fw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Converts Jena result set to XML.
@@ -149,20 +200,21 @@ public class SparqlQuery {
 	 * @return String xml
 	 */
 	public static String getAsXMLString(ResultSet resultSet) {
-		//if (rs == null)
-		//	this.send();
+		// if (rs == null)
+		// this.send();
 		return ResultSetFormatter.asXMLString(resultSet);
 	}
 
 	/**
 	 * Converts Jena result set to JSON.
 	 * 
-	 * @param resultSet The result set to transform.
+	 * @param resultSet
+	 *            The result set to transform.
 	 * @return JSON representation of the result set.
 	 */
 	public static String getAsJSON(ResultSet resultSet) {
 		// if (rs == null)
-		//	this.send();
+		// this.send();
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		ResultSetFormatter.outputAsJSON(baos, resultSet);
 		// possible Jena bug: Jena modifies the result set during
@@ -186,10 +238,10 @@ public class SparqlQuery {
 	public static ResultSet JSONtoResultSet(String json) {
 		ByteArrayInputStream bais = new ByteArrayInputStream(json
 				.getBytes(Charset.forName("UTF-8")));
-		//System.out.println("JSON " + json);
+		// System.out.println("JSON " + json);
 		return ResultSetFactory.fromJSON(bais);
 	}
-	
+
 	/**
 	 * Converts from JSON to xml format.
 	 * 
@@ -199,40 +251,5 @@ public class SparqlQuery {
 	 */
 	public static String JSONtoXML(String json) {
 		return getAsXMLString(JSONtoResultSet(json));
-	}
-
-	public String getJson() {
-	    	if(wasExecuted == false){this.send();}
-		return json;
-	}
-
-	public void setJson(String json) {
-	    	this.wasExecuted=true;
-		this.json = json;
-	}
-	
-	public void setRunning(boolean running){
-		this.isRunning=running;
-	}
-	
-	public static void writeToSparqlLog(String s){
-	    try{
-	    FileWriter fw = new FileWriter("log/sparql.txt",true);
-	    fw.write(s+"\n");
-	    fw.flush();
-	    fw.close();
-	    }catch (Exception e) {
-		e.printStackTrace();
-	    }
-	}
-	
-	public ResultSet getResultSet(){
-	    	if(getJson() == null) {return null;}
-		else return JSONtoResultSet(json) ;
-	}
-	
-	public String getXMLString(){
-	    	if(getJson() == null) {return null;}
-		else return JSONtoXML(json) ;
 	}
 }
