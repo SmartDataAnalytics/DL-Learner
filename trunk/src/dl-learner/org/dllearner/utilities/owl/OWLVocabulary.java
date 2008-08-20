481417560abfffc967969bcfd115694568ad0258
kurzum <kurzum@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-08-20 16:29:59 +0000

    changed back to old tupelaquisitor

--- trunk/src/dl-learner/org/dllearner/utilities/owl/OWLVocabulary.java
+++ trunk/src/dl-learner/org/dllearner/utilities/owl/OWLVocabulary.java
@@ -24,6 +24,7 @@ public class OWLVocabulary {
 	public static final String RDF_TYPE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 	
 	public static final String RDFS_SUBCLASS_OF = "http://www.w3.org/2000/01/rdf-schema#subClassOf";
+	public static final String RDFS_CLASS = "http://www.w3.org/2000/01/rdf-schema#Class";
 	
 	
 	public static final String OWL_SAME_AS = "http://www.w3.org/2002/07/owl#sameAs";
