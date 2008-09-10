470c3a63f6de3b8724d44bb13176504b0810f40b
kurzum <kurzum@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-09-10 10:34:43 +0000

    added URL import

--- trunk/src/dl-learner/org/dllearner/core/configurators/OWLFileConfigurator.java
+++ trunk/src/dl-learner/org/dllearner/core/configurators/OWLFileConfigurator.java
@@ -16,12 +16,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- */ 
+ */
 
 package org.dllearner.core.configurators;
 
 import java.net.URL;
-
 import org.dllearner.core.ComponentManager;
 import org.dllearner.kb.OWLFile;
 
