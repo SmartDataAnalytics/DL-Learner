b74b92c3e9cc26dbd75a3c59565e608537885e84
sknappe <sknappe@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-09-15 07:59:19 +0000

    put all javascript functions into js files

--- src/dbpedia-navigator/index.php
+++ src/dbpedia-navigator/index.php
@@ -41,70 +41,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
     <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=<?php print $settings->googleMapsKey;?>"
       type="text/javascript"></script>
     <script  src="js/ajax.js"></script>
-    <script type="text/javascript">
-    	function setRunning(running)
-    	{
-    		if (running) document.getElementById('Loading').style.display='inline';
-    		else document.getElementById('Loading').style.display='none';
-    	}
-    
-        function loadGoogleMap(Lat,Lng,Label) {
-        	if (GBrowserIsCompatible()) {
-	      		// Create and Center a Map
-	        	var map = new GMap2(document.getElementById("map"));
-	        	map.setCenter(new GLatLng(Lat, Lng), 12);
-	        	map.addControl(new GLargeMapControl());
-	        	map.addControl(new GMapTypeControl());
-	        	var marker=new GMarker(new GLatLng(Lat, Lng));
-	        	map.addOverlay(marker);
-	      	}
-    	}
-    	
-    	function show_results(class, number)
-		{
-		    var links=document.getElementById('results').getElementsByTagName('p');
-		    var j=0;
-		    var names;
-		    var hasClass;
-		    for (var i=0;i<links.length;i++){
-		    	if (class=='all'){
-		    		if ((j+1)>number&&j<(number+25)) links[i].style.display='block';
-		    		else links[i].style.display='none';
-		    		j++;
-		    	}
-		    	else{
-		    		names=links[i].getElementsByTagName('a')[0].className.split(' ');
-		    		hasClass=false;
-		    		for (var k=0;k<names.length;k++){
-		    			if (names[k]==class){
-		    				hasClass=true;
-		    				break;
-		    			}
-		    		}
-		    		if (hasClass){
-		    			if ((j+1)>number&&j<(number+25)) links[i].style.display='block';
-		    			else links[i].style.display='none';
-		    			j++;
-		    		}
-		    		else links[i].style.display='none';
-		    	}
-		    }
-		    if (j<number){
-		    	show_results(class,0);
-		    	return;
-		    }
-		    		
-		    var sitenumbers=document.getElementById('sitenumbers').getElementsByTagName('span');
-		    for (var i=0;i<sitenumbers.length;i++){
-		    	if ((parseInt(sitenumbers[i].getElementsByTagName('a')[0].innerHTML)-1)*25==number) sitenumbers[i].getElementsByTagName('a')[0].style.textDecoration='none';
-		    	else sitenumbers[i].getElementsByTagName('a')[0].style.textDecoration='underline';
-		    	if ((parseInt(sitenumbers[i].getElementsByTagName('a')[0].innerHTML)-1)*25>=j)
-		    		sitenumbers[i].style.display='none';
-		    	else 
-		    		sitenumbers[i].style.display='inline';
-		    }
-		}
-  </script>
+    <script  src="js/navigator.js"></script>
   </head>
   <body <?php print $onLoad;?>>
 
