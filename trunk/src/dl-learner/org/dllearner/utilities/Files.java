adbb9180291f706a58b4afa16d0f4a3d5e906169
kurzum <kurzum@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-09-25 19:26:59 +0000

    - added constructor in gui.config - new Classes for collecting data and writing a latex table

--- trunk/src/dl-learner/org/dllearner/utilities/Files.java
+++ trunk/src/dl-learner/org/dllearner/utilities/Files.java
@@ -19,12 +19,21 @@
  */
 package org.dllearner.utilities;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ibm.icu.util.StringTokenizer;
 
 /**
  * @author Jens Lehmann
@@ -41,8 +50,7 @@ public class Files {
 	 * @return Content of the file.
 	 */
 	public static String readFile(File file) throws FileNotFoundException, IOException {
-		
-		
+			
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		StringBuffer content = new StringBuffer();
 		try{
@@ -56,6 +64,74 @@ public class Files {
 		return content.toString();
 		
 	}
+	
+	/**
+	 * Reads in a file as Array
+	 * 
+	 * @param file
+	 *            The file to read.
+	 * @return StringArray with lines
+	 */
+	public static String[] readFileAsArray(File file) throws FileNotFoundException, IOException {
+		String content = readFile(file);
+		StringTokenizer st = new StringTokenizer(content, System.getProperty("line.separator"));
+		List<String> l = new ArrayList<String>();
+		while (st.hasMoreTokens()) {
+			l.add((String) st.nextToken());
+			
+		}
+		
+		return l.toArray(new String[l.size()]);
+		
+	}
+	
+	/**
+	 * writes a serializable Object to a File.
+	 * @param obj
+	 * @param file
+	 */
+	public static void writeObjectToFile(Object obj, File file){
+		
+		ObjectOutputStream oos = null;
+		try{
+			FileOutputStream fos = new FileOutputStream(file);
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+			oos = new ObjectOutputStream(bos);
+			
+			oos.writeObject(obj);
+			oos.flush();
+			oos.close();
+			}catch (Exception e) {
+				 e.printStackTrace();
+			}finally{
+				try{
+					oos.close();
+				}catch (Exception e) {
+					 e.printStackTrace();
+				}
+			}
+	}
+	
+	public static Object readObjectfromFile( File file){
+		ObjectInputStream ois = null;
+		try{
+			FileInputStream fis = new FileInputStream(file);
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			ois = new ObjectInputStream(bis);
+			return ois.readObject();
+		}catch (Exception e) {
+			 e.printStackTrace();
+		}finally{
+			try {
+				ois.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		return null;
+	}
+		
 
 	/**
 	 * Creates a new file with the given content or replaces the content of a
@@ -185,5 +261,7 @@ public class Files {
 		e.printStackTrace();
 	}
 	}
+	
+	
 
 }
