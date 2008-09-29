4e0e30061b351eb589a40e0392af52d577d94a4a
sknappe <sknappe@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-09-29 08:28:07 +0000

    changed encoding of some stuff

--- trunk/src/dbpedia-navigator/helper_functions.php
+++ trunk/src/dbpedia-navigator/helper_functions.php
@@ -466,7 +466,7 @@ function createCharacteristics($char)
 			}
 			else{
 				if ($number>1) $ret.='<li>';
-				$ret .= $val['value'];
+				$ret .= htmlentities(urldecode($val['value']));
 				if ($number>1) $ret.='</li>';
 			}
 		}
