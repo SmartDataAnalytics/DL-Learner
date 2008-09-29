c45f35e0c3f79bebbca10bec6467b7d870b06dd8
sknappe <sknappe@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-09-29 07:37:09 +0000

    bugfix

--- trunk/src/dbpedia-navigator/ajax_get_article.php
+++ trunk/src/dbpedia-navigator/ajax_get_article.php
@@ -61,11 +61,11 @@
 				$artTitle=urldecode(str_replace("_"," ",substr (strrchr ($url, "/"), 1)));
 			
 			// display a picture if there is one
-			if (isset($triples['http://dbpedia.org/property/imageCaption'])&&$triples['http://dbpedia.org/property/imageCaption'][0]['type']!='uri') $alt=$triples['http://dbpedia.org/property/imageCaption'][0]['value'];
+			if (isset($triples['http://dbpedia.org/property/imageCaption'])&&@$triples['http://dbpedia.org/property/imageCaption'][0]['type']!='uri') $alt=$triples['http://dbpedia.org/property/imageCaption'][0]['value'];
 			else if (isset($triples['http://dbpedia.org/property/caption'])&&$triples['http://dbpedia.org/property/imageCaption'][0]['type']!='uri') $alt=$triples['http://dbpedia.org/property/caption'][0]['value'];
 			else $alt='Picture of '.$artTitle;
 						
-			if(isset($triples['http://xmlns.com/foaf/0.1/depiction'])&&fopen($triples['http://xmlns.com/foaf/0.1/depiction'][0]['value'], 'r')){
+			if(isset($triples['http://xmlns.com/foaf/0.1/depiction'])&&@fopen($triples['http://xmlns.com/foaf/0.1/depiction'][0]['value'], 'r')){
 				$content.='<img src="'.$triples['http://xmlns.com/foaf/0.1/depiction'][0]['value'].'" alt="'.$alt.'" style="float:right; max-width:200px;" \>';
 			}
 			else if(isset($triples['http://xmlns.com/foaf/0.1/img'])&&fopen($triples['http://xmlns.com/foaf/0.1/img'][0]['value'], 'r')){
