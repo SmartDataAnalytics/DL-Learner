3cc25ce3ea3aa9921da15b2aece0fecb67dafef2
Jens Lehmann <JensLehmann@users.noreply.github.com>
2008-10-27 08:29:37 +0000

    started DBpedia Navigator templates

---
+++ src/dbpedia-navigator/templates/AbstractTemplate.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright (C) 2007-2008, Jens Lehmann
+ *
+ * This file is part of DL-Learner.
+ * 
+ * DL-Learner is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DL-Learner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * Abstract template class. Templates are used for displaying information
+ * about a particular entity in a user friendly way.
+ * 
+ * You can also use this class for convenience functions accessible by
+ * all concrete templates.
+ *  
+ * @author Jens Lehmann
+ */
+class AbstractTemplate {
+
+	abstract function printTemplate($triples);
+	
+}
+
+?>
