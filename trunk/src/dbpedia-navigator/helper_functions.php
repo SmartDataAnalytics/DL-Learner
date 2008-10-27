3b5af6a1cd55abddae814253e74b1935fdb0ad44
sknappe <sknappe@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-10-27 13:56:41 +0000

    bugfix

--- trunk/src/dbpedia-navigator/helper_functions.php
+++ trunk/src/dbpedia-navigator/helper_functions.php
@@ -398,7 +398,7 @@ function formatClassArray($ar,$classSystem) {
 	$string="<ul>";
 	for($i=0; $i<count($ar); $i++) {
 		if ($classSystem=="YAGO") $prefix = 'http://dbpedia.org/class/yago/';
-		else if ($classSystem=="DBpedia") $prefix='http://dbpedia.org/ontology/'
+		else if ($classSystem=="DBpedia") $prefix='http://dbpedia.org/ontology/';
 		if(substr($ar[$i]['value'],0,30)!=$prefix) continue;
 		$query="SELECT label FROM categories WHERE category='".$ar[$i]['value']."' LIMIT 1";
 		$res=$databaseConnection->query($query);
