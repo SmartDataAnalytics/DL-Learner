76805cfec9cbcde2049cbdc866ab25996d963f11
jenslehmann <jenslehmann@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-10-27 08:29:37 +0000

    started DBpedia Navigator templates

---
+++ trunk/src/dbpedia-navigator/templates/CityTemplate.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright (C) 2007-2008, Jens Lehmann
+ *
+ * This file is part of DL-Learner.
+ * 
+ * DL-Learner is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DL-Learner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * Template for cities.
+ *  
+ * @author Jens Lehmann
+ */
+class CityTemplate {
+
+	function getTemplate($triples) {
+		$content = "";
+		
+		$latitude = $triples['http://dbpedia.org/ontology/latitutedegrees'] + "° "
+			+ $triples['http://dbpedia.org/ontology/latitudeminutes'] + "′"
+			+ $triples['http://dbpedia.org/ontology/latitudeseconds'] + "″N";
+		
+		// .. continue ...
+		
+		return $content;
+	}
+	
+}
+?>
