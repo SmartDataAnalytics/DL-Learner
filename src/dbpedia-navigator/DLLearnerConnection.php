05f412c6a89e298f02faf1edf2e4f95b54b25c50
sknappe <sknappe@af9c125c-f337-0410-9a96-b85bd07cbc14>
2008-10-28 10:15:47 +0000

    bugfix

--- src/dbpedia-navigator/DLLearnerConnection.php
+++ src/dbpedia-navigator/DLLearnerConnection.php
@@ -62,11 +62,11 @@ class DLLearnerConnection
 		$this->client->applyConfigEntryString($this->id, $this->ksID, "predefinedManipulator", "DBPEDIA-NAVIGATOR");
 		$this->client->applyConfigEntryBoolean($this->id, $this->ksID, "useCache", true);
 		if(empty($negExamples)){
-			/*if ($settings->classSystem=="YAGO") $filterClasses=array("http://xmlns.com/foaf/","http://dbpedia.org/ontology/");
+			if ($settings->classSystem=="YAGO") $filterClasses=array("http://xmlns.com/foaf/","http://dbpedia.org/ontology/");
 			else if ($settings->classSystem=="DBpedia") $filterClasses=array("http://xmlns.com/foaf/","http://dbpedia.org/class/yago/","http://dbpedia.org/ontology/Resource");
 			$negExamples=$this->client->getNegativeExamples($this->id,$this->ksID,$posExamples,count($posExamples),"http://dbpedia.org/resource/",$filterClasses);
-			$negExamples=$negExamples->item;*/
-			$negExamples=getNegativeExamplesFromParallelClass($posExamples);
+			$negExamples=$negExamples->item;
+			//$negExamples=getNegativeExamplesFromParallelClass($posExamples);
 		}
 		$this->client->applyConfigEntryStringArray($this->id, $this->ksID, "instances", array_merge($posExamples,$negExamples));
 		$this->client->setReasoner($this->id, "fastInstanceChecker");
