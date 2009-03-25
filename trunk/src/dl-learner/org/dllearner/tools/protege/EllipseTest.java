3f543ada2e140ab179f7f6e1ac7869eb2f6fb182
jenslehmann <jenslehmann@af9c125c-f337-0410-9a96-b85bd07cbc14>
2009-03-25 11:23:17 +0000

    modified ellipse test such that it is working correctly

--- trunk/src/dl-learner/org/dllearner/tools/protege/EllipseTest.java
+++ trunk/src/dl-learner/org/dllearner/tools/protege/EllipseTest.java
@@ -10,19 +10,23 @@ import java.util.Random;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 
-public class EllipseTest extends JPanel{
-private Ellipse2D te;
-private Random random;
+public class EllipseTest extends JPanel {
+
+	private static final long serialVersionUID = -5676466024192284648L;
+	private Ellipse2D te;
+	private Random random;
+
 	public EllipseTest() {
-		te = new Ellipse2D.Double(0,0,150,150);
+		te = new Ellipse2D.Double(0, 0, 150, 150);
 		this.setSize(500, 500);
 		random = new Random();
 		JDialog dialog = new JDialog();
-		 dialog.add(this);
-		 dialog.setSize(600, 600);
-		 dialog.setVisible(true);
+		dialog.add(this);
+		dialog.setSize(600, 600);
+		dialog.setVisible(true);
 	}
-	
+
+	@Override
 	protected void paintComponent(Graphics g) {
 		Graphics2D g2D;
 		g2D = (Graphics2D) g;
@@ -32,9 +36,12 @@ private Random random;
 		double x = random.nextInt(500);
 		double y = random.nextInt(500);
 		int i = 0;
-		while(i < 10000) {
-			if(te.contains(new Point2D.Double(x, y))) {
-				g2D.drawString("*", ((int)x), ((int) y));
+		while (i < 1000) {
+			Point2D point = new Point2D.Double(x, y);
+			if (te.contains(point)) {
+				Ellipse2D circlePoint = new Ellipse2D.Double(x - 1, y - 1, 3, 3);
+				g2D.draw(circlePoint);
+				// g2D.drawString("*", ((int)x), ((int) y));
 				x = random.nextInt(500);
 				y = random.nextInt(500);
 				i++;
@@ -44,8 +51,8 @@ private Random random;
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		EllipseTest te = new EllipseTest();
+		new EllipseTest();
 	}
 }
