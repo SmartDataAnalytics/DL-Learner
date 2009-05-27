571bcf076b3a9ba6df8b237c06233b5289450e89
Jens Lehmann <JensLehmann@users.noreply.github.com>
2009-05-27 10:51:47 +0000

    minor change

--- src/dl-learner/org/dllearner/scripts/matching/DBpediaLinkedGeoData.java
+++ src/dl-learner/org/dllearner/scripts/matching/DBpediaLinkedGeoData.java
@@ -366,7 +366,7 @@ public class DBpediaLinkedGeoData {
 			queryStr += "OPTIONAL { ?point <http://linkedgeodata.org/vocabulary#name_int> ?name_int } .";
 			// filter out ways => we assume that it is always better to match a point and not a way
 			// (if there is a way, there should also be a point but not vice versa)
-			queryStr += "FILTER (?point LIKE <http://linkedgeodata.org/triplify/node/%>) .";
+//			queryStr += "FILTER (?point LIKE <http://linkedgeodata.org/triplify/node/%>) .";
 			queryStr += "}";
 			
 //			SparqlQuery query = new SparqlQuery(queryStr, geoDataEndpoint);
@@ -414,7 +414,10 @@ public class DBpediaLinkedGeoData {
 				double distanceScore = Math.pow(frac-1,4);
 				
 				double score = 0.8 * stringSimilarity + 0.2 * distanceScore;
-//				if(qs.getResource("point").toString().contains("/way/");
+				// if there is a node and a way, we prefer the node (better representative) 
+				if(qs.getResource("point").toString().contains("/way/")) {
+					score -= 0.02;
+				}
 				
 				if(score > highestScore) {
 					highestScore = score;
