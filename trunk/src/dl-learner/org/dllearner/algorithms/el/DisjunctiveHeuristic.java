bdee6fe95b486600659052ca8663e36954196d40
jenslehmann <jenslehmann@af9c125c-f337-0410-9a96-b85bd07cbc14>
2009-06-25 16:56:18 +0000

    disjunctive EL algorithm ctd.

--- trunk/src/dl-learner/org/dllearner/algorithms/el/DisjunctiveHeuristic.java
+++ trunk/src/dl-learner/org/dllearner/algorithms/el/DisjunctiveHeuristic.java
@@ -8,8 +8,11 @@ public class DisjunctiveHeuristic implements ELHeuristic {
 		double diff = tree1.getScore()-tree2.getScore();
 		if(diff < 0.00001 && diff > -0.00001) {
 			return edt.compare(tree1.getDescriptionTree(), tree2.getDescriptionTree());
+		} else if(diff > 0){
+			return 1;
+//			return (int)Math.signum(diff);
 		} else {
-			return (int)Math.signum(diff);
+			return -1;
 		}
 	}
 
