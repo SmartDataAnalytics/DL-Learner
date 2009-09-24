8d08ed227625ce1574214028e54abe2b2253ffb9
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2009-09-24 08:51:02 +0000

    some gui fixes cleaned up code

--- trunk/src/dl-learner/org/dllearner/tools/ore/Sparql.java
+++ trunk/src/dl-learner/org/dllearner/tools/ore/Sparql.java
@@ -1,7 +1,6 @@
 package org.dllearner.tools.ore;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 
 import org.dllearner.kb.sparql.SPARQLTasks;
 import org.dllearner.kb.sparql.SparqlEndpoint;
@@ -13,8 +12,7 @@ public class Sparql {
 	 * @throws MalformedURLException 
 	 */
 	public static void main(String[] args) throws MalformedURLException {
-		URL url = new URL("http://dbpedia.org/sparql");
-		SPARQLTasks task = new SPARQLTasks(SparqlEndpoint.getEndpointDBpedia());//new SparqlEndpoint(url));
+		SPARQLTasks task = new SPARQLTasks(SparqlEndpoint.getEndpointDBpedia());
 
 		String queryString = "SELECT DISTINCT ?class WHERE {?class rdf:type owl:Class ." +
 				"?class rdfs:label ?label . FILTER(regex(?label, \"City\")) }";
