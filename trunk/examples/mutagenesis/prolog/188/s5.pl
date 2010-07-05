95af4846aa3b886eacb5873b22174a0c10ef7396
heeroyuy <heeroyuy@af9c125c-f337-0410-9a96-b85bd07cbc14>
2010-07-05 14:59:23 +0000

    -added conf files for mutagenesis and suramin ontology -added missing prolog files for mutagenesis knowledge base -added prolog files for alzheimer knowledge base

---
+++ trunk/examples/mutagenesis/prolog/188/s5.pl
@@ -0,0 +1,19 @@
+active(d6).
+active(d15).
+active(d69).
+active(d71).
+active(d87).
+active(d95).
+active(d104).
+active(d109).
+active(d177).
+active(d187).
+active(d44).
+active(d93).
+active(d97).
+active(d106).
+:- active(d132).
+:- active(d141).
+:- active(d73).
+:- active(d76).
+:- active(d113).
---
+++ trunk/examples/mutagenesis/prolog/188/s9.pl
@@ -0,0 +1,19 @@
+active(d4).
+active(d41).
+active(d49).
+active(d50).
+active(d59).
+active(d96).
+active(d117).
+active(d136).
+active(d153).
+active(d23).
+active(d32).
+active(d158).
+active(d167).
+active(d176).
+:- active(d98).
+:- active(d88).
+:- active(d124).
+:- active(d139).
+:- active(d168).
