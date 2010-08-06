245acee73d3b1c8071f424aa5f92fc2f5a2c2806
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2010-08-06 14:16:23 +0000

    Implemented OWL/XML conversion, which was incomplete before and reported as bug 3027353.

--- trunk/src/dl-learner/org/dllearner/utilities/owl/OWLAPIRenderers.java
+++ trunk/src/dl-learner/org/dllearner/utilities/owl/OWLAPIRenderers.java
@@ -21,14 +21,13 @@ package org.dllearner.utilities.owl;
 
 import java.io.StringWriter;
 
-import org.coode.xml.XMLWriterNamespaceManager;
+import org.coode.owlapi.owlxml.renderer.OWLXMLObjectRenderer;
+import org.coode.owlapi.owlxml.renderer.OWLXMLWriter;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClassExpression;
-import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLObjectProperty;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.util.ShortFormProvider;
 import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 
@@ -79,25 +78,13 @@ public class OWLAPIRenderers {
 	 */
 	public static String toOWLXMLSyntax(OWLClassExpression description) {
 		StringWriter sw = new StringWriter();
-		// set up default namespace and prefixes
-		XMLWriterNamespaceManager ns = new XMLWriterNamespaceManager("http://example.com");
-		ns.setPrefix("owl2", "http://www.w3.org/2006/12/owl2-xml#");
-		//TODO:OWLAPI3 conversion		
-//		OWLXMLWriter oxw = new OWLXMLWriter(sw, description.getontology);
-//		OWLXMLObjectRenderer renderer = new OWLXMLObjectRenderer(oxw);
-//		description.accept(renderer);
+		try {
+			OWLXMLWriter oxw = new OWLXMLWriter(sw, OWLManager.createOWLOntologyManager().createOntology(IRI.create("http://example.com/")));
+			OWLXMLObjectRenderer renderer = new OWLXMLObjectRenderer(oxw);
+			description.accept(renderer);
+		} catch (OWLOntologyCreationException e) {
+			e.printStackTrace();
+		}
 		return sw.toString();
 	}	
-	
-	public static void main(String args[]) {
-		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		OWLDataFactory factory = manager.getOWLDataFactory();
-		OWLClassExpression a1 = factory.getOWLClass(IRI.create("http://example.com/test#a1"));
-		OWLClassExpression a2 = factory.getOWLClass(IRI.create("http://example.com/test#a2"));
-		OWLObjectProperty r = factory.getOWLObjectProperty(IRI.create("http://example.com/test#r"));
-		OWLClassExpression d3 = factory.getOWLObjectSomeValuesFrom(r, a2);
-		OWLClassExpression d = factory.getOWLObjectIntersectionOf(a1,d3);
-		String s = toOWLXMLSyntax(d);
-		System.out.println(s);
-	}
 }
