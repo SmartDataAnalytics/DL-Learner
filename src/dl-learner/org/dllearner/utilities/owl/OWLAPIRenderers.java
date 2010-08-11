0660add9089051c0704675b2748c131bf75812e1
Jens Lehmann <JensLehmann@users.noreply.github.com>
2010-08-11 10:03:22 +0000

    implemented Turtle + RDF/XML renderer for axioms

--- src/dl-learner/org/dllearner/utilities/owl/OWLAPIRenderers.java
+++ src/dl-learner/org/dllearner/utilities/owl/OWLAPIRenderers.java
@@ -19,15 +19,25 @@
  */
 package org.dllearner.utilities.owl;
 
+import java.io.ByteArrayOutputStream;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 
 import org.coode.owlapi.owlxml.renderer.OWLXMLObjectRenderer;
 import org.coode.owlapi.owlxml.renderer.OWLXMLWriter;
+import org.coode.owlapi.turtle.TurtleOntologyFormat;
+import org.dllearner.core.owl.Axiom;
+import org.dllearner.utilities.StringFormatter;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.io.RDFXMLOntologyFormat;
+import org.semanticweb.owlapi.model.AddAxiom;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.semanticweb.owlapi.util.ShortFormProvider;
 import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 
@@ -87,4 +97,87 @@ public class OWLAPIRenderers {
 		}
 		return sw.toString();
 	}	
+	
+	public static String toOWLXMLSyntax(Axiom axiom) {
+		return toOWLXMLSyntax(OWLAPIAxiomConvertVisitor.convertAxiom(axiom));
+	}		
+	
+	public static String toOWLXMLSyntax(OWLAxiom axiom) {
+		StringWriter sw = new StringWriter();
+		try {
+			OWLXMLWriter oxw = new OWLXMLWriter(sw, OWLManager.createOWLOntologyManager().createOntology(IRI.create("http://example.com/")));
+			OWLXMLObjectRenderer renderer = new OWLXMLObjectRenderer(oxw);
+			axiom.accept(renderer);
+		} catch (OWLOntologyCreationException e) {
+			e.printStackTrace();
+		}
+		return sw.toString();
+	}		
+	
+	public static String toRDFXMLSyntax(Axiom axiom) {
+		return toRDFXMLSyntax(OWLAPIAxiomConvertVisitor.convertAxiom(axiom));
+	}		
+	
+	public static String toRDFXMLSyntax(OWLAxiom axiom) {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		String str = "";
+		try {
+			OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+			OWLOntology ontology = manager.createOntology(IRI.create("http://example.com/"));
+			manager.applyChange(new AddAxiom(ontology, axiom));
+			manager.saveOntology(ontology, new RDFXMLOntologyFormat(), out);
+			str = new String(out.toByteArray(), "UTF-8");
+		} catch (OWLOntologyCreationException e) {
+			e.printStackTrace();
+		} catch (OWLOntologyStorageException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return str;
+	}		
+	
+	public static String toTurtleSyntax(Axiom axiom) {
+		return toTurtleSyntax(OWLAPIAxiomConvertVisitor.convertAxiom(axiom), false);
+	}		
+	
+	// short version = a lot of stuff thrown out (not a standalone Turtle file anymore)
+	public static String toTurtleSyntax(Axiom axiom, boolean shortVersion) {
+		return toTurtleSyntax(OWLAPIAxiomConvertVisitor.convertAxiom(axiom), shortVersion);
+	}		
+	
+	public static String toTurtleSyntax(OWLAxiom axiom, boolean shortVersion) {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		String str = "";
+		try {
+			OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+			OWLOntology ontology = manager.createOntology(IRI.create("http://example.com/"));
+			manager.applyChange(new AddAxiom(ontology, axiom));
+			manager.saveOntology(ontology, new TurtleOntologyFormat(), out);
+			str = new String(out.toByteArray(), "UTF-8");
+		} catch (OWLOntologyCreationException e) {
+			e.printStackTrace();
+		} catch (OWLOntologyStorageException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		if(shortVersion) {
+			String[] lines = str.split("\n");
+			String shortStr = "";
+			for(String line : lines) {
+				if(!line.startsWith("@prefix") && 
+				   !line.startsWith("@base") && 
+				   !line.startsWith("#") &&
+				   !line.startsWith("<http://example.com/>") &&
+				   !(StringFormatter.isWhitespace(line))) {
+					shortStr += line + "\n";
+				}
+			}
+			return shortStr;
+		}
+		
+		return str;
+	}	
+	
 }
