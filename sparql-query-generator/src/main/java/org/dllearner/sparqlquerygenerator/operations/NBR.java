70eacebfa321eb58c32797b849197f4d91741c27
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2010-09-17 19:15:29 +0000

    Added License and author entries. Added preliminary clone() method for Query graph.

--- sparql-query-generator/src/main/java/org/dllearner/sparqlquerygenerator/operations/NBR.java
+++ sparql-query-generator/src/main/java/org/dllearner/sparqlquerygenerator/operations/NBR.java
@@ -1,9 +1,33 @@
+/**
+ * Copyright (C) 2007-2010, Jens Lehmann
+ *
+ * This file is part of DL-Learner.
+ * 
+ * DL-Learner is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DL-Learner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.dllearner.sparqlquerygenerator.operations;
 
 import java.util.Set;
 
 import org.dllearner.sparqlquerygenerator.datastructures.QueryGraph;
 
+/**
+ * 
+ * @author Lorenz Bühmann
+ *
+ */
 public class NBR {
 	
 	public static void computeNBR(QueryGraph posGraph, Set<QueryGraph> negGraphs){
