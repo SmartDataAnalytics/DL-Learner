820f9cdb4b3bf4314a6c341cdc7ba12639d2b856
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2010-09-25 08:04:55 +0000

    Continued query generator. Changed working datastructure from graph to tree. Implemented LGG operation Implemented NBR operation. Added some unit tests.

---
+++ trunk/sparql-query-generator/src/main/java/org/dllearner/sparqlquerygenerator/impl/QueryTreeFactoryImpl.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (C) 2007-2010, Jens Lehmann
+ *
+ * This file is part of DL-Learner.
+ * 
+ * DL-Learner is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DL-Learner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.dllearner.sparqlquerygenerator.impl;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import org.dllearner.sparqlquerygenerator.QueryTreeFactory;
+import org.dllearner.sparqlquerygenerator.datastructures.impl.QueryTreeImpl;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+
+/**
+ * 
+ * @author Lorenz Bühmann
+ *
+ */
+public class QueryTreeFactoryImpl implements QueryTreeFactory<String> {
+	
+	@Override
+	public QueryTreeImpl<String> getQueryTree(String example, Model model) {
+		return createTree2(model.getResource(example), model);
+	}
+
+	@Override
+	public QueryTreeImpl<String> getQueryTree(Resource example, Model model) {
+		return createTree2(example, model);
+	}
+	
+	private QueryTreeImpl<String> createTree(Resource s, Model model){
+		Map<Resource, Set<Statement>> resource2Statements = new HashMap<Resource, Set<Statement>>();
+		Statement st;
+		Set<Statement> statements;
+		for(Iterator<Statement> it = model.listStatements(); it.hasNext();){
+			st = it.next();
+			statements = resource2Statements.get(st.getSubject());
+			if(statements == null){
+				statements = new HashSet<Statement>();
+				resource2Statements.put(st.getSubject(), statements);
+			}
+			statements.add(st);
+		}
+				
+		return createTree(s, resource2Statements);
+	}
+	
+	private QueryTreeImpl<String> createTree(Resource s, Map<Resource, Set<Statement>> resource2Statements){
+		QueryTreeImpl<String> tree = new QueryTreeImpl<String>(s.toString());
+		if(resource2Statements.containsKey(s)){
+			for(Statement st : resource2Statements.get(s)){
+				if(st.getObject().isLiteral()){
+					tree.addChild(new QueryTreeImpl<String>(st.getObject().toString()), st.getPredicate().toString());
+				} else {
+					if(!tree.getUserObjectPathToRoot().contains(st.getObject().toString())){
+						tree.addChild(createTree(st.getObject().asResource(), resource2Statements), st.getPredicate().toString());
+					}
+				}
+			}
+		}
+		return tree;
+	}
+	
+	private QueryTreeImpl<String> createTree2(Resource s, Model model){
+		Map<String, Set<Statement>> resource2Statements = new HashMap<String, Set<Statement>>();
+		Statement st;
+		Set<Statement> statements;
+		for(Iterator<Statement> it = model.listStatements(); it.hasNext();){
+			st = it.next();
+			statements = resource2Statements.get(st.getSubject().toString());
+			if(statements == null){
+				statements = new HashSet<Statement>();
+				resource2Statements.put(st.getSubject().toString(), statements);
+			}
+			statements.add(st);
+		}
+		QueryTreeImpl<String> tree = new QueryTreeImpl<String>(s.toString());
+		fillTree(tree, resource2Statements);
+				
+		return tree;
+	}
+	
+	private void fillTree(QueryTreeImpl<String> tree, Map<String, Set<Statement>> resource2Statements){
+		if(resource2Statements.containsKey(tree.getUserObject())){
+			QueryTreeImpl<String> subTree;
+			for(Statement st : resource2Statements.get(tree.getUserObject())){
+				if(st.getObject().isLiteral()){
+					tree.addChild(new QueryTreeImpl<String>(st.getObject().toString()), st.getPredicate().toString());
+				} else {
+					if(!tree.getUserObjectPathToRoot().contains(st.getObject().toString())){
+						subTree = new QueryTreeImpl<String>(st.getObject().toString());
+						tree.addChild(subTree, st.getPredicate().toString());
+						fillTree(subTree, resource2Statements);
+					}
+				}
+			}
+		}
+	}
+
+}
