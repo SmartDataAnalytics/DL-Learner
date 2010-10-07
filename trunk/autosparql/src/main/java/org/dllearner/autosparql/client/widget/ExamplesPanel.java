f9277d4ef3fe3b1efd4f11d3533378afea74c87b
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2010-10-07 19:22:06 +0000

    Continued autosparql.

--- trunk/autosparql/src/main/java/org/dllearner/autosparql/client/widget/ExamplesPanel.java
+++ trunk/autosparql/src/main/java/org/dllearner/autosparql/client/widget/ExamplesPanel.java
@@ -1,7 +1,136 @@
 package org.dllearner.autosparql.client.widget;
 
+import java.util.ArrayList;
+
+import org.dllearner.autosparql.client.model.Example;
+
+import com.extjs.gxt.ui.client.core.XTemplate;
+import com.extjs.gxt.ui.client.data.ModelData;
+import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
+import com.extjs.gxt.ui.client.widget.grid.ColumnData;
+import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
+import com.extjs.gxt.ui.client.widget.grid.Grid;
+import com.extjs.gxt.ui.client.widget.grid.GridCellRenderer;
+import com.extjs.gxt.ui.client.widget.grid.GridViewConfig;
+import com.extjs.gxt.ui.client.widget.grid.RowExpander;
+import com.extjs.gxt.ui.client.widget.layout.RowData;
+import com.extjs.gxt.ui.client.widget.layout.RowLayout;
+import com.google.gwt.event.dom.client.ErrorEvent;
+import com.google.gwt.event.dom.client.ErrorHandler;
+import com.google.gwt.user.client.ui.Image;
 
 public class ExamplesPanel extends ContentPanel {
+	
+	private static final int WIDTH = 400;
+	private static final int HEIGHT = 600;
+	
+	private ListStore<Example> posExamplesStore;
+	private ListStore<Example> negExamplesStore;
+	
+	public ExamplesPanel(){
+		setLayout(new RowLayout());
+		setHeading("Examples");
+		setCollapsible(true);
+		setAnimCollapse(false);
+		setSize(WIDTH, HEIGHT);
+		
+		createPosExamplesGrid();
+		createNegExamplesGrid();
+	}
+	
+	private void createPosExamplesGrid(){
+		posExamplesStore = new ListStore<Example>();
+		
+		ArrayList<ColumnConfig> columns = new ArrayList<ColumnConfig>();
+		
+		XTemplate tpl = XTemplate.create("<p><b>Comment:</b><br>{comment}</p>");
+		RowExpander expander = new RowExpander();
+		expander.setTemplate(tpl);
+		columns.add(expander);
+		
+		GridCellRenderer<Example> imageRender = new GridCellRenderer<Example>() {
+
+			@Override
+			public Object render(Example model, String property,
+					ColumnData config, int rowIndex, int colIndex,
+					ListStore<Example> store, Grid<Example> grid) {
+				final Image image = new Image(model.getImageURL());
+				image.addErrorHandler(new ErrorHandler() {
+					
+					@Override
+					public void onError(ErrorEvent event) {
+						image.setUrl("no_images.jpeg");
+						
+					}
+				});
+				image.setPixelSize(40, 40);
+				return image;
+			}
+		
+		};
+		
+		ColumnConfig c = new ColumnConfig();
+		c.setId("imageURL");
+		columns.add(c);
+		c.setWidth(50);
+		c.setRenderer(imageRender);
+		
+		c = new ColumnConfig();
+		c.setId("label");
+		columns.add(c);
+		
+		GridCellRenderer<Example> buttonRender = new GridCellRenderer<Example>() {
+
+			@Override
+			public Object render(Example model, String property,
+					ColumnData config, int rowIndex, int colIndex,
+					ListStore<Example> store, Grid<Example> grid) {
+				
+				return null;
+			}
+		
+		};
+		
+		c = new ColumnConfig();
+		c.setId("");
+		c.setWidth(50);
+		columns.add(c);
+		
+		ColumnModel cm = new ColumnModel(columns);
+		
+		Grid<Example> grid = new Grid<Example>(posExamplesStore, cm);
+		grid.setHideHeaders(true);
+		grid.setAutoExpandColumn("label");
+		grid.setLoadMask(true);
+		grid.addPlugin(expander);
+		grid.getView().setViewConfig(new GridViewConfig(){
+			@Override
+			public String getRowStyle(ModelData model, int rowIndex,
+					ListStore<ModelData> ds) {
+				// TODO Auto-generated method stub
+				if(rowIndex % 2 == 0){
+					return "row-Style-Odd";
+				} else {
+					return "row-Style-Even";
+				}
+			}
+		});
+		
+		add(grid, new RowData(0.5, 1));
+	}
+	
+	private void createNegExamplesGrid(){
+		
+	}
+	
+	public void addPositiveExample(Example example){
+		posExamplesStore.add(example);
+	}
+	
+	public void addNegativeExample(Example example){
+		negExamplesStore.add(example);
+	}
 
 }
--- trunk/autosparql/src/main/java/org/dllearner/autosparql/client/widget/SearchPanel.java
+++ trunk/autosparql/src/main/java/org/dllearner/autosparql/client/widget/SearchPanel.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.dllearner.autosparql.client.SPARQLService;
 import org.dllearner.autosparql.client.model.Example;
 
+import com.extjs.gxt.ui.client.core.XTemplate;
 import com.extjs.gxt.ui.client.data.BasePagingLoader;
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.data.PagingLoadConfig;
@@ -12,6 +13,8 @@ import com.extjs.gxt.ui.client.data.PagingLoadResult;
 import com.extjs.gxt.ui.client.data.PagingLoader;
 import com.extjs.gxt.ui.client.data.RpcProxy;
 import com.extjs.gxt.ui.client.event.ButtonEvent;
+import com.extjs.gxt.ui.client.event.ComponentEvent;
+import com.extjs.gxt.ui.client.event.KeyListener;
 import com.extjs.gxt.ui.client.event.SelectionListener;
 import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
@@ -23,14 +26,22 @@ import com.extjs.gxt.ui.client.widget.grid.ColumnData;
 import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
 import com.extjs.gxt.ui.client.widget.grid.Grid;
 import com.extjs.gxt.ui.client.widget.grid.GridCellRenderer;
+import com.extjs.gxt.ui.client.widget.grid.GridViewConfig;
+import com.extjs.gxt.ui.client.widget.grid.RowExpander;
 import com.extjs.gxt.ui.client.widget.layout.HBoxLayout;
 import com.extjs.gxt.ui.client.widget.layout.RowData;
 import com.extjs.gxt.ui.client.widget.layout.RowLayout;
 import com.extjs.gxt.ui.client.widget.toolbar.PagingToolBar;
+import com.google.gwt.event.dom.client.ErrorEvent;
+import com.google.gwt.event.dom.client.ErrorHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Image;
 
 public class SearchPanel extends ContentPanel {
+	
+	private static final int WIDTH = 400;
+	private static final int HEIGHT = 600;
 	
 	private TextField<String> inputField;
 	private Button searchButton;
@@ -42,7 +53,7 @@ public class SearchPanel extends ContentPanel {
 		setHeading("Search");
 		setCollapsible(true);
 		setAnimCollapse(false);
-		setSize(300, 600);
+		setSize(WIDTH, HEIGHT);
 		
 		createInputPanel();
 		createSearchResultGrid();
@@ -55,6 +66,15 @@ public class SearchPanel extends ContentPanel {
 		inputField = new TextField<String>();
 		inputField.setTitle("");
 		inputField.setEmptyText("Enter search term");
+		inputField.addKeyListener(new KeyListener(){
+			@Override
+			public void componentKeyPress(ComponentEvent event) {
+				if(event.getKeyCode() == KeyCodes.KEY_ENTER){
+					onSearch();
+				}
+				super.componentKeyPress(event);
+			}
+		});
 		c.add(inputField);
 		
 		searchButton = new Button("Search");
@@ -90,14 +110,27 @@ public class SearchPanel extends ContentPanel {
 		
 		ArrayList<ColumnConfig> columns = new ArrayList<ColumnConfig>();
 		
+		XTemplate tpl = XTemplate.create("<p><b>Comment:</b><br>{comment}</p>");
+		RowExpander expander = new RowExpander();
+		expander.setTemplate(tpl);
+		columns.add(expander);
+		
 		GridCellRenderer<Example> imageRender = new GridCellRenderer<Example>() {
 
 			@Override
 			public Object render(Example model, String property,
 					ColumnData config, int rowIndex, int colIndex,
 					ListStore<Example> store, Grid<Example> grid) {
-				System.out.println(model.getImageURL());
-				Image image = new Image(model.getImageURL());
+				final Image image = new Image(model.getImageURL());
+				image.addErrorHandler(new ErrorHandler() {
+					
+					@Override
+					public void onError(ErrorEvent event) {
+						image.setUrl("no_images.jpeg");
+						
+					}
+				});
+				image.setPixelSize(40, 40);
 				return image;
 			}
 		
@@ -136,6 +169,19 @@ public class SearchPanel extends ContentPanel {
 		grid.setHideHeaders(true);
 		grid.setAutoExpandColumn("label");
 		grid.setLoadMask(true);
+		grid.addPlugin(expander);
+		grid.getView().setViewConfig(new GridViewConfig(){
+			@Override
+			public String getRowStyle(ModelData model, int rowIndex,
+					ListStore<ModelData> ds) {
+				// TODO Auto-generated method stub
+				if(rowIndex % 2 == 0){
+					return "row-Style-Odd";
+				} else {
+					return "row-Style-Even";
+				}
+			}
+		});
 		
 		add(grid, new RowData(1, 1));
 		add(toolbar, new RowData(1, -1));
