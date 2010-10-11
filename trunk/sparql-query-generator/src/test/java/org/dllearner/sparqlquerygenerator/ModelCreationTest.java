bc8a29dc4c1ff535b4e51eb3bbad3497215021d4
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2010-10-11 09:43:54 +0000

    Some logging extensions for testing.

--- trunk/sparql-query-generator/src/test/java/org/dllearner/sparqlquerygenerator/ModelCreationTest.java
+++ trunk/sparql-query-generator/src/test/java/org/dllearner/sparqlquerygenerator/ModelCreationTest.java
@@ -25,6 +25,8 @@ public class ModelCreationTest {
 	private static final int RECURSION_DEPTH = 2;
 	private static final String RESOURCE = "http://dbpedia.org/resource/Leipzig";
 	
+	private static final Logger logger = Logger.getLogger(ModelCreationTest.class);
+	
 	@Test
 	public void test1(){
 		try {
@@ -37,6 +39,7 @@ public class ModelCreationTest {
 			logger.addAppender(fileAppender);
 			logger.setLevel(Level.INFO);		
 			Logger.getLogger(ModelGenerator.class).setLevel(Level.DEBUG);
+			Logger.getLogger(ModelCreationTest.class).setLevel(Level.DEBUG);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -44,22 +47,29 @@ public class ModelCreationTest {
 		
 		ModelGenerator modelGen = new ModelGenerator(SparqlEndpoint.getEndpointDBpedia());
 		
+		logger.debug("Using chunk strategy.");
 		Model model1 = modelGen.createModel(RESOURCE, ModelGenerator.Strategy.CHUNKS, RECURSION_DEPTH);
-		System.out.println(model1.size());
+		logger.debug("Got overall " + model1.size() + " triple.");
 		
+		logger.debug("Using incremental strategy.");
 		Model model2 = modelGen.createModel(RESOURCE, ModelGenerator.Strategy.INCREMENTALLY, RECURSION_DEPTH);
-		System.out.println(model2.size());
+		logger.debug("Got overall " + model2.size() + " triple.");
 		
 		Model diff = ModelFactory.createDefaultModel();
 		if(model1.size() > model2.size()){
+			logger.debug("Chunk strategy returned " + (model1.size() - model2.size()) + " more triple.");
 			diff.add(model1.difference(model2));
 		} else if(model2.size() > model1.size()){
+			logger.debug("Inremental strategy returned " + (model2.size() - model1.size()) + " more triple.");
 			diff.add(model2.difference(model1));
+		} else {
+			logger.debug("Both strategies returned the same number of triple.");
 		}
 		
+		logger.debug("Difference: ");
 		Statement st = null;
 		for(Iterator<Statement> i = diff.listStatements();i.hasNext(); st = i.next()){
-			System.out.println(st);
+			logger.debug(st);
 		}
 		assertTrue(model1.size() == model2.size());
 	}
