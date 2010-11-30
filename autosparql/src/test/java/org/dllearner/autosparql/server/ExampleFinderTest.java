57b2779b58ceb7c83a92d2c2d1111dac067a2ff7
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2010-11-30 15:02:10 +0000

    Continued autosparql.

--- autosparql/src/test/java/org/dllearner/autosparql/server/ExampleFinderTest.java
+++ autosparql/src/test/java/org/dllearner/autosparql/server/ExampleFinderTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.dllearner.autosparql.client.exception.SPARQLQueryException;
+import org.dllearner.autosparql.server.util.Endpoints;
+import org.dllearner.autosparql.server.util.SPARQLEndpointEx;
 import org.junit.Test;
 
 import org.dllearner.kb.sparql.ExtractionDBCache;
@@ -11,7 +13,7 @@ import org.dllearner.kb.sparql.SparqlEndpoint;
 
 public class ExampleFinderTest {
 	
-	private static final SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpediaLive();
+	private static final SPARQLEndpointEx endpoint = Endpoints.getDBPediaAKSWEndpoint();
 	
 	@Test
 	public void test1(){
@@ -19,11 +21,11 @@ public class ExampleFinderTest {
 			ExampleFinder f = new ExampleFinder(endpoint, new ExtractionDBCache("select-cache"), new ExtractionDBCache("construct-cache"));
 			
 			List<String> posExamples = new ArrayList<String>();
-			posExamples.add("http://dbpedia.org/resource/University_of_Leipzig");
+			posExamples.add("http://dbpedia.org/resource/Berlin");
 			
 			List<String> negExamples = new ArrayList<String>();
-			negExamples.add("http://dbpedia.org/resource/Ackermann%E2%80%93Teubner_Memorial_Award");
-			negExamples.add("http://dbpedia.org/resource/Arizona_State_University");
+//			negExamples.add("http://dbpedia.org/resource/Ackermann%E2%80%93Teubner_Memorial_Award");
+//			negExamples.add("http://dbpedia.org/resource/Arizona_State_University");
 			
 			
 			System.out.println(f.findSimilarExample(posExamples, negExamples));
