247d167b70b38e885c42a3d2c19b8177b0052023
Jens Lehmann <JensLehmann@users.noreply.github.com>
2010-12-10 07:02:41 +0000

    material for landing page

--- autosparql/src/main/java/org/dllearner/autosparql/client/view/HomeView.java
+++ autosparql/src/main/java/org/dllearner/autosparql/client/view/HomeView.java
@@ -21,6 +21,7 @@ import com.extjs.gxt.ui.client.widget.form.ComboBox;
 import com.extjs.gxt.ui.client.widget.form.ComboBox.TriggerAction;
 import com.extjs.gxt.ui.client.widget.layout.RowLayout;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Hyperlink;
 
 public class HomeView extends View {
@@ -46,7 +47,18 @@ public class HomeView extends View {
 		container.add(createEndpointSelector());
 		Hyperlink learnQueryLink = new Hyperlink("Learn Query", HistoryTokens.QUERY);
 		container.add(learnQueryLink);
-		
+		HTML savedQueries = new HTML("<br /><p>TODO: box with saved queries</p>");
+		container.add(savedQueries);
+		HTML slogan = new HTML("<p>Slogan: AutoSPARQL - Queries made Easy</p>");
+		container.add(slogan);
+		HTML infos = new HTML("<p>AutoSPARQL allows you to create queries for over RDF knowledge bases with low " +
+				"effort. It can be used to find lists of things and display their properties. Watch the screencast to see how it works: " + 
+				"TODO: screencast</p>" +
+				"<p>authors: <a href=\"http://jens-lehmann.org\">Jens Lehmann</a>, " +
+				"<a href=\"http://bis.informatik.uni-leipzig.de/LorenzBuehmann\">Lorenz Bühmann</a></p>" +
+				"<p>Research Group: <a href=\"http://aksw.org/Groups/MOLE\">MOLE</a>@<a href=\"http://aksw.org\">AKSW</a>@<a href=\"http://www.zv.uni-leipzig.de/en/\">University of Leipzig</a></p>" +
+				"<p>powered by <a href=\"http://dl-learner.org\">DL-Learner</a> [LOGO]</p>");
+		container.add(infos);
 	}
 	
 	private ComboBox<Endpoint> createEndpointSelector(){
