077c4352cb9085f85a3674b00aac7603b6d5cab4
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2011-01-11 14:38:13 +0000

    Continued optimized NBR algorithm.

--- autosparql/src/main/java/org/dllearner/autosparql/server/PostLGG.java
+++ autosparql/src/main/java/org/dllearner/autosparql/server/PostLGG.java
@@ -4,24 +4,70 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.dllearner.sparqlquerygenerator.datastructures.QueryTree;
+import org.dllearner.sparqlquerygenerator.datastructures.impl.QueryTreeImpl;
 
 public class PostLGG<N> {
 	
+	private static final Logger logger = Logger.getLogger(PostLGG.class);
+	
 	public void simplifyTree(QueryTree<N> tree, List<QueryTree<N>> negTrees){
+		if(logger.isInfoEnabled()){
+			logger.info("LGG:\n" + tree.getStringRepresentation());
+			for(QueryTree<N> negTree : negTrees){
+				logger.info("Neg tree:\n" + negTree.getStringRepresentation());
+			}
+		}
+		
 		List<Object> path;
-		System.out.println("Direct children");
-		for(QueryTree<N> child : tree.getChildren()){
-			path = getPathFromRootToNode(child);
-			System.out.println(path);
-			System.out.println(existsPathInEveryTree(path, negTrees));
-		}
-		System.out.println("Leaf nodes");
+		boolean pathExists;
 		for(QueryTree<N> leaf : tree.getLeafs()){
+			pathExists = false;
 			path = getPathFromRootToNode(leaf);
-			System.out.println(path);
-			System.out.println(existsPathInEveryTree(path, negTrees));
+			if(logger.isInfoEnabled()){
+				logger.info("Path: " + path);
+			}
+			if(leaf.getParent().getUserObject().equals("?")){
+				pathExists = true;
+				for(QueryTree<N> negTree : negTrees){
+					if(!pathExists(leaf, new ArrayList<Object>(path), negTree)){
+						pathExists = false;
+						break;
+					}
+				}
+			}
+			if(logger.isInfoEnabled()){
+				logger.info("Exists: " + pathExists);
+			}
+			if(pathExists){
+				leaf.getParent().removeChild((QueryTreeImpl<N>) leaf);
+			}
 		}
+		if(logger.isInfoEnabled()){
+			logger.info("Pruned tree: " + tree.getStringRepresentation());
+		}
+	}
+	
+	private boolean pathExists(QueryTree<N> leaf, List<Object> path, QueryTree<N> tree){
+		List<QueryTree<N>> negLeaves;
+		Object lastEdge = path.remove(path.size()-1);
+		for(QueryTree<N> node : getNodesByPath(tree, path)){
+			negLeaves = node.getChildren(lastEdge);
+			if(negLeaves.isEmpty()){
+				break;
+			} else {
+				if(leaf.getUserObject().equals("?")){
+					return true;
+				}
+				for(QueryTree<N> negLeaf : negLeaves){
+					if(negLeaf.getUserObject().equals(leaf.getUserObject())){
+						return true;
+					}
+				}
+			}
+		}
+		return false;
 	}
 	
 	private List<Object> getPathFromRootToNode(QueryTree<N> node){
@@ -33,6 +79,23 @@ public class PostLGG<N> {
 		}
 		Collections.reverse(path);
 		return path;
+	}
+	
+	private List<QueryTree<N>> getNodesByPath(QueryTree<N> tree, List<Object> path){
+		if(path.isEmpty()){
+			return Collections.singletonList(tree);
+		}
+		List<QueryTree<N>> nodes = new ArrayList<QueryTree<N>>();
+		Object edge = path.remove(0);
+		for(QueryTree<N> child : tree.getChildren(edge)){
+			if(path.isEmpty()){
+				nodes.add(child);
+			} else {
+				nodes.addAll(getNodesByPath(child, path));
+			}
+		}
+		
+		return nodes;
 	}
 	
 	private boolean existsPathInEveryTree(List<Object> path, List<QueryTree<N>> trees){
