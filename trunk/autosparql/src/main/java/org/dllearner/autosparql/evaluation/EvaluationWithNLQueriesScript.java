6be87c258996c67cacae70a26622fc57ceaba886
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2011-02-23 17:17:08 +0000

    Added script class for evaluating a set of questions. Added files with the questions and the cleaned file.

---
+++ trunk/autosparql/src/main/java/org/dllearner/autosparql/evaluation/EvaluationWithNLQueriesScript.java
@@ -0,0 +1,313 @@
+package org.dllearner.autosparql.evaluation;
+
+import info.bliki.api.Page;
+import info.bliki.api.XMLPagesParser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.CommonsHttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.params.ModifiableSolrParams;
+import org.dllearner.autosparql.client.exception.SPARQLQueryException;
+import org.dllearner.autosparql.server.ExampleFinder;
+import org.dllearner.autosparql.server.Generalisation;
+import org.dllearner.autosparql.server.exception.TimeOutException;
+import org.dllearner.autosparql.server.util.SPARQLEndpointEx;
+import org.dllearner.kb.sparql.ExtractionDBCache;
+import org.dllearner.kb.sparql.SparqlEndpoint;
+import org.dllearner.kb.sparql.SparqlQuery;
+import org.dllearner.sparqlquerygenerator.operations.lgg.LGGGeneratorImpl;
+import org.dllearner.sparqlquerygenerator.util.ExactMatchFilter;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import com.hp.hpl.jena.query.ResultSet;
+
+public class EvaluationWithNLQueriesScript {
+	private static Logger logger = Logger.getLogger(EvaluationWithNLQueriesScript.class);
+	
+	private static final boolean USE_SYNONYMS = false;
+	private static final String SOLR_SERVER_URL = "http://139.18.2.164:8983/solr/dbpediaCore/";
+	private static final String QUERY_ANSWERS_FILE_PATH = "evaluation/dbpedia-train_cleaned.xml";
+	private static final SparqlEndpoint ENDPOINT = SparqlEndpoint.getEndpointDBpediaLiveAKSW();
+	
+	private static final int NR_OF_POS_START_EXAMPLES_COUNT = 3;
+	private static final int NR_OF_NEG_START_EXAMPLES_COUNT = 3;
+	
+	private static final int TOP_K = 10;
+	
+	
+	private Map<String, String> question2query = new Hashtable<String, String>();
+	private SortedMap<String, Set<String>> question2Answers = new TreeMap<String, Set<String>>();
+	
+	private SolrServer server;
+	
+	private ExtractionDBCache selectCache = new ExtractionDBCache("select-cache");
+	private ExtractionDBCache constructCache = new ExtractionDBCache("construct-cache");
+	
+	private ExampleFinder exFinder;
+	
+	
+	public EvaluationWithNLQueriesScript(){
+		try {
+			server = new CommonsHttpSolrServer(SOLR_SERVER_URL);
+			
+			//predicate filters used when sending sparql query for model creation
+			List<String> predicateFilters = new ArrayList<String>();
+			predicateFilters.add("http://dbpedia.org/ontology/wikiPageWikiLink");
+			predicateFilters.add("http://dbpedia.org/property/wikiPageUsesTemplate");
+			exFinder = new ExampleFinder(new SPARQLEndpointEx(
+					new SparqlEndpoint(new URL("http://lod.openlinksw.com/sparql"), 
+							Collections.singletonList("http://dbpedia.org"), Collections.<String>emptyList()), null, null, predicateFilters), selectCache, constructCache);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+		readQueries();
+	}
+	
+	private void readQueries(){
+		logger.info("Reading file containing queries and answers...");
+		try {
+			File file = new File(QUERY_ANSWERS_FILE_PATH);
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(file);
+			doc.getDocumentElement().normalize();
+			NodeList questionNodes = doc.getElementsByTagName("question");
+			String question;
+			String query;
+			Set<String> answers;
+			for(int i = 0; i < questionNodes.getLength(); i++){
+				Element questionNode = (Element) questionNodes.item(i);
+				//Read question
+				question = ((Element)questionNode.getElementsByTagName("string").item(0)).getChildNodes().item(0).getNodeValue().trim();
+				//Read SPARQL query
+				query = ((Element)questionNode.getElementsByTagName("query").item(0)).getChildNodes().item(0).getNodeValue().trim();
+				//Read answers
+				answers = new HashSet<String>();
+				NodeList aswersNodes = questionNode.getElementsByTagName("answer");
+				for(int j = 0; j < aswersNodes.getLength(); j++){
+					Element answerNode = (Element) aswersNodes.item(j);
+					answers.add(((Element)answerNode.getElementsByTagName("uri").item(0)).getChildNodes().item(0).getNodeValue().trim());
+				}
+				
+				question2query.put(question, query);
+				question2Answers.put(question, answers);
+				
+			}
+		} catch (DOMException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		logger.info("Done.");
+	}
+	
+	private Set<String> getResourcesByNLQuery(String query){
+		logger.info("Getting Top " + TOP_K + " resources related to question with Solr...");
+		Set<String> resources = new HashSet<String>();
+		QueryResponse response;
+		try {
+			ModifiableSolrParams params = new ModifiableSolrParams();
+			params.set("q", query);
+			params.set("rows", TOP_K);
+			response = server.query(params);
+			for(SolrDocument d : response.getResults()){
+				resources.add((String) d.get("id"));
+			}
+			logger.info("Got " + resources.size() + " resources in " + response.getElapsedTime() + "ms.");
+			logger.info(resources);
+		} catch (SolrServerException e) {
+			e.printStackTrace();
+		}
+		return resources;
+	}
+	
+	private List<String> getResourcesByWikipedia(String query) {
+		logger.info("Getting Top " + TOP_K + " resources related to question with Wikipedia...");
+		long startTime = System.currentTimeMillis();
+		List<String> resources = new ArrayList<String>();
+		try {
+			String[] words = query.split(" ");
+			String modifiedQuery = "";
+			for(int i = 0; i < words.length-1; i++){
+				modifiedQuery += words[i] + "+";
+			}
+			modifiedQuery += words[words.length-1];
+			URL url = new URL("http://en.wikipedia.org/w/api.php?action=query&list=search&format=xml&srsearch=" + modifiedQuery + "&srlimit="+TOP_K);
+			URLConnection conn = url.openConnection ();
+			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			StringBuffer sb = new StringBuffer();
+			String line;
+			while ((line = rd.readLine()) != null) {
+				sb.append(line);
+			}
+			rd.close();
+			XMLPagesParser parser = new XMLPagesParser(sb.toString());
+			parser.parse();
+			String title;
+			for (Page page : parser.getPagesList()) {
+				resources.add("http://dbpedia.org/resource/" + page.getTitle().replace(" ", "_"));
+			}
+			logger.info("Got " + resources.size() + " resources in " + (System.currentTimeMillis()-startTime) + "ms.");
+			logger.info(resources);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+		return resources;
+	}
+	
+	private Set<String> getResourcesBySPARQLQuery(String query){
+		logger.info("Sending query...");
+		long startTime = System.currentTimeMillis();
+		Set<String> resources = new HashSet<String>();
+		ResultSet rs = SparqlQuery.convertJSONtoResultSet(selectCache.executeSelectQuery(ENDPOINT, query));
+		while(rs.hasNext()){
+			resources.add(rs.nextSolution().get("x0").asResource().getURI());
+		}
+		logger.info("Done in " + (System.currentTimeMillis()-startTime) + "ms");
+		return resources;
+	}
+	
+	public void evaluate(){
+		String targetQuery;
+		Set<String> answers;
+		List<String> examples;
+		Set<String> relatedResources;
+		int i = 1;
+		for(String question : question2Answers.keySet()){
+			logger.info(getNewQuestionString(i++, question));
+			try {
+				logger.info("Evaluating question \"" + question + "\"...");
+				targetQuery = question2query.get(question);
+				logger.info("Target query: \n" + targetQuery);
+				answers = question2Answers.get(question);
+				logger.info("Answers (" + answers.size() + "): " + answers);
+				examples = getResourcesByWikipedia(question);
+				relatedResources = getResourcesByNLQuery(question.substring(0, question.length()-1));
+				
+				exFinder.setObjectFilter(new ExactMatchFilter(relatedResources));
+				
+				//select some positive example and negative examples
+				List<String> posExamples = new ArrayList<String>();
+				List<String> negExamples = new ArrayList<String>();
+				for(String ex : examples){
+					if(answers.contains(ex)){
+						if(posExamples.size() < NR_OF_POS_START_EXAMPLES_COUNT){
+							posExamples.add(ex);
+						}
+					} else {
+						if(negExamples.size() < NR_OF_NEG_START_EXAMPLES_COUNT){
+							negExamples.add(ex);
+						}
+					}
+				}
+				if(posExamples.isEmpty()){
+					logger.warn("Current search returned no positive example in the Top " + TOP_K + ".\n" +
+							"Skipping query...");
+					continue;
+				}
+				logger.info("Starting positive example(s) (#" +posExamples.size() + "): " + posExamples);
+				logger.info("Starting negative example(s) (#" +negExamples.size() + "): " + negExamples);
+				
+				//start learning
+				Set<String> learnedResources;
+				do {
+					// compute new similiar example
+					logger.info("Computing similiar example...");
+					long startTime = System.currentTimeMillis();
+					String example = exFinder.findSimilarExample(posExamples,
+							negExamples).getURI();
+					logger.info("Computed similiar example \"" + example
+							+ "\" in " + (System.currentTimeMillis() - startTime)
+							+ "ms");
+
+					// print learned query up to here
+					String learnedQuery = exFinder.getCurrentQuery();
+					logger.info("Learned SPARQL query: \n" + learnedQuery);
+					learnedQuery = "SELECT DISTINCT " + learnedQuery.substring(7);
+					learnedResources = getResourcesBySPARQLQuery(learnedQuery);
+					logger.info("Number of resources in learned query: "
+							+ learnedResources.size());
+					if (answers.contains(example)) {
+						posExamples.add(example);
+					} else {
+						negExamples.add(example);
+					}
+				} while (answers.equals(learnedResources));
+				logger.info("Learned successfully query for question \""+ question + "\".");
+			} catch (TimeOutException e) {
+				e.printStackTrace();
+			} catch (SPARQLQueryException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private String getNewQuestionString(int i, String question){
+		String s = "*****************************************************\n" +
+				   "*                  " + i + ". QUESTION                      *\n       " +
+				   question + 
+				   "\n*****************************************************\n";
+		return s;
+	}
+
+	/**
+	 * @param args
+	 * @throws SPARQLQueryException 
+	 * @throws TimeOutException 
+	 * @throws SolrServerException 
+	 * @throws ParserConfigurationException 
+	 * @throws IOException 
+	 * @throws SAXException 
+	 */
+	public static void main(String[] args) throws TimeOutException, SPARQLQueryException, SolrServerException, ParserConfigurationException, SAXException, IOException {
+		Logger.getLogger(Generalisation.class).setLevel(Level.OFF);
+		Logger.getLogger(LGGGeneratorImpl.class).setLevel(Level.OFF);
+		Logger.getRootLogger().removeAllAppenders();
+		ConsoleAppender appender = new ConsoleAppender(new PatternLayout("%m%n"));
+		Logger.getRootLogger().addAppender(appender);
+		new EvaluationWithNLQueriesScript().evaluate();
+	}
+	
+	
+
+}
