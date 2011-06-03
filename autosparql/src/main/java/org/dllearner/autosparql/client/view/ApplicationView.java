c198d8812c5f3a0cf17ab53af28c0df46f422f87
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2011-06-03 09:48:46 +0000

    Small changes in UI.

--- autosparql/src/main/java/org/dllearner/autosparql/client/view/ApplicationView.java
+++ autosparql/src/main/java/org/dllearner/autosparql/client/view/ApplicationView.java
@@ -1,25 +1,28 @@
 package org.dllearner.autosparql.client.view;
 
 import org.dllearner.autosparql.client.AppEvents;
+import org.dllearner.autosparql.client.HistoryTokens;
 
+import com.extjs.gxt.charts.client.model.charts.dots.Anchor;
 import com.extjs.gxt.ui.client.Registry;
 import com.extjs.gxt.ui.client.Style.LayoutRegion;
 import com.extjs.gxt.ui.client.Style.Orientation;
 import com.extjs.gxt.ui.client.mvc.AppEvent;
 import com.extjs.gxt.ui.client.mvc.Controller;
 import com.extjs.gxt.ui.client.mvc.View;
+import com.extjs.gxt.ui.client.util.IconHelper;
 import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.HtmlContainer;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
-import com.extjs.gxt.ui.client.widget.Text;
 import com.extjs.gxt.ui.client.widget.Viewport;
-import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.button.IconButton;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.extjs.gxt.ui.client.widget.layout.FitLayout;
-import com.extjs.gxt.ui.client.widget.layout.FlowLayout;
 import com.extjs.gxt.ui.client.widget.layout.RowData;
 import com.extjs.gxt.ui.client.widget.layout.RowLayout;
+import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -37,6 +40,8 @@ public class ApplicationView extends View {
 	  
 	  private String endpoint;
 	  private String query;
+	  
+	  private Button homeButton;
 
 	  public ApplicationView(Controller controller) {
 	    super(controller);
@@ -70,7 +75,7 @@ public class ApplicationView extends View {
 
 	private void createNorth() {
 		north = new LayoutContainer(new RowLayout(
-				Orientation.VERTICAL));
+				Orientation.HORIZONTAL));
 		StringBuffer sb = new StringBuffer();
 		sb.append("<span id=demo-header-logo></div><div id=demo-header-title>AutoSPARQL</div>");
 		sb.append("<div id=demo-header-title>looks for</div>");
@@ -95,7 +100,12 @@ public class ApplicationView extends View {
 //		LayoutContainer c1 = new LayoutContainer(new RowLayout(Orientation.HORIZONTAL));
 //		c1.add(headerPanel, new RowData(-1, 1));c1.add(tF, new RowData(1, 1));
 //		c.add(c1, new RowData(1, 1));
-		north.add(headerPanel);
+		north.add(headerPanel, new RowData(1, 1));
+		
+		Hyperlink li = new Hyperlink("Home", HistoryTokens.HOME);
+//		li.addStyleName(".homeLink");
+//		north.add(li, new RowData(-1, -1));
+
 		Registry.register("View", this);
 
 		BorderLayoutData data = new BorderLayoutData(LayoutRegion.NORTH, 50);
