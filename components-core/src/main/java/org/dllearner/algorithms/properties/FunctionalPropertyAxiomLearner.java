7335f4bc0c97d29c96e4d69d30412b8e4aaa66ae
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2011-07-27 09:22:02 +0000

    Started new learning algorithms for property axioms.

---
+++ components-core/src/main/java/org/dllearner/algorithms/properties/FunctionalPropertyAxiomLearner.java
@@ -0,0 +1,53 @@
+package org.dllearner.algorithms.properties;
+
+import java.util.List;
+
+import org.dllearner.core.AxiomLearningAlgorithm;
+import org.dllearner.core.Component;
+import org.dllearner.core.ComponentInitException;
+import org.dllearner.core.configurators.Configurator;
+import org.dllearner.core.owl.Axiom;
+import org.dllearner.kb.SparqlEndpointKS;
+
+public class FunctionalPropertyAxiomLearner extends Component implements AxiomLearningAlgorithm {
+	
+	@org.dllearner.ConfigOption(name="propertyToDescribe", description="property of which a axiom should be learned")
+	private String propertyToDescribe;
+	
+	public String getPropertyToDescribe() {
+		return propertyToDescribe;
+	}
+
+	public void setPropertyToDescribe(String propertyToDescribe) {
+		this.propertyToDescribe = propertyToDescribe;
+	}
+
+	public FunctionalPropertyAxiomLearner(SparqlEndpointKS ks){
+		
+	}
+	
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Axiom> getCurrentlyBestAxioms(int nrOfAxioms) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Configurator getConfigurator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init() throws ComponentInitException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
