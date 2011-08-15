877f09299e114d6639bbeaea0fa85b0a2a8367d7
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2011-08-15 09:05:43 +0000

    Outsourced autosparql module.

--- autosparql/src/main/java/org/dllearner/autosparql/client/HistoryTokens.java
+++
@@ -1,9 +0,0 @@
-package org.dllearner.autosparql.client;
-
-public class HistoryTokens {
-	
-	public static final String HOME = "home";
-	public static final String QUERY = "query";
-	public static final String LOADQUERY = "loadquery";
-
-}
