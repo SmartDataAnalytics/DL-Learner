98d7b0bff419b6b10c71ec0b7a2ec53e18f86b8a
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2011-08-21 20:31:32 +0000

    Added maven-license-plugin to automatically create/update license headers (USAGE: mvn license:format -Dyear=${currentYear}   ). Added header file. Updated license headers for release.

--- components-core/src/main/java/org/dllearner/reasoning/OWLAPIReasoner.java
+++ components-core/src/main/java/org/dllearner/reasoning/OWLAPIReasoner.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2008, Jens Lehmann
+ * Copyright (C) 2007-2011, Jens Lehmann
  *
  * This file is part of DL-Learner.
  *
@@ -15,8 +15,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
+
 package org.dllearner.reasoning;
 
 import com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory;
--- components-core/src/main/java/org/dllearner/refinementoperators/ELDown.java
+++ components-core/src/main/java/org/dllearner/refinementoperators/ELDown.java
@@ -1,8 +1,8 @@
 /**
- * Copyright (C) 2007-2008, Jens Lehmann
+ * Copyright (C) 2007-2011, Jens Lehmann
  *
  * This file is part of DL-Learner.
- * 
+ *
  * DL-Learner is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
@@ -15,8 +15,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
+
 package org.dllearner.refinementoperators;
 
 import java.util.Collection;
