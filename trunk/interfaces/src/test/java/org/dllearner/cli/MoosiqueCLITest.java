4c504516da8a52cc2237b13afb51240b5b0b919a
shadowtm <shadowtm@af9c125c-f337-0410-9a96-b85bd07cbc14>
2011-08-27 21:13:37 +0000

    Made the OWLAPIReasoner optional on the Fast Instance Checker - and added MoosiqueCLITest for testing purposes - it's not working yet though.

---
+++ trunk/interfaces/src/test/java/org/dllearner/cli/MoosiqueCLITest.java
@@ -0,0 +1,68 @@
+package org.dllearner.cli;
+
+import junit.framework.Assert;
+import org.dllearner.algorithms.celoe.CELOE;
+import org.dllearner.configuration.IConfiguration;
+import org.dllearner.configuration.spring.ApplicationContextBuilder;
+import org.dllearner.configuration.spring.DefaultApplicationContextBuilder;
+import org.dllearner.confparser3.ConfParserConfiguration;
+import org.dllearner.learningproblems.PosNegLPStandard;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chris
+ * Date: 8/23/11
+ * Time: 5:21 AM
+ * <p/>
+ * Test for the CLI Class
+ */
+public class MoosiqueCLITest {
+
+    public ApplicationContext createApplicationContext(Resource confFile) throws IOException {
+        ApplicationContextBuilder builder = new DefaultApplicationContextBuilder();
+
+        List<String> componentKeyPrefixes = new ArrayList<String>();
+        componentKeyPrefixes.add("component:");
+        componentKeyPrefixes.add(":");
+
+        //Spring Config Files
+        List<Resource> springConfigResources = new ArrayList<Resource>();
+
+        //DL-Learner Configuration Object
+        IConfiguration configuration = new ConfParserConfiguration(confFile);
+        //Build The Application Context
+
+        ApplicationContext context = builder.buildApplicationContext(configuration, componentKeyPrefixes, springConfigResources);
+        return context;
+    }
+
+
+    @Test
+    public void testMoosiqueConf() throws Exception {
+        Resource confFile = new FileSystemResource("../examples/sparql/moosique_new.conf");
+        ApplicationContext context = createApplicationContext(confFile);
+        validateContext(context);
+    }
+
+
+
+    private void validateContext(ApplicationContext context) {
+        PosNegLPStandard lp = context.getBean("lp", PosNegLPStandard.class);
+        Assert.assertTrue(lp.getPositiveExamples().size() == 3);
+        Assert.assertTrue(lp.getNegativeExamples().size() == 4);
+        Assert.assertNotNull(lp.getReasoner());
+
+        CELOE algorithm = context.getBean("alg", CELOE.class);
+        Assert.assertNotNull(algorithm);
+
+        algorithm.start();
+    }
+}
