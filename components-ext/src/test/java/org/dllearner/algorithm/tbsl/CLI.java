be2350a541acec73e1c226996c130dd6ad5be1a5
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2011-09-13 18:40:45 +0000

    Added simple CLI for TBSL.

---
+++ components-ext/src/test/java/org/dllearner/algorithm/tbsl/CLI.java
@@ -0,0 +1,62 @@
+package org.dllearner.algorithm.tbsl;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Collections;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.dllearner.algorithm.tbsl.learning.NoTemplateFoundException;
+import org.dllearner.algorithm.tbsl.learning.SPARQLTemplateBasedLearner;
+import org.dllearner.algorithm.tbsl.templator.Templator;
+import org.dllearner.kb.sparql.SparqlEndpoint;
+import org.ini4j.InvalidFileFormatException;
+
+public class CLI {
+	
+
+	    public static void main(String[] args) throws InvalidFileFormatException, FileNotFoundException, IOException {
+	    	
+//	    	Logger.getLogger(SPARQLTemplateBasedLearner.class).setLevel(Level.OFF);
+	    	
+			SPARQLTemplateBasedLearner learner = new SPARQLTemplateBasedLearner();
+			SparqlEndpoint endpoint = new SparqlEndpoint(new URL("http://live.dbpedia.org/sparql"), 
+					Collections.<String>singletonList(""), Collections.<String>emptyList());
+	    	
+	        System.out.println("======= TBSL v0.1 =============");       
+	        System.out.println("\nType ':q' to quit.");
+
+	        while (true) {
+	            String question = getStringFromUser("Question > ").trim(); 
+	            
+	            if (question.equals(":q")) {
+	                System.exit(0);
+	            }
+	         
+	            learner.setEndpoint(endpoint);
+				learner.setQuestion(question);
+				try {
+					learner.learnSPARQLQueries();
+					System.out.println(learner.getBestSPARQLQuery());
+				} catch (NoTemplateFoundException e) {
+					System.out.println("Sorry, could not generate a template.");
+				}
+	            
+	        }
+	    }
+
+	    public static String getStringFromUser(String msg) {
+	        String str = "";
+	        try {
+	        	System.out.println("\n===========================================\n");
+	            System.out.print(msg);
+	            str = new BufferedReader(new InputStreamReader(System.in)).readLine();
+	        } catch (IOException e) {
+	        }
+	        return str;
+	    }
+
+}
