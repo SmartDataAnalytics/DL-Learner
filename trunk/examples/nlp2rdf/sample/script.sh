1d2cf2fd94c528404290fab747631163fa52c41e
kurzum <kurzum@af9c125c-f337-0410-9a96-b85bd07cbc14>
2011-10-20 18:58:19 +0000

    fixed sample

--- trunk/examples/nlp2rdf/sample/script.sh
+++ trunk/examples/nlp2rdf/sample/script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl http://nlp2rdf.lod2.eu/demo/NIFStanfordCore --data-urlencode input@sentences1.txt -d input-type=text -d nif=true  > data1.owl
-curl http://nlp2rdf.lod2.eu/demo/NIFStanfordCore --data-urlencode input@sentences2.txt -d input-type=text -d nif=true  > data2.owl
-#curl http://localhost:8080/demo/NIFStanfordCore --data-urlencode input@sentences.txt -d input-type=text -d nif=true  > data.owl
-#curl http://nlp2rdf.lod2.eu/demo/NIFStanfordCore --data-urlencode input@sentences.txt -d input-type=text -d nif=true -d format=n3 > data.ttl
-#curl http://nlp2rdf.lod2.eu/demo/NIFStanfordCore --data-urlencode input@sentences.txt -d input-type=text -d nif=true -d format=ntriples > data.nt
+curl http://nlp2rdf.lod2.eu/demo/NIFStanfordCore --data-urlencode input@sentences1.txt -d input-type=text -d nif=true  > /tmp/data.owl
+rdfcat -x /tmp/data.owl import.owl > data1.owl
+
+curl http://nlp2rdf.lod2.eu/demo/NIFStanfordCore --data-urlencode input@sentences2.txt -d input-type=text -d nif=true  > /tmp/data.owl
+rdfcat -x /tmp/data.owl import.owl > data2.owl
