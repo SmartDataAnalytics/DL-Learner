7caff7d6d0db02944b7c8d9a8910839bb869be5d
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2011-11-02 22:28:33 +0000

    Added simple type restriction to ignore invalid queries.

--- trunk/components-ext/src/main/java/org/dllearner/algorithm/tbsl/sparql/Allocation.java
+++ trunk/components-ext/src/main/java/org/dllearner/algorithm/tbsl/sparql/Allocation.java
@@ -49,6 +49,36 @@ public class Allocation {
 	public String toString() {
 		return uri + "(similarity: " + similarity + "; prominence: " + inDegree + ")";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(score);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((uri == null) ? 0 : uri.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Allocation other = (Allocation) obj;
+		if (Double.doubleToLongBits(score) != Double.doubleToLongBits(other.score))
+			return false;
+		if (uri == null) {
+			if (other.uri != null)
+				return false;
+		} else if (!uri.equals(other.uri))
+			return false;
+		return true;
+	}
 	
 	
 	
