1103ed16374bb388adf4e4ff9909d964d69a2bc4
kurzum <kurzum@af9c125c-f337-0410-9a96-b85bd07cbc14>
2011-11-21 14:43:55 +0000

    added sparql capabilities to owlfile

--- trunk/components-core/src/main/java/org/dllearner/kb/OWLFile.java
+++ trunk/components-core/src/main/java/org/dllearner/kb/OWLFile.java
@@ -25,6 +25,7 @@ import java.net.URI;
 import java.net.URL;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.dllearner.core.AbstractKnowledgeSource;
@@ -32,130 +33,143 @@ import org.dllearner.core.ComponentAnn;
 import org.dllearner.core.ComponentInitException;
 import org.dllearner.core.OntologyFormat;
 import org.dllearner.core.OntologyFormatUnsupportedException;
+import org.dllearner.core.config.ListStringEditor;
 import org.dllearner.core.options.ConfigEntry;
 import org.dllearner.core.options.ConfigOption;
 import org.dllearner.core.options.InvalidConfigOptionValueException;
 import org.dllearner.core.options.URLConfigOption;
 import org.dllearner.core.owl.KB;
 import org.dllearner.reasoning.OWLAPIDIGConverter;
+import org.dllearner.utilities.URLencodeUTF8;
 
 /**
  * @author Jens Lehmann
- * 
+ * @author Sebastian Hellmann
+ *         <p/>
+ *         SH: I added SPARQL capabilities.  Either URL is set directly or the basedir and filename is set or the URL and the SPARQL query is set
  */
 @ComponentAnn(name = "OWL File", shortName = "owlfile", version = 0.9)
 public class OWLFile extends AbstractKnowledgeSource {
 
-	private static Logger logger = Logger.getLogger(OWLFile.class);
-	
-	// TODO: turn this into a config option
-	private URL url;
+    private static Logger logger = Logger.getLogger(OWLFile.class);
+
+    // TODO: turn this into a config option
+    private URL url;
     private String fileName;
-	private String baseDir;
-//	private URL url;
-//	private OWLFileConfigurator configurator ;
-//	@Override
-//	public OWLFileConfigurator getConfigurator(){
-//		return configurator;
-//	}
+    private String baseDir;
 
-	public static String getName() {
-		return "OWL file";
-	}
-	
-	public OWLFile(){
+    private String sparql = null;
+    private List<String> defaultGraphURIs = new LinkedList<String>();
+    private List<String> namedGraphURIs = new LinkedList<String>();
 
-	}
-	
-	public OWLFile(URL url) {
-		this.url = url;
-	}
+    public static String getName() {
+        return "OWL file";
+    }
 
-	public OWLFile(String filename) {
-		try {
-			url = new File(filename).toURI().toURL();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static Collection<ConfigOption<?>> createConfigOptions() {
-		Collection<ConfigOption<?>> options = new LinkedList<ConfigOption<?>>();
-		URLConfigOption urlOption = new URLConfigOption("url", "URL pointing to the OWL file", null, true, true);
-		urlOption.setRefersToFile(true);
-		options.add(urlOption);
-		return options;
-	}
+    public OWLFile() {
 
-	/*
-	 * @see org.dllearner.core.Component#applyConfigEntry(org.dllearner.core.ConfigEntry)
-	 */
-	@Override
-	public <T> void applyConfigEntry(ConfigEntry<T> entry) throws InvalidConfigOptionValueException {
-		
-	}
+    }
 
-	/* (non-Javadoc)
-	 * @see org.dllearner.core.Component#init()
-	 */
-	@Override
-	public void init() throws ComponentInitException {
-		if(url == null) {
+    public OWLFile(URL url) {
+        this.url = url;
+    }
+
+    public OWLFile(String filename) {
+        try {
+            url = new File(filename).toURI().toURL();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Collection<ConfigOption<?>> createConfigOptions() {
+        Collection<ConfigOption<?>> options = new LinkedList<ConfigOption<?>>();
+        URLConfigOption urlOption = new URLConfigOption("url", "URL pointing to the OWL file", null, true, true);
+        urlOption.setRefersToFile(true);
+        options.add(urlOption);
+        return options;
+    }
+
+    /*
+      * @see org.dllearner.core.Component#applyConfigEntry(org.dllearner.core.ConfigEntry)
+      */
+    @Override
+    public <T> void applyConfigEntry(ConfigEntry<T> entry) throws InvalidConfigOptionValueException {
+
+    }
+
+    /* (non-Javadoc)
+      * @see org.dllearner.core.Component#init()
+      */
+    @Override
+    public void init() throws ComponentInitException {
+        if (sparql != null) {
+            StringBuilder sb = new StringBuilder();
+
+            //make URL
+            sb.append(url.toString());
+            sb.append("?query=").append(URLencodeUTF8.encode(sparql));
+            sb.append("&format=application%2Frdf%2Bxml");
+
+            for (String graph : defaultGraphURIs) {
+                sb.append("&default-graph-uri=").append(URLencodeUTF8.encode(graph));
+            }
+            for (String graph : namedGraphURIs) {
+                sb.append("&named-graph-uri=").append(URLencodeUTF8.encode(graph));
+            }
+            logger.info(sb.toString());
+
             try {
-                url = new URL("file://"+ baseDir + "/" + fileName);
+                url = new URL(sb.toString());
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            }
+
+        } else if (url == null) {
+            try {
+                url = new URL("file://" + baseDir + "/" + fileName);
             } catch (MalformedURLException e) {
                 throw new RuntimeException(e);
             }
         }
-		
-		/*
-			try {
-				url = new URL(configurator.getUrl());
-			} catch (MalformedURLException e) {
-				logger.error(e.getMessage());
-				//throw new InvalidConfigOptionValueException(entry.getOption(), entry.getValue(),"malformed URL " + configurator.getUrl());
-			} 
-		*/
-		
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.dllearner.core.KnowledgeSource#toDIG()
-	 */
-	@Override
-	public String toDIG(URI kbURI) {
-		// TODO: need some handling for cases where the URL was not set
-		return OWLAPIDIGConverter.getTellsString(url, OntologyFormat.RDF_XML, kbURI);
-	}
+    }
 
-	public URL getURL() {
-//		return configurator.getUrl();
-		return url;
-	}
-	
-	public void setURL(URL url) {
-		this.url = url;
-//		configurator.setUrl(url);
-	}
 
-	/* (non-Javadoc)
-	 * @see org.dllearner.core.KnowledgeSource#export(java.io.File, org.dllearner.core.OntologyFormat)
-	 */
-	@Override
-	public void export(File file, OntologyFormat format) throws OntologyFormatUnsupportedException {
-		// currently no export functions implemented, so we just throw an exception
-		throw new OntologyFormatUnsupportedException("export", format);
-	}
+    /*
+    * (non-Javadoc)
+    *
+    * @see org.dllearner.core.KnowledgeSource#toDIG()
+    */
+    @Override
+    public String toDIG(URI kbURI) {
+        // TODO: need some handling for cases where the URL was not set
+        return OWLAPIDIGConverter.getTellsString(url, OntologyFormat.RDF_XML, kbURI);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.dllearner.core.KnowledgeSource#toKB()
-	 */
-	@Override
-	public KB toKB() {
-		throw new Error("OWL -> KB conversion not implemented yet.");
-	}
+    public URL getURL() {
+        return url;
+    }
+
+    public void setURL(URL url) {
+        this.url = url;
+    }
+
+    /* (non-Javadoc)
+      * @see org.dllearner.core.KnowledgeSource#export(java.io.File, org.dllearner.core.OntologyFormat)
+      */
+    @Override
+    public void export(File file, OntologyFormat format) throws OntologyFormatUnsupportedException {
+        // currently no export functions implemented, so we just throw an exception
+        throw new OntologyFormatUnsupportedException("export", format);
+    }
+
+    /* (non-Javadoc)
+      * @see org.dllearner.core.KnowledgeSource#toKB()
+      */
+    @Override
+    public KB toKB() {
+        throw new Error("OWL -> KB conversion not implemented yet.");
+    }
 
     public String getBaseDir() {
         return baseDir;
@@ -165,13 +179,13 @@ public class OWLFile extends AbstractKnowledgeSource {
         this.baseDir = baseDir;
     }
 
-	public URL getUrl() {
-		return url;
-	}
+    public URL getUrl() {
+        return url;
+    }
 
-	public void setUrl(URL url) {
-		this.url = url;
-	}
+    public void setUrl(URL url) {
+        this.url = url;
+    }
 
     public String getFileName() {
         return fileName;
@@ -179,5 +193,29 @@ public class OWLFile extends AbstractKnowledgeSource {
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public String getSparql() {
+        return sparql;
+    }
+
+    public void setSparql(String sparql) {
+        this.sparql = sparql;
+    }
+
+    public List<String> getDefaultGraphURIs() {
+        return defaultGraphURIs;
+    }
+
+    public void setDefaultGraphURIs(List<String> defaultGraphURIs) {
+        this.defaultGraphURIs = defaultGraphURIs;
+    }
+
+    public List<String> getNamedGraphURIs() {
+        return namedGraphURIs;
+    }
+
+    public void setNamedGraphURIs(List<String> namedGraphURIs) {
+        this.namedGraphURIs = namedGraphURIs;
     }
 }
