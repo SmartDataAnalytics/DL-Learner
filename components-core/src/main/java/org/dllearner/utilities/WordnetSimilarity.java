f56efb26270a6068fd982faa71fdc37689488bd8
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2011-11-22 21:19:51 +0000

    Continued WordNet similarity measure.

--- components-core/src/main/java/org/dllearner/utilities/WordnetSimilarity.java
+++ components-core/src/main/java/org/dllearner/utilities/WordnetSimilarity.java
@@ -32,26 +32,9 @@ public class WordnetSimilarity {
 		try {
 			IndexWord iw = dict.getIndexWord(pos, s1);//dict.getMorphologicalProcessor().lookupBaseForm(pos, s)
 //			IndexWord iw = dict.getMorphologicalProcessor().lookupBaseForm(pos, s);
-			if(iw != null){
-				Synset[] synsets = iw.getSenses();
-				for(Synset s : synsets){
-					System.out.println(s);
-					PointerTarget[] targets = s.getTargets(PointerType.HYPERNYM);
-					for (PointerTarget target : targets)
-		               {
-		                  Word[] words = ((Synset) target).getWords();
-		                  for (Word word : words)
-		                  {
-		                     System.out.println(word);
-		                  }
-		               }
-				}
-//				Word[] words = synsets[0].getWords();
-//				for(Word w : words){
-//					String c = w.getLemma();
-//					System.out.println(c);
-//				}
-			}
+			getUpwardHierachy(s1, pos);
+			getUpwardHierachy(s2, pos);
+			
 			
 		} catch (JWNLException e) {
 			e.printStackTrace();
@@ -59,6 +42,86 @@ public class WordnetSimilarity {
 		
 		
 		return -1;
+	}
+	
+	private List<PointerTarget> getUpwardHierachy(PointerTarget target){
+		List<PointerTarget> hierarchy = new ArrayList<PointerTarget>();
+		try {
+			PointerTarget[] targets = target.getTargets(PointerType.HYPERNYM);
+			for (PointerTarget t : targets) {
+				hierarchy.add(t);
+				hierarchy.addAll(getUpwardHierachy(t));
+			}
+		} catch (JWNLException e) {
+			e.printStackTrace();
+		}
+		return hierarchy;
+		
+	}
+	
+//	private List<List<PointerTarget>> getUpwardHierachies(List<List<PointerTarget>> targets){
+//		List<List<PointerTarget>> hierarchies = new ArrayList<List<PointerTarget>>();
+//		try {
+//			PointerTarget[] targets = target.getTargets(PointerType.HYPERNYM);
+//			for (PointerTarget t : targets) {
+//				hierarchy.add(t);
+//				hierarchy.addAll(getUpwardHierachy(t));
+//			}
+//		} catch (JWNLException e) {
+//			e.printStackTrace();
+//		}
+//		return hierarchy;
+//		
+//	}
+	
+	private List<PointerTarget> getUpwardHierachy2(PointerTarget target){
+		List<PointerTarget> hierarchy = new ArrayList<PointerTarget>();
+		try {
+			PointerTarget[] targets = target.getTargets(PointerType.HYPERNYM);
+			for (PointerTarget t : targets) {
+				hierarchy.add(t);
+				hierarchy.addAll(getUpwardHierachy(t));
+			}
+		} catch (JWNLException e) {
+			e.printStackTrace();
+		}
+		return hierarchy;
+		
+	}
+	
+	private void getUpwardHierachy(String word, POS pos){
+		try {
+			IndexWord iw = dict.getIndexWord(pos, word);
+			for(Synset synset : iw.getSenses()){
+				for(PointerTarget t : getUpwardHierachy(synset)){
+					System.out.print(((Synset)t).getWord(0).getLemma() + "-->");
+				}
+				System.out.println();
+			}
+		} catch (JWNLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void getHypernyms(IndexWord iw){
+		try {
+			if(iw != null){
+				Synset[] synsets = iw.getSenses();
+				for(Synset s : synsets){
+					System.out.println(s);
+					PointerTarget[] targets = s.getTargets(PointerType.HYPERNYM);
+					for (PointerTarget target : targets) {
+						Word[] words = ((Synset) target).getWords();
+						for (Word word : words) {
+							System.out.println(word);
+						}
+					}
+				}
+			}
+		} catch (JWNLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
