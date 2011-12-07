17ab30ca73f47da3827027fb756188d4cd3626b9
jenslehmann <jenslehmann@af9c125c-f337-0410-9a96-b85bd07cbc14>
2011-12-07 15:07:59 +0000

    commented out one unit test and moved some examples to test directory

--- trunk/components-core/src/test/java/org/dllearner/test/junit/ConfigOptionTest.java
+++ trunk/components-core/src/test/java/org/dllearner/test/junit/ConfigOptionTest.java
@@ -38,21 +38,23 @@ public class ConfigOptionTest {
 
 	@Test
 	public void testConfigOption() {
-		List<ConfigOption> configOptions = ConfigHelper.getConfigOptions(CELOE.class);
-		assertFalse(configOptions.isEmpty());
+		// now outdated, because property editors do not need to be specified for each option
 		
-		CELOE celoe = new CELOE();
-		celoe.setMaxExecutionTimeInSeconds(10);
-		Map<ConfigOption,Object> optionValues = ConfigHelper.getConfigOptionValues(celoe);
-		boolean found = false;
-		for(Entry<ConfigOption,Object> entry : optionValues.entrySet()) {
-			System.out.println(entry.getKey() + " " + entry.getValue());
-			if(entry.getKey().name().equals("maxExecutionTimeInSeconds")) {
-				found = true;
-				assertTrue(Integer.valueOf(entry.getValue().toString())==10);
-			}
-		}
-		assertTrue(found);
+//		List<ConfigOption> configOptions = ConfigHelper.getConfigOptions(CELOE.class);
+//		assertFalse(configOptions.isEmpty());
+//		
+//		CELOE celoe = new CELOE();
+//		celoe.setMaxExecutionTimeInSeconds(10);
+//		Map<ConfigOption,Object> optionValues = ConfigHelper.getConfigOptionValues(celoe);
+//		boolean found = false;
+//		for(Entry<ConfigOption,Object> entry : optionValues.entrySet()) {
+//			System.out.println(entry.getKey() + " " + entry.getValue());
+//			if(entry.getKey().name().equals("maxExecutionTimeInSeconds")) {
+//				found = true;
+//				assertTrue(Integer.valueOf(entry.getValue().toString())==10);
+//			}
+//		}
+//		assertTrue(found);
 		
 	}
 	
