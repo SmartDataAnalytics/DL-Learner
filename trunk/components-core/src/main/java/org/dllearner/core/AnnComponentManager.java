056539246661912b6b49f5763f0b6e84cf314410
jenslehmann <jenslehmann@af9c125c-f337-0410-9a96-b85bd07cbc14>
2012-01-06 11:38:20 +0000

    adapted ELTL to new component system; fixed a bug in disjunctive ELTL

--- trunk/components-core/src/main/java/org/dllearner/core/AnnComponentManager.java
+++ trunk/components-core/src/main/java/org/dllearner/core/AnnComponentManager.java
@@ -51,6 +51,8 @@ public class AnnComponentManager {
     // (CLI, GUI, Web Service) and scripts (HTML documentation generator)
     private static List<String> componentClassNames = new ArrayList<String>  ( Arrays.asList(new String[]{
             "org.dllearner.algorithms.celoe.CELOE",
+            "org.dllearner.algorithms.el.ELLearningAlgorithm",
+            "org.dllearner.algorithms.el.ELLearningAlgorithmDisjunctive",
             "org.dllearner.algorithms.fuzzydll.FuzzyCELOE",   
             "org.dllearner.algorithms.BruteForceLearner",
             "org.dllearner.algorithms.RandomGuesser",
