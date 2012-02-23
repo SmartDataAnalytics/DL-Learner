210406057cecc4aff277214ca17cd09a75cf8ec6
sebastianwtr <sebastianwtr@af9c125c-f337-0410-9a96-b85bd07cbc14>
2012-02-23 15:16:14 +0000

    [tbsl exploration] now generates templates with a set of hypothesis

--- trunk/components-ext/src/main/java/org/dllearner/algorithm/tbsl/exploration/Sparql/Hypothesis.java
+++ trunk/components-ext/src/main/java/org/dllearner/algorithm/tbsl/exploration/Sparql/Hypothesis.java
@@ -1,9 +1,17 @@
 package org.dllearner.algorithm.tbsl.exploration.Sparql;
 
+import edu.stanford.nlp.io.EncodingPrintWriter.out;
+
 public class Hypothesis {
 private String variable;
 private String uri;
 private float rank;
+
+/**
+ * RESOURCE,PROPERTY,UNSPEC
+ */
+private String type;
+
 public String getUri() {
 	return uri;
 }
@@ -23,9 +31,27 @@ public void setRank(float rank) {
 	this.rank = rank;
 }
 
-public Hypothesis(String variable, String uri, float rank){
+public Hypothesis(String variable, String uri, String type, float rank){
 	setRank(rank);
 	setVariable(variable);
 	setUri(uri);
+	setType(type);
 }
+
+public String getType() {
+	return type;
+}
+public void setType(String type) {
+	this.type = type;
+}
+
+public void printAll(){
+	System.out.println("%%%%%%%%%%%");
+	System.out.println("Variable: "+variable);
+	System.out.println("Uri: " + uri);
+	System.out.println("Type: " + type);
+	System.out.println("Rank: "+rank);
+	System.out.println("%%%%%%%%%%%");
+}
+
 }
