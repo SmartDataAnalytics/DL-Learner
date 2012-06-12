3d35d9d954f5861572b9b1b9aefba3a215804116
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2012-06-12 13:19:03 +0000

    Some modification to deal with new template generation. Added separate index classes for object and datatype properties.

--- trunk/components-ext/src/main/java/org/dllearner/common/index/SPARQLObjectPropertiesIndex.java
+++ trunk/components-ext/src/main/java/org/dllearner/common/index/SPARQLObjectPropertiesIndex.java
@@ -4,37 +4,39 @@ import org.dllearner.kb.sparql.SparqlEndpoint;
 
 import com.hp.hpl.jena.rdf.model.Model;
 
-public class SPARQLPropertiesIndex extends SPARQLIndex{
+public class SPARQLObjectPropertiesIndex extends SPARQLPropertiesIndex{
 	
-	public SPARQLPropertiesIndex(SparqlEndpoint endpoint) {
+	public SPARQLObjectPropertiesIndex(SparqlEndpoint endpoint) {
 		super(endpoint);
 		init();
 	}
 	
-	public SPARQLPropertiesIndex(Model model) {
+	public SPARQLObjectPropertiesIndex(Model model) {
 		super(model);
 		init();
 	}
 	
-	public SPARQLPropertiesIndex(SPARQLIndex index) {
+	public SPARQLObjectPropertiesIndex(SPARQLIndex index) {
 		super(index);
 		init();
 	}
 	
 	private void init(){
-		super.queryTemplate = "SELECT DISTINCT ?uri WHERE {\n" +
+		super.queryTemplate = "SELECT ?uri WHERE {\n" +
 				"?s ?uri ?o.\n" + 
+				"?uri a owl:ObjectProperty." + 
 				"?uri <http://www.w3.org/2000/01/rdf-schema#label> ?label\n" +
 				"FILTER(REGEX(STR(?label), '%s', 'i'))}\n" +
 				"LIMIT %d OFFSET %d";
 		
 		super.queryWithLabelTemplate = "PREFIX owl:<http://www.w3.org/2002/07/owl#>  SELECT DISTINCT * WHERE {\n" +
 				"?s ?uri ?o.\n" + 
-//				"{?uri a owl:DatatypeProperty.} UNION {?uri a owl:ObjectProperty.} " + 
+				"?uri a owl:ObjectProperty." + 
 				"?uri <http://www.w3.org/2000/01/rdf-schema#label> ?label\n" +
 				"FILTER(REGEX(STR(?label), '%s', 'i'))}\n" +
 				"LIMIT %d OFFSET %d";
 	}
+	
 	
 
 }
