5ebfa406e5f40b6b3f0a302e7a3ae6ffe6b0233a
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2012-06-18 12:51:44 +0000

    Added further lexicon entries for Oxford data. Extended eval output. Added some manual mappings.

--- components-ext/src/test/java/org/dllearner/algorithm/tbsl/OxfordEvaluation.java
+++ components-ext/src/test/java/org/dllearner/algorithm/tbsl/OxfordEvaluation.java
@@ -22,11 +22,12 @@ import org.dllearner.kb.sparql.SparqlEndpoint;
 
 import com.hp.hpl.jena.query.QueryParseException;
 import com.hp.hpl.jena.sparql.engine.http.QueryExceptionHTTP;
+import com.jamonapi.Monitor;
+import com.jamonapi.MonitorFactory;
 
 public class OxfordEvaluation {
 	
-	private static final String QUERIES_FILE1 = OxfordEvaluation.class.getClassLoader().getResource("tbsl/oxford_eval_queries.txt").getPath();
-	private static final String QUERIES_FILE2 = OxfordEvaluation.class.getClassLoader().getResource("tbsl/oxford_eval_additional_queries.txt").getPath();
+	private static final String QUERIES_FILE = OxfordEvaluation.class.getClassLoader().getResource("tbsl/oxford_eval_queries.txt").getPath();
 	private static final String LOG_DIRECTORY = "log/oxford/";
 	private static final String LOG_FILE = "evaluation.txt";
 	
@@ -52,8 +53,14 @@ public class OxfordEvaluation {
 		int learnedQuestions = 0;
 		Map<String, String> question2QueryMap = new HashMap<String, String>();
 		
-		BufferedReader in = new BufferedReader(new FileReader(new File(QUERIES_FILE1)));
+		Monitor mon = MonitorFactory.getTimeMonitor("tbsl");
+		
+		BufferedReader in = new BufferedReader(new FileReader(new File(QUERIES_FILE)));
 		BufferedWriter out = new BufferedWriter(new FileWriter(new File(LOG_DIRECTORY + LOG_FILE), false));
+		BufferedWriter answerOut = new BufferedWriter(new FileWriter(new File(LOG_DIRECTORY + "questionsWithAnswer.txt"), false));
+		BufferedWriter noAnswerOut = new BufferedWriter(new FileWriter(new File(LOG_DIRECTORY + "questionsWithNoAnswer.txt"), false));
+		BufferedWriter templatesOut = new BufferedWriter(new FileWriter(new File(LOG_DIRECTORY + "questionsWithTemplate.txt"), false));
+		BufferedWriter noTemplatesOut = new BufferedWriter(new FileWriter(new File(LOG_DIRECTORY + "questionsWithNoTemplate.txt"), false));
 		
 		int questionCnt = 0;
 		int errorCnt = 0;
@@ -73,26 +80,49 @@ public class OxfordEvaluation {
 				out.write("****************************************\n");
 				out.write("QUESTION: " + question + "\n");
 				learner.setQuestion(question);
+				mon.start();
 				learner.learnSPARQLQueries();
+				mon.stop();
 				String learnedQuery = learner.getBestSPARQLQuery();
 				if(learnedQuery != null){
 					question2QueryMap.put(question, learnedQuery);
 					learnedQuestions++;
 					out.write("ANSWER FOUND: YES\n");
 					out.write(learnedQuery + "\n");
+					
+					answerOut.write("****************************************\n");
+					answerOut.write("QUESTION: " + question + "\n");
+					answerOut.write("ANSWER FOUND: YES\n");
+					answerOut.write(learnedQuery + "\n");
+					answerOut.write("TIME NEEDED: " + mon.getLastValue() + "ms\n");
+					answerOut.flush();
+					
 				} else {
 					noQueryWithNonEmptyResultSetCnt++;
 					out.write("ANSWER FOUND: NO\n");
 					out.write("REASON: NO SPARQL QUERY WITH NON-EMPTY RESULTSET FOUND\n");
 					out.write("SPARQL QUERY WITH HIGHEST SCORE TESTED:\n" + learner.getGeneratedQueries().first());
 					
+					noAnswerOut.write("****************************************\n");
+					noAnswerOut.write("QUESTION: " + question + "\n");
+					noAnswerOut.write("ANSWER FOUND: NO\n");
+					noAnswerOut.write("REASON: NO SPARQL QUERY WITH NON-EMPTY RESULTSET FOUND\n");
+					noAnswerOut.write("SPARQL QUERY WITH HIGHEST SCORE TESTED:\n" + learner.getGeneratedQueries().first() + "\n");
+					noAnswerOut.write("TIME NEEDED: " + mon.getLastValue() + "ms\n");
+					noAnswerOut.flush();
+					
 				}
+				templatesOut.write(question + "\n");
+				templatesOut.flush();
 			} catch (Exception e) {
+				mon.stop();
 				e.printStackTrace();
 				out.write("ANSWER FOUND: NO\n");
 				if(e instanceof NoTemplateFoundException){
 					noTemplateFoundCnt++;
 					out.write("REASON: NO TEMPLATE FOUND");
+					noTemplatesOut.write(question + "\n");
+					noTemplatesOut.flush();
 				} else {
 					errorCnt++;
 					out.write("REASON: ERROR OCCURED (" + e.getClass() + ")\n");
@@ -100,7 +130,9 @@ public class OxfordEvaluation {
 						out.write("\nLast tested SPARQL query: " + learner.getCurrentlyExecutedQuery());
 					}
 				}
+				
 			} catch (Error e){
+				mon.stop();
 				e.printStackTrace();
 				out.write("ANSWER FOUND: NO\n");
 				errorCnt++;
@@ -111,6 +143,10 @@ public class OxfordEvaluation {
 		}
 		out.write("\n\n###################SUMMARY################\n");
 		out.write("Questions tested:\t" + questionCnt + "\n");
+		out.write("Overall time:\t" + mon.getTotal() + "ms\n");
+		out.write("Avg. time per question:\t" + mon.getAvg() + "ms\n");
+		out.write("Longest time:\t" + mon.getMax() + "ms\n");
+		out.write("Shortest time:\t" + mon.getMin() + "ms\n");
 		out.write("Questions with answer:\t" + learnedQuestions + "\n");
 		out.write("Questions with no answer (and no error):\t" + noQueryWithNonEmptyResultSetCnt + "\n");
 		out.write("Questions with no templates:\t" + noTemplateFoundCnt + "\n");
@@ -118,6 +154,8 @@ public class OxfordEvaluation {
 		
 		in.close();
 		out.close();
+		templatesOut.close();
+		noTemplatesOut.close();
 		
 
 	}
