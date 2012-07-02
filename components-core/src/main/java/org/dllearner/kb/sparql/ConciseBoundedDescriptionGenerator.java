0921ad4999ee41606f4085ef8a6690f641834028
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2012-07-02 11:47:59 +0000

    Added caching decorator for CBD generator. Added some config options.

--- components-core/src/main/java/org/dllearner/kb/sparql/ConciseBoundedDescriptionGenerator.java
+++ components-core/src/main/java/org/dllearner/kb/sparql/ConciseBoundedDescriptionGenerator.java
@@ -1,5 +1,7 @@
 package org.dllearner.kb.sparql;
 
+import java.util.List;
+
 import com.hp.hpl.jena.rdf.model.Model;
 
 public interface ConciseBoundedDescriptionGenerator {
@@ -7,4 +9,8 @@ public interface ConciseBoundedDescriptionGenerator {
 	public Model getConciseBoundedDescription(String resourceURI);
 	
 	public Model getConciseBoundedDescription(String resourceURI, int depth);
+	
+	public void setRestrictToNamespaces(List<String> namespaces);
+	
+	public void setRecursionDepth(int maxRecursionDepth);
 }
