c791790a5c236a26118af1f079e53cfe56880b2f
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2012-07-13 08:19:48 +0000

    Updated JENA dependencies.

--- trunk/components-core/src/main/java/org/dllearner/utilities/CSVFileToArray.java
+++ trunk/components-core/src/main/java/org/dllearner/utilities/CSVFileToArray.java
@@ -25,8 +25,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-import com.ibm.icu.util.StringTokenizer;
 
 /**
  * @author Jens Lehmann
