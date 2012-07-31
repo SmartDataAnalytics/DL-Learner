0ee52bfd4476bff8cc8cda333c0189e546fb9d46
Konrad Höffner <KonradHoeffner@users.noreply.github.com>
2012-07-31 16:14:04 +0000

    SPARQLTemplateBasedLearner3Test now also saves an evaluation history and creates an html file that displays it.

--- components-ext/src/main/java/org/dllearner/algorithm/tbsl/nlp/SynchronizedStanfordPartOfSpeechTagger.java
+++ components-ext/src/main/java/org/dllearner/algorithm/tbsl/nlp/SynchronizedStanfordPartOfSpeechTagger.java
@@ -1,10 +1,6 @@
 package org.dllearner.algorithm.tbsl.nlp;
 
-public class SynchronizedStanfordPartOfSpeechTagger extends StanfordPartOfSpeechTagger {
-
-	@Override
-	public synchronized String tag(String sentence) {
-		return super.tag(sentence);
-	}
-
+public class SynchronizedStanfordPartOfSpeechTagger extends StanfordPartOfSpeechTagger
+{
+	@Override public synchronized String tag(String sentence) {return super.tag(sentence);}
 }
