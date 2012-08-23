3a07b1a4bd3ae0a232a8a97487ef92311212359e
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2012-08-23 13:19:03 +0000

    Started feature to explain score in enrichment algorithms and return pos and neg examples.

---
+++ components-core/src/main/java/org/dllearner/algorithms/properties/ObjectPropertyDomainAxiomLearner2.java
@@ -0,0 +1,221 @@
+/**
+ * Copyright (C) 2007-2011, Jens Lehmann
+ *
+ * This file is part of DL-Learner.
+ *
+ * DL-Learner is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DL-Learner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.dllearner.algorithms.properties;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.SimpleLayout;
+import org.dllearner.core.AbstractAxiomLearningAlgorithm;
+import org.dllearner.core.ComponentAnn;
+import org.dllearner.core.EvaluatedAxiom;
+import org.dllearner.core.config.ConfigOption;
+import org.dllearner.core.config.ObjectPropertyEditor;
+import org.dllearner.core.owl.Description;
+import org.dllearner.core.owl.Individual;
+import org.dllearner.core.owl.NamedClass;
+import org.dllearner.core.owl.ObjectProperty;
+import org.dllearner.core.owl.ObjectPropertyDomainAxiom;
+import org.dllearner.kb.SparqlEndpointKS;
+import org.dllearner.kb.sparql.SparqlEndpoint;
+import org.dllearner.reasoning.SPARQLReasoner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hp.hpl.jena.query.ParameterizedSparqlString;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.OWL;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
+
+@ComponentAnn(name="objectproperty domain axiom learner", shortName="opldomain", version=0.1)
+public class ObjectPropertyDomainAxiomLearner2 extends AbstractAxiomLearningAlgorithm {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ObjectPropertyDomainAxiomLearner2.class);
+	
+	private Map<Individual, SortedSet<Description>> individual2Types;
+	
+	
+	@ConfigOption(name="propertyToDescribe", description="", propertyEditorClass=ObjectPropertyEditor.class)
+	private ObjectProperty propertyToDescribe;
+	
+	public ObjectPropertyDomainAxiomLearner2(SparqlEndpointKS ks){
+		this.ks = ks;
+		super.posExamplesQueryTemplate = new ParameterizedSparqlString("SELECT DISTINCT ?s WHERE {?s a ?type}");
+		super.negExamplesQueryTemplate = new ParameterizedSparqlString("SELECT DISTINCT ?s WHERE {?s ?p ?o. FILTER NOT EXISTS{?s a ?type}}");
+	
+	}
+	
+	public ObjectProperty getPropertyToDescribe() {
+		return propertyToDescribe;
+	}
+
+	public void setPropertyToDescribe(ObjectProperty propertyToDescribe) {
+		this.propertyToDescribe = propertyToDescribe;
+//		negExamplesQueryTemplate.clearParams();
+//		posExamplesQueryTemplate.clearParams();
+	}
+	
+	@Override
+	public void start() {
+		iterativeQueryTemplate.setIri("p", propertyToDescribe.getName());
+		logger.info("Start learning...");
+		startTime = System.currentTimeMillis();
+		fetchedRows = 0;
+		currentlyBestAxioms = new ArrayList<EvaluatedAxiom>();
+		
+		if(returnOnlyNewAxioms){
+			//get existing domains
+			Description existingDomain = reasoner.getDomain(propertyToDescribe);
+			if(existingDomain != null){
+				existingAxioms.add(new ObjectPropertyDomainAxiom(propertyToDescribe, existingDomain));
+				if(reasoner.isPrepared()){
+					if(reasoner.getClassHierarchy().contains(existingDomain)){
+						for(Description sup : reasoner.getClassHierarchy().getSuperClasses(existingDomain)){
+							existingAxioms.add(new ObjectPropertyDomainAxiom(propertyToDescribe, existingDomain));
+							logger.info("Existing domain(inferred): " + sup);
+						}
+					}
+					
+				}
+			}
+		}
+		
+		runSPARQL1_0_Mode();
+		logger.info("...finished in {}ms.", (System.currentTimeMillis()-startTime));
+	}
+	
+	private void runSPARQL1_0_Mode() {
+		workingModel = ModelFactory.createDefaultModel();
+		int limit = 1000;
+		int offset = 0;
+		String baseQuery  = "CONSTRUCT {?s a ?type.} WHERE {?s <%s> ?o. ?s a ?type.} LIMIT %d OFFSET %d";
+		String query = String.format(baseQuery, propertyToDescribe.getName(), limit, offset);
+		Model newModel = executeConstructQuery(query);
+		while(!terminationCriteriaSatisfied() && newModel.size() != 0){
+			workingModel.add(newModel);
+			// get number of distinct subjects
+			query = "SELECT (COUNT(DISTINCT ?s) AS ?all) WHERE {?s a ?type.}";
+			ResultSet rs = executeSelectQuery(query, workingModel);
+			QuerySolution qs;
+			int all = 1;
+			while (rs.hasNext()) {
+				qs = rs.next();
+				all = qs.getLiteral("all").getInt();
+			}
+			
+			// get class and number of instances
+			query = "SELECT ?type (COUNT(DISTINCT ?s) AS ?cnt) WHERE {?s a ?type.} GROUP BY ?type";
+			rs = executeSelectQuery(query, workingModel);
+			
+			if (all > 0) {
+				currentlyBestAxioms.clear();
+				while(rs.hasNext()){
+					qs = rs.next();
+					Resource type = qs.get("type").asResource();
+					//omit owl:Thing as trivial domain
+					if(type.equals(OWL.Thing)){
+						continue;
+					}
+					currentlyBestAxioms.add(new EvaluatedAxiom(
+							new ObjectPropertyDomainAxiom(propertyToDescribe, new NamedClass(type.getURI())),
+							computeScore(all, qs.get("cnt").asLiteral().getInt())));
+				}
+				
+			}
+			offset += limit;
+			query = String.format(baseQuery, propertyToDescribe.getName(), limit, offset);
+			newModel = executeConstructQuery(query);
+			fillWithInference(newModel);
+		}
+	}
+	
+	private void fillWithInference(Model model){
+		Model additionalModel = ModelFactory.createDefaultModel();
+		if(reasoner.isPrepared()){
+			for(StmtIterator iter = model.listStatements(null, RDF.type, (RDFNode)null); iter.hasNext();){
+				Statement st = iter.next();
+				Description cls = new NamedClass(st.getObject().asResource().getURI());
+				if(reasoner.getClassHierarchy().contains(cls)){
+					for(Description sup : reasoner.getClassHierarchy().getSuperClasses(cls)){
+						additionalModel.add(st.getSubject(), st.getPredicate(), model.createResource(sup.toString()));
+					}
+				}
+			}
+		}
+		model.add(additionalModel);
+	}
+	
+	@Override
+	protected SortedSet<Individual> getPositiveExamples(EvaluatedAxiom evAxiom) {
+		ObjectPropertyDomainAxiom axiom = (ObjectPropertyDomainAxiom) evAxiom.getAxiom();
+		posExamplesQueryTemplate.setIri("type", axiom.getDomain().toString());
+		return super.getPositiveExamples(evAxiom);
+	}
+	
+	@Override
+	protected SortedSet<Individual> getNegativeExamples(EvaluatedAxiom evAxiom) {
+		ObjectPropertyDomainAxiom axiom = (ObjectPropertyDomainAxiom) evAxiom.getAxiom();
+		negExamplesQueryTemplate.setIri("type", axiom.getDomain().toString());
+		return super.getNegativeExamples(evAxiom);
+	}
+	
+	public static void main(String[] args) throws Exception{
+		org.apache.log4j.Logger.getRootLogger().addAppender(new ConsoleAppender(new SimpleLayout()));
+		org.apache.log4j.Logger.getRootLogger().setLevel(Level.INFO);
+		org.apache.log4j.Logger.getLogger(DataPropertyDomainAxiomLearner.class).setLevel(Level.INFO);		
+		
+		SparqlEndpointKS ks = new SparqlEndpointKS(SparqlEndpoint.getEndpointDBpediaLiveAKSW());
+		
+		SPARQLReasoner reasoner = new SPARQLReasoner(ks);
+		reasoner.prepareSubsumptionHierarchy();
+		
+		
+		ObjectPropertyDomainAxiomLearner2 l = new ObjectPropertyDomainAxiomLearner2(ks);
+		l.setReasoner(reasoner);
+		l.setPropertyToDescribe(new ObjectProperty("http://dbpedia.org/ontology/league"));
+		l.setMaxFetchedRows(20000);
+		l.setMaxExecutionTimeInSeconds(20);
+		l.addFilterNamespace("http://dbpedia.org/ontology/");
+//		l.setReturnOnlyNewAxioms(true);
+		l.init();
+		l.start();
+		
+		List<EvaluatedAxiom> axioms = l.getCurrentlyBestEvaluatedAxioms(10, 0.3);
+		System.out.println(axioms);
+		for(EvaluatedAxiom axiom : axioms){
+			printSubset(l.getPositiveExamples(axiom), 10);
+			printSubset(l.getNegativeExamples(axiom), 10);
+			l.explainScore(axiom);
+		}
+	}
+	
+}
