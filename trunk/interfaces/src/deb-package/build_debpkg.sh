5a850a31a1d4e6a59aced9cb69b1a53725cd0912
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2012-09-12 22:56:17 +0000

    Updated Debian package creation script.

--- trunk/interfaces/src/deb-package/build_debpkg.sh
+++ trunk/interfaces/src/deb-package/build_debpkg.sh
@@ -1,31 +1,30 @@
 #!/bin/sh
-MODULE_NAME=interfaces-0.5-SNAPSHOT
+
+# $1 the version passed by the maven call
+
+PACKAGE_NAME=dllearner-interfaces
+VERSION=$1
+
 echo "Building Debian package for ${MODULE_NAME}"
 echo
-rm -rf ../../target/dl-learner-interfaces-0.5
-mkdir -p ../../target/dl-learner-interfaces-0.5/usr/share/dllearner
-mkdir -p ../../target/dl-learner-interfaces-0.5/usr/share/pixmaps
-mkdir -p ../../target/dl-learner-interfaces-0.5/usr/share/applications
+
+rm -rf ../../target/deb-pkg
+mkdir -p ../../target/deb-pkg/bin
+
 # Extract the tarball to the package workspace
 #tar xfz data.tar.gz --directory ../../target/deb-pkg
+
 # copy war file to package workspace
-cp ../../target/interfaces-jar-with-dependencies.jar ../../target/dl-learner-interfaces-0.5/usr/share/dllearner
-cp dllearner-gui dllearner-gui.desktop
-cp dllearner-cli dllearner-cli.desktop
-mv -v dllearner-gui.desktop ../../target/dl-learner-interfaces-0.5/usr/share/applications
-mv -v dllearner-cli.desktop ../../target/dl-learner-interfaces-0.5/usr/share/applications
-cp ../../target/appassembler/bin/StartCLI ../../target/appassembler/bin/dllearner-CLI.sh
-cp ../../target/appassembler/bin/StartGUI ../../target/appassembler/bin/dllearner-GUI.sh
-mv ../../target/appassembler/bin/dllearner-CLI.sh ../../target/dl-learner-interfaces-0.5/usr/share/dllearner
-mv ../../target/appassembler/bin/dllearner-GUI.sh ../../target/dl-learner-interfaces-0.5/usr/share/dllearner
-cp ../../../images/logos/dllearner_small.png ../../target/dl-learner-interfaces-0.5/usr/share/pixmaps
+# remove the version in the name
+cp ../../target/dl-learner-dist/bin/cli ../../target/deb-pkg/bin/
+cp ../../target/dl-learner-dist/bin/enrichment ../../target/deb-pkg/bin/
+cp -r ../../target/dl-learner-dist/lib/ ../../target/deb-pkg/
+cp -r ../../target/original-interfaces.jar ../../target/deb-pkg/lib/interfaces.jar
+
 # Add the Debian control files
-cd dl-learner-interfaces-0.5/debian
-dch -n
-cd ..
-cp -r debian ../../../target/dl-learner-interfaces-0.5
-cd ../../../target/dl-learner-interfaces-0.5/debian
-# Build the package and sign it.
-cd ../../../target/dl-learner-interfaces-0.5
-debuild --check-dirname-level 0
+cp -r debian ../../target/deb-pkg
+
+# Build the package
+cd ../../target/deb-pkg
+debuild --check-dirname-level 0 -b
 
