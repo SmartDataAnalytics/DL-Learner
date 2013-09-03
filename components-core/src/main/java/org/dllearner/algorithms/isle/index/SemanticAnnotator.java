6a46483000832d99cabbbc172c2a3fb2e0b03317
Daniel Fleischhacker <dfleischhacker@users.noreply.github.com>
2013-09-03 14:43:56 +0000

    Refactor SemanticAnnotator with predefined constructor

--- components-core/src/main/java/org/dllearner/algorithms/isle/index/SemanticAnnotator.java
+++ components-core/src/main/java/org/dllearner/algorithms/isle/index/SemanticAnnotator.java
@@ -1,14 +1,29 @@
 package org.dllearner.algorithms.isle.index;
 
+import org.semanticweb.owlapi.model.OWLOntology;
+
 /**
  * Provides methods to annotate documents.
+ *
+ * @author Daniel Fleischhacker
  */
-public interface SemanticAnnotator {
+public abstract class SemanticAnnotator {
+    OWLOntology ontology;
+
+    /**
+     * Initialize this semantic annotator to use the entities from the provided ontology.
+     *
+     * @param ontology the ontology to use entities from
+     */
+    public SemanticAnnotator(OWLOntology ontology) {
+        this.ontology = ontology;
+    }
+
     /**
      * Processes the given document and returns the annotated version of this document.
      *
      * @param document the document to annotate
      * @return the given document extended with annotations
      */
-    AnnotatedDocument processDocument(Document document);
+    public abstract AnnotatedDocument processDocument(Document document);
 }
