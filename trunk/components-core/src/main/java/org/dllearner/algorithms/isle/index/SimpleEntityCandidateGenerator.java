17698dc20a200eb3a9973099ba3335e9abfc4d2b
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2013-09-03 15:49:06 +0000

    Added simple implementation of entity generator.

---
+++ trunk/components-core/src/main/java/org/dllearner/algorithms/isle/index/SimpleEntityCandidateGenerator.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.dllearner.algorithms.isle.index;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.dllearner.algorithms.isle.EntityCandidateGenerator;
+import org.dllearner.core.owl.Entity;
+import org.dllearner.utilities.owl.OWLAPIConverter;
+import org.semanticweb.owlapi.model.OWLEntity;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+/**
+ * @author Lorenz Buehmann
+ *
+ */
+public class SimpleEntityCandidateGenerator extends EntityCandidateGenerator{
+	
+	private Set<Entity> allEntities = new HashSet<Entity>();
+	
+	public SimpleEntityCandidateGenerator(OWLOntology ontology) {
+		super(ontology);
+		
+		Set<OWLEntity> owlEntities = new HashSet<OWLEntity>();
+		owlEntities.addAll(ontology.getClassesInSignature());
+		owlEntities.addAll(ontology.getObjectPropertiesInSignature());
+		owlEntities.addAll(ontology.getDataPropertiesInSignature());
+		
+		allEntities.addAll(OWLAPIConverter.getEntities(owlEntities));
+	}
+
+	/* (non-Javadoc)
+	 * @see org.dllearner.algorithms.isle.EntityCandidateGenerator#getCandidates(org.dllearner.algorithms.isle.index.Annotation)
+	 */
+	@Override
+	public Set<Entity> getCandidates(Annotation annotation) {
+		return allEntities;
+	}
+
+}
