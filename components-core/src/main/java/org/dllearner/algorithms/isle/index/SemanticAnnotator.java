848762a521ee13c085a54c593e8a061eef57092d
Daniel Fleischhacker <dfleischhacker@users.noreply.github.com>
2013-09-03 14:32:34 +0000

    Add method to process document to SemanticAnnotator

--- components-core/src/main/java/org/dllearner/algorithms/isle/index/SemanticAnnotator.java
+++ components-core/src/main/java/org/dllearner/algorithms/isle/index/SemanticAnnotator.java
@@ -1,10 +1,14 @@
 package org.dllearner.algorithms.isle.index;
 
 /**
- *
+ * Provides methods to annotate documents.
  */
 public interface SemanticAnnotator {
     /**
-     * Processes the given document and return
+     * Processes the given document and returns the annotated version of this document.
+     *
+     * @param document the document to annotate
+     * @return the given document extended with annotations
      */
+    AnnotatedDocument processDocument(Document document);
 }
