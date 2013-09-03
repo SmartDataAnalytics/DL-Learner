5b9ec6c155f447a3e83caff7de85a38db4ca9e0c
jenslehmann <jenslehmann@af9c125c-f337-0410-9a96-b85bd07cbc14>
2013-09-03 16:09:27 +0000

    implemented random WSD as baseline method

---
+++ trunk/components-core/src/main/java/org/dllearner/algorithms/isle/RandomWordSenseDisambiguation.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2007-2013, Jens Lehmann
+ *
+ * This file is part of DL-Learner.
+ *
+ * DL-Learner is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DL-Learner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.dllearner.algorithms.isle;
+
+import java.util.Random;
+import java.util.Set;
+
+import org.dllearner.algorithms.isle.index.Annotation;
+import org.dllearner.algorithms.isle.index.SemanticAnnotation;
+import org.dllearner.core.owl.Entity;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+/**
+ * Disambiguation by randomly selecting one of the candidates (baseline method).
+ * 
+ * @author Jens Lehmann
+ *
+ */
+public class RandomWordSenseDisambiguation extends WordSenseDisambiguation {
+
+	private Random random;
+	
+	public RandomWordSenseDisambiguation(OWLOntology ontology) {
+		super(ontology);
+		random = new Random();
+	}
+
+	@Override
+	public SemanticAnnotation disambiguate(Annotation annotation,
+			Set<Entity> candidateEntities) {
+		int pos = random.nextInt(candidateEntities.size());
+		int i = 0;
+		for(Entity e : candidateEntities)
+		{
+		    if (i == pos) {
+		    	return new SemanticAnnotation(annotation, e);
+		    }
+		    i++;
+		}
+		return null;
+	}
+
+}
