22824628e99a51670104af0c4adb36b1486555bd
Daniel Fleischhacker <dfleischhacker@users.noreply.github.com>
2013-09-09 10:12:21 +0000

    Add possibility to switch off word normalization in annotator

--- components-core/src/main/java/org/dllearner/algorithms/isle/index/TrieLinguisticAnnotator.java
+++ components-core/src/main/java/org/dllearner/algorithms/isle/index/TrieLinguisticAnnotator.java
@@ -4,19 +4,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Annotates a document using a prefix trie
+ * Annotates a document using a prefix trie.
  *
  * @author Andre Melo
  */
 public class TrieLinguisticAnnotator implements LinguisticAnnotator {
     EntityCandidatesTrie candidatesTrie;
+    private boolean normalizeWords = true;
 
     public TrieLinguisticAnnotator(EntityCandidatesTrie candidatesTrie) {
         this.candidatesTrie = candidatesTrie;
     }
 
     /**
-     * Generates annotation based on trie's longest matching strings
+     * Generates annotation based on trie's longest matching strings. By default, the document's contents are
+     * normalized using a lemmatizer. The normalization step can be disabled using the
      *
      * @param document the document to get annotations for
      * @return the set of annotation for the given document
@@ -26,8 +28,14 @@ public class TrieLinguisticAnnotator implements LinguisticAnnotator {
         String content = document.getContent();
         Set<Annotation> annotations = new HashSet<Annotation>();
         for (int i = 0; i < content.length(); i++) {
+            if (Character.isWhitespace(content.charAt(i))) {
+                continue;
+            }
             String unparsed = content.substring(i);
-            String match = candidatesTrie.getLongestMatch(LinguisticUtil.getInstance().getNormalizedForm(unparsed));
+            if (normalizeWords) {
+                unparsed = LinguisticUtil.getInstance().getNormalizedForm(unparsed);
+            }
+            String match = candidatesTrie.getLongestMatch(unparsed);
             if (match != null && !match.isEmpty()) {
                 Annotation annotation = new Annotation(document, i, match.length());
                 annotations.add(annotation);
@@ -37,4 +45,11 @@ public class TrieLinguisticAnnotator implements LinguisticAnnotator {
         return annotations;
     }
 
+    /**
+     * Sets whether the document's contents should be normalized or not.
+     * @param enabled if true normalizing is enabled, otherwise disabled
+     */
+    public void setNormalizeWords(boolean enabled) {
+        normalizeWords = enabled;
+    }
 }
