8a37dee6555496195dda0d15d434c5b434f07e92
Daniel Fleischhacker <dfleischhacker@users.noreply.github.com>
2013-11-21 12:44:09 +0000

    Re-implement TextDocument based on Tokens

--- components-core/src/main/java/org/dllearner/algorithms/isle/index/TextDocument.java
+++ components-core/src/main/java/org/dllearner/algorithms/isle/index/TextDocument.java
@@ -7,6 +7,63 @@ import java.util.LinkedList;
  *
  * @author Daniel Fleischhacker
  */
-public class TextDocument extends LinkedList<Token> {
+public class TextDocument extends LinkedList<Token> implements Document {
+    @Override
+    public String getContent() {
+        return getContentStartingAtToken(this.getFirst(), Level.STEMMED);
+    }
 
+    @Override
+    public String getRawContent() {
+        return getContentStartingAtToken(this.getFirst(), Level.RAW);
+    }
+
+    @Override
+    public String getPOSTaggedContent() {
+        return getContentStartingAtToken(this.getFirst(), Level.POS_TAGGED);
+    }
+
+    public static enum Level {
+        RAW,
+        POS_TAGGED,
+        STEMMED
+    }
+
+    /**
+     * Returns a string containing all tokens starting at the token {@code start} until the end of the list. The
+     * surface forms according to {@code level} are used to build the string.
+     *
+     * @param start token to start building the string at, i.e., the first token in the returned string
+     * @param l level of surface forms to use
+     * @return built string
+     */
+    public String getContentStartingAtToken(Token start, Level l) {
+        StringBuilder sb = new StringBuilder();
+        boolean found = false;
+        for (Token t : this) {
+            if (found) {
+                sb.append(" ");
+                sb.append(getStringForLevel(t, l));
+            }
+            else if (t == start) {
+                found = true;
+                sb.append(getStringForLevel(t, l));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private String getStringForLevel(Token t, Level l) {
+        switch (l) {
+            case RAW:
+                return t.getRawForm();
+            case POS_TAGGED:
+                return t.getPOSTag();
+            case STEMMED:
+                return t.getStemmedForm();
+        }
+
+        return null;
+    }
 }
