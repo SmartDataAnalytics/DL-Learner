3d4b9bdf603d8ecd9d72c1fd40874ac400c84ae4
Lorenz Bühmann <LorenzBuehmann@users.noreply.github.com>
2013-12-10 13:47:53 +0000

    Added PMI test.

--- components-core/src/test/java/org/dllearner/algorithms/isle/DBpediaExperiment.java
+++ components-core/src/test/java/org/dllearner/algorithms/isle/DBpediaExperiment.java
@@ -3,23 +3,42 @@
  */
 package org.dllearner.algorithms.isle;
 
-import com.google.common.collect.Sets;
-import com.hp.hpl.jena.rdf.model.Model;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import org.apache.commons.compress.compressors.CompressorException;
 import org.apache.commons.compress.compressors.CompressorInputStream;
 import org.apache.commons.compress.compressors.CompressorStreamFactory;
 import org.dllearner.core.owl.NamedClass;
 import org.dllearner.kb.sparql.SparqlEndpoint;
+import org.dllearner.utilities.owl.OWLEntityTypeAdder;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.AxiomType;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
-import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashSet;
-import java.util.Set;
+import com.google.common.collect.Sets;
+import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.OWL;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
+import com.hp.hpl.jena.vocabulary.XSD;
 
 /**
  * @author Lorenz Buehmann
@@ -28,7 +47,7 @@ import java.util.Set;
 public class DBpediaExperiment extends Experiment{
 	
 	final SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpedia();
-	final int maxNrOfInstancesPerClass = 100;
+	final int maxNrOfInstancesPerClass = 10;
 	
 	
 
@@ -38,12 +57,12 @@ public class DBpediaExperiment extends Experiment{
 	@Override
 	protected OWLOntology getOntology() {
 		//load the DBpedia schema
+		OWLOntology schema = null;
 		try {
 			URL url = new URL("http://downloads.dbpedia.org/3.9/dbpedia_3.9.owl.bz2");
 			InputStream is = new BufferedInputStream(url.openStream());
 			 CompressorInputStream in = new CompressorStreamFactory().createCompressorInputStream("bzip2", is);
-			 OWLOntology schema = OWLManager.createOWLOntologyManager().loadOntologyFromOntologyDocument(in);
-			 return schema;
+			 schema = OWLManager.createOWLOntologyManager().loadOntologyFromOntologyDocument(in);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -59,17 +78,99 @@ public class DBpediaExperiment extends Experiment{
 				"http://dbpedia.org/ontology/", 
 				Sets.newHashSet(new NamedClass("http://dbpedia.org/ontology/Person")),
 				maxNrOfInstancesPerClass);
+		cleanUpModel(sample);
+		filter(sample, "http://dbpedia.org/ontology/");
+		OWLEntityTypeAdder.addEntityTypes(sample);
+		StmtIterator iterator = sample.listStatements();
+		while(iterator.hasNext()){
+			System.out.println(iterator.next());
+		}
+		
 		try {
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			sample.write(baos, "TURTLE", null);
 			OWLOntologyManager man = OWLManager.createOWLOntologyManager();
 			OWLOntology ontology = man.loadOntologyFromOntologyDocument(new ByteArrayInputStream(baos.toByteArray()));
+			man.addAxioms(ontology, schema.getAxioms());
+			man.removeAxioms(ontology, ontology.getAxioms(AxiomType.FUNCTIONAL_DATA_PROPERTY));
+			man.removeAxioms(ontology, ontology.getAxioms(AxiomType.FUNCTIONAL_OBJECT_PROPERTY));
+			man.removeAxioms(ontology, ontology.getAxioms(AxiomType.DATA_PROPERTY_RANGE));
 			return ontology;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 		return null;
+	}
+	
+	/**
+	 * Filter triples which are not relevant based on the given knowledge base
+	 * namespace.
+	 *
+	 * @param model
+	 * @param namespace
+	 */
+	private void filter(Model model, String namespace) {
+		List<Statement> statementsToRemove = new ArrayList<Statement>();
+		for (Iterator<Statement> iter = model.listStatements().toList().iterator(); iter.hasNext();) {
+			Statement st = iter.next();
+			Property predicate = st.getPredicate();
+			if (predicate.equals(RDF.type)) {
+				if (!st.getObject().asResource().getURI().startsWith(namespace)) {
+					statementsToRemove.add(st);
+				} else if (st.getObject().equals(OWL.FunctionalProperty.asNode())) {
+					statementsToRemove.add(st);
+				} else if (st.getObject().isLiteral() && st.getObject().asLiteral().getDatatypeURI().equals(XSD.gYear.getURI())) {
+					statementsToRemove.add(st);
+				}
+			} else if (!predicate.equals(RDFS.subClassOf) && !predicate.equals(OWL.sameAs) && !predicate.asResource().getURI().startsWith(namespace)) {
+				statementsToRemove.add(st);
+			}
+		}
+		model.remove(statementsToRemove);
+	}
+	
+	private static void cleanUpModel(Model model) {
+		// filter out triples with String literals, as therein often occur
+		// some syntax errors and they are not relevant for learning
+		List<Statement> statementsToRemove = new ArrayList<Statement>();
+		for (Iterator<Statement> iter = model.listStatements().toList().iterator(); iter.hasNext();) {
+			Statement st = iter.next();
+			RDFNode object = st.getObject();
+			if (object.isLiteral()) {
+				// statementsToRemove.add(st);
+				Literal lit = object.asLiteral();
+				if (lit.getDatatype() == null || lit.getDatatype().equals(XSD.xstring)) {
+					st.changeObject("shortened", "en");
+				} else if (lit.getDatatype().getURI().equals(XSD.gYear.getURI())) {
+					statementsToRemove.add(st);
+					//                    System.err.println("REMOVE " + st);
+				} else if (lit.getDatatype().getURI().equals(XSD.gYearMonth.getURI())) {
+					statementsToRemove.add(st);
+//					                    System.err.println("REMOVE " + st);
+				}
+			}
+			//remove statements like <x a owl:Class>
+			if (st.getPredicate().equals(RDF.type)) {
+				if (object.equals(RDFS.Class.asNode()) || object.equals(OWL.Class.asNode()) || object.equals(RDFS.Literal.asNode())
+						|| object.equals(RDFS.Resource)) {
+					statementsToRemove.add(st);
+				}
+			}
+
+			//remove unwanted properties
+			String dbo = "http://dbpedia.org/ontology/";
+			Set<String> blackList = Sets.newHashSet(dbo + "wikiPageDisambiguates",dbo + "wikiPageExternalLink",
+					dbo + "wikiPageID", dbo + "wikiPageInterLanguageLink", dbo + "wikiPageRedirects", dbo + "wikiPageRevisionID",
+					dbo + "wikiPageWikiLink");
+			for(String bl: blackList){
+				if (st.getPredicate().getURI().equals(bl)) {
+					statementsToRemove.add(st);
+				}
+			}
+		}
+
+		model.remove(statementsToRemove);
 	}
 	
 	
