df68ab26bc8005a7f18449373aebeeff52cc2977
lorenz_b <lorenz_b@af9c125c-f337-0410-9a96-b85bd07cbc14>
2014-05-07 11:22:01 +0000

    Added QTL tests.

---
+++ trunk/test/qtl/mutagenesis/prolog/188/s6.pl
@@ -0,0 +1,19 @@
+active(d25).
+active(d45).
+active(d54).
+active(d58).
+active(d75).
+active(d101).
+active(d140).
+active(d149).
+active(d159).
+active(d171).
+active(d8).
+active(d47).
+active(d121).
+active(d134).
+:- active(d70).
+:- active(d77).
+:- active(d111).
+:- active(d62).
+:- active(d179).
