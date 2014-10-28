6f0764bb755f528db6eaf1c80787822499fe0dbf
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2014-10-28 09:07:59 +0100

    added test case for schema generation.

---
+++ components-core/src/test/java/org/dllearner/algorithms/schema/SchemaGeneratorTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.dllearner.algorithms.schema;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Lorenz Buehmann 
+ * @since Oct 25, 2014
+ */
+public class SchemaGeneratorTest {
+
+	/**
+	 * Test method for {@link org.dllearner.algorithms.schema.SchemaGenerator#generateSchema()}.
+	 */
+	@Test
+	public void testGenerateSchema() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ components-core/src/test/java/org/dllearner/algorithms/schema/SyntheticDataGenerator.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.dllearner.algorithms.schema;
+
+/**
+ * @author Lorenz Buehmann 
+ * @since Oct 25, 2014
+ */
+public class SyntheticDataGenerator {
+
+}
