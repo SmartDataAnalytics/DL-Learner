5af8e965faaf600d24bbfb1f55d8efe6a9b9fac6
Lorenz Bühmann <buehmann@informatik.uni-leipzig.de>
2014-11-19 11:56:37 +0100

    Added utility class to generate fresh individuals.

---
+++ components-core/src/main/java/org/dllearner/reasoning/GenericIndividualGenerator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.dllearner.reasoning;
+
+import org.dllearner.core.owl.Individual;
+
+/**
+ * Generates generic individuals.
+ * @author Lorenz Buehmann
+ *
+ */
+public class GenericIndividualGenerator {
+	
+	private int cnt = 0;
+	
+	public Individual newIndividual(){
+		return new Individual("http://dllearner.org#genInd_" + cnt++);
+	}
+}
