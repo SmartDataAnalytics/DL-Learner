15e92bb79534e4de9660e4a37f0e81d225afc69b
Lorenz Bühmann <buehmann@informatik.uni-leipzig.de>
2015-01-27 18:23:12 +0100

    Made CLI working again.

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script builds the DL-LEarner project and creates the Zip and Tarball file
 # which contains the command line interface 'cli' and 'enrichment'.
-mvn -DskipTests=true clean install
+mvn -DskipTests=true -pl components-core,interfaces install
 cd interfaces
 mvn -Prelease package -DskipTests=true
