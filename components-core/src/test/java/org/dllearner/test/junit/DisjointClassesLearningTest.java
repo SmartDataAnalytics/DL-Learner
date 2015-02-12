8881a39bd3e9a686696deb26ecaf8056d7dc614e
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-02-12 23:37:54 +0100

    defused test that depends on external services

--- components-core/src/test/java/org/dllearner/test/junit/DisjointClassesLearningTest.java
+++ components-core/src/test/java/org/dllearner/test/junit/DisjointClassesLearningTest.java
@@ -1,57 +1,54 @@
 package org.dllearner.test.junit;
 
-import junit.framework.TestCase;
-
 import org.dllearner.algorithms.DisjointClassesLearner;
 import org.dllearner.kb.SparqlEndpointKS;
-import org.dllearner.kb.sparql.SparqlEndpoint;
 import org.dllearner.reasoning.SPARQLReasoner;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClassExpression;
 
 import uk.ac.manchester.cs.owl.owlapi.OWLClassImpl;
 
-public class DisjointClassesLearningTest extends TestCase{
-	
+public class DisjointClassesLearningTest { //extends TestCase{
+
 	private SparqlEndpointKS ks;
 	private SPARQLReasoner reasoner;
-	
+
 	private static final int maxExecutionTimeInSeconds = 10;
-	
-	@Override
-	protected void setUp() throws Exception {
-		super.setUp();
-		ks = new SparqlEndpointKS(SparqlEndpoint.getEndpointDBpediaLiveAKSW());
-		
-		reasoner = new SPARQLReasoner(ks);
-		reasoner.prepareSubsumptionHierarchy();
-	}
-	
+
+//	@Override
+//	protected void setUp() throws Exception {
+//		super.setUp();
+//		ks = new SparqlEndpointKS(SparqlEndpoint.getEndpointDBpediaLiveAKSW());
+//
+//		reasoner = new SPARQLReasoner(ks);
+//		reasoner.prepareSubsumptionHierarchy();
+//	}
+
 	public void testLearnSingleClass(){
 		DisjointClassesLearner l = new DisjointClassesLearner(ks);
 		l.setReasoner(reasoner);
 		l.setMaxExecutionTimeInSeconds(maxExecutionTimeInSeconds);
 		l.setEntityToDescribe(new OWLClassImpl(IRI.create("http://dbpedia.org/ontology/Book")));
-		
+
 		l.start();
-		
+
 		System.out.println(l.getCurrentlyBestAxioms(5));
 	}
-	
+
 	public void testLearnForMostGeneralClasses(){
 		DisjointClassesLearner l = new DisjointClassesLearner(ks);
 		l.setReasoner(reasoner);
 		l.setMaxExecutionTimeInSeconds(maxExecutionTimeInSeconds);
-		
+
 		for(OWLClassExpression cls : reasoner.getClassHierarchy().getMostGeneralClasses()){
 			l.setEntityToDescribe(cls.asOWLClass());
-			
+
 			l.start();
-			
+
 			System.out.println(l.getCurrentlyBestAxioms(5));
 		}
 	}
-	
-	
+
+
 
 }
