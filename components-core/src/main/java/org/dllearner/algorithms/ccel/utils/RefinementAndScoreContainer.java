183f0f0495d1bdab7b3e13d9d8a4f66c7d5426a7
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2015-04-24 21:04:09 +0200

    added helper class for parallelization

---
+++ components-core/src/main/java/org/dllearner/algorithms/ccel/utils/RefinementAndScoreContainer.java
@@ -0,0 +1,44 @@
+package org.dllearner.algorithms.ccel.utils;
+
+import java.io.Serializable;
+
+import org.dllearner.algorithms.celoe.OENode;
+import org.semanticweb.owlapi.model.OWLClassExpression;
+
+public class RefinementAndScoreContainer implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private OWLClassExpression refinement;
+    private OENode parentNode;
+//    private ScoreTwoValued<OWLEntity> score;
+    private double accuracy;
+
+    public RefinementAndScoreContainer(OWLClassExpression refinement, OENode parentNode) {
+        this.refinement = refinement;
+        this.parentNode = parentNode;
+    }
+
+//    public void setScore(ScoreTwoValued<OWLEntity> score) {
+//        this.score = score;
+//    }
+//
+//    public ScoreTwoValued<OWLEntity> getScore() {
+//        return this.score;
+//    }
+
+    public void setAccuracy(double accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public double getAccuracy() {
+        return this.accuracy;
+    }
+
+    public OWLClassExpression getRefinement() {
+        return this.refinement;
+    }
+
+    public OENode getParentNode() {
+        return this.parentNode;
+    }
+}
