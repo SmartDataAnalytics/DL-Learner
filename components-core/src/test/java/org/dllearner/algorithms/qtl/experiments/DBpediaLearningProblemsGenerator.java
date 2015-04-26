9b16812974d8ce80469d2c555fe8d8df32a33428
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2015-04-26 17:10:22 +0200

    Cont. benchmark generator.

--- components-core/src/test/java/org/dllearner/algorithms/qtl/experiments/DBpediaLearningProblemsGenerator.java
+++ components-core/src/test/java/org/dllearner/algorithms/qtl/experiments/DBpediaLearningProblemsGenerator.java
@@ -13,6 +13,9 @@ import java.io.InputStream;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import org.aksw.jena_sparql_api.model.QueryExecutionFactoryModel;
 import org.apache.jena.riot.Lang;
@@ -62,8 +65,8 @@ public class DBpediaLearningProblemsGenerator {
 		
 		ks = new SparqlEndpointKS(endpoint);
 		ks.setCacheDir(new File(benchmarkDirectory, "cache").getPath() + ";mv_store=false");
-//		ks.setPageSize(100000);
-//		ks.setUseCache(false);
+		ks.setPageSize(50000);
+		ks.setUseCache(false);
 		ks.setQueryDelay(100);
 		ks.init();
 		
@@ -83,125 +86,37 @@ public class DBpediaLearningProblemsGenerator {
 		return reasoner.getMostSpecificClasses();
 	}
 	
-	public void generateBenchmark(int size, int maxDepth) {
+	public void generateBenchmark(int size, final int maxDepth) {
 		Set<OWLClass> classes = getClasses();
 		
-		Model data;
 		Iterator<OWLClass> iterator = classes.iterator();
 		int i = 0;
+		
+		ExecutorService tp = Executors.newFixedThreadPool(4);
 		while(i < size && !classes.isEmpty()) {
+			i++;
 			
 			// pick class randomly
-			OWLClass cls = iterator.next();
-			cls = new OWLClassImpl(IRI.create("http://dbpedia.org/ontology/AcademicJournal"));
+			final OWLClass cls = iterator.next();
+//			cls = new OWLClassImpl(IRI.create("http://dbpedia.org/ontology/AcademicJournal"));
 			System.out.println(cls);
 			
-			// load data
-			System.out.print("Loading data...");
-			long s = System.currentTimeMillis();
-			data = loadDataFromCacheOrCompute(cls, maxDepth, true);
-			System.out.println(data.size() + " triples in " + (System.currentTimeMillis() - s) + "ms");
+			tp.submit(new Worker(ks, cls, maxDepth));
 			
-			// analyze
-			System.out.print("Analyzing...");
-			s = System.currentTimeMillis();
-			analyze(cls, data);
-			System.out.println("done in " + (System.currentTimeMillis() - s) + "ms");
-			
-			break;
+			if(i == 8) {
+				break;
+			}
+		}
+		
+		tp.shutdown();
+		try {
+			tp.awaitTermination(1, TimeUnit.DAYS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 	
-	private void analyze(OWLClass cls, Model model) {
-		ParameterizedSparqlString template = new ParameterizedSparqlString(
-				"SELECT DISTINCT ?p WHERE {"
-				+ "?s a ?cls . "
-				+ "?s ?p ?o . "
-				+ "?p a <http://www.w3.org/2002/07/owl#ObjectProperty> .}");
-		template.setIri("cls", cls.toStringID());
-		
-		ParameterizedSparqlString template2 = new ParameterizedSparqlString(
-				"SELECT DISTINCT ?p2 WHERE {"
-				+ "?s a ?cls . "
-				+ "?s ?p1 ?o1 . "
-				+ "?o1 ?p2 ?o2 . "
-				+ "?p2 a <http://www.w3.org/2002/07/owl#ObjectProperty> .}");
-		template2.setIri("cls", cls.toStringID());
-		
-		ParameterizedSparqlString template3 = new ParameterizedSparqlString(
-				"SELECT ?o (COUNT(DISTINCT ?s1) AS ?cnt) WHERE {"
-				+ "?s1 a ?cls . "
-				+ "?s2 a ?cls . "
-//				+ "?p2 a <http://www.w3.org/2002/07/owl#ObjectProperty> ."
-				+ "?s1 ?p1 ?o1_1 . ?o1_1 ?p2 ?o ."
-				+ "?s2 ?p1 ?o2_1 . ?o2_1 ?p2 ?o ."
-				+ "FILTER(!sameterm(?s1, ?s2) && !sameterm(?o1_1, ?o2_1))"
-				+ "} GROUP BY ?o ORDER BY DESC(?cnt)");
-		template3 = new ParameterizedSparqlString(
-				"SELECT ?o (COUNT(DISTINCT ?s1) AS ?cnt) WHERE {"
-				+ "?s1 a ?cls . "
-				+ "?s1 ?p1 ?o1_1 . ?o1_1 ?p2 ?o ."
-				+ "FILTER EXISTS{?s2 a ?cls . ?s2 ?p1 ?o2_1 . ?o2_1 ?p2 ?o ."
-				+ "FILTER(!sameterm(?s1, ?s2) && !sameterm(?o1_1, ?o2_1))}"
-				+ "} GROUP BY ?o ORDER BY DESC(?cnt)");
-		template3.setIri("cls", cls.toStringID());
-		
-		
-		
-		QueryExecution qe = new QueryExecutionFactoryModel(model).createQueryExecution(template.toString());
-		ResultSet rs = qe.execSelect();
-		StringBuilder sb = new StringBuilder();
-		while(rs.hasNext()) {
-			String property1 = rs.next().getResource("p").getURI();
-			template2.setIri("p1", property1);
-			
-			QueryExecution qe2 = new QueryExecutionFactoryModel(model).createQueryExecution(template2.toString());
-			ResultSet rs2 = qe2.execSelect();
-			
-			while(rs2.hasNext()) {
-				String property2 = rs2.next().getResource("p2").getURI();
-				template3.setIri("p1", property1);
-				template3.setIri("p2", property2);
-				
-//				System.out.println(template3.asQuery());
-				QueryExecution qe3 = new QueryExecutionFactoryModel(model).createQueryExecution(template3.toString());
-				ResultSet rs3 = qe3.execSelect();
-				String filename = UrlEscapers.urlFormParameterEscaper().escape(cls.toStringID()) + ".log";
-				
-				String delimiter = "\t";
-				QuerySolution qs;
-				while(rs3.hasNext()) {
-					qs = rs3.next();
-					if(qs.get("o") != null) {
-						sb.append(property1).append(delimiter).
-						append(property2).append(delimiter).
-						append(qs.getResource("o").getURI()).append(delimiter).
-						append(qs.getLiteral("cnt").getInt()).
-						append("\n");
-					}
-				}
-//				sb.append(ResultSetFormatter.asText(rs));
-//				System.out.println(sb);
-				try {
-					Files.write(sb.toString(), new File(dataDir, filename), Charsets.UTF_8);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				qe3.close();
-			}
-			qe2.close();
-		}
-		qe.close();
-		
-//		query = String.format("SELECT ?p1 ?p2 ?o (COUNT(DISTINCT ?s) AS ?cnt) WHERE {"
-//				+ "?s a <%s> . "
-//				+ "?p1 a <http://www.w3.org/2002/07/owl#ObjectProperty> . "
-//				+ "?p2 a <http://www.w3.org/2002/07/owl#ObjectProperty> ."
-//				+ "?s ?p1 ?o1_1 . ?o1_1 ?p2 ?o ."
-//				+ "FILTER EXISTS {?s2 a <%s> . ?s ?p1 ?o2_1 . ?o2_1 ?p2 ?o . FILTER(?s2 != ?s && ?o2_1 != ?o1_1)}"
-//				+ "} GROUP BY ?p1 ?p2 ?o HAVING(?cnt > 10) ORDER BY ?p1 ?p2 DESC(?cnt)", cls.toStringID(), cls.toStringID());
-		
-	}
+	
 	
 	private void analyzeSingleQuery(OWLClass cls, Model model) {
 		ParameterizedSparqlString template = new ParameterizedSparqlString(
@@ -270,92 +185,206 @@ public class DBpediaLearningProblemsGenerator {
 		
 	}
 	
-	private Model loadData(OWLClass cls, int maxDepth) {
-		Model model = ModelFactory.createDefaultModel();
-		try(InputStream is = new BufferedInputStream(new FileInputStream("/home/user/work/datasets/qtl/Airport2.ttl"))){
-			RDFDataMgr.read(model, is, Lang.TURTLE);
-		} catch (Exception e) {
-			e.printStackTrace();
+	class Worker implements Runnable {
+		
+		private OWLClass cls;
+		private SparqlEndpointKS ks;
+		private int maxDepth;
+
+		public Worker(SparqlEndpointKS ks, OWLClass cls, int maxDepth) {
+			this.ks = ks;
+			this.cls = cls;
+			this.maxDepth = maxDepth;
 		}
-		
-		return model;
-	}
-	
-	private Model loadDataFromCacheOrCompute(OWLClass cls, int maxDepth, boolean singleQuery) {
-		String filename = UrlEscapers.urlFormParameterEscaper().escape(cls.toStringID()) + ".ttl";
-		File file = new File(dataDir, filename);
-		
-		Model model;
-		if(file.exists()) {
-			model = ModelFactory.createDefaultModel();
+
+		/* (non-Javadoc)
+		 * @see java.lang.Runnable#run()
+		 */
+		@Override
+		public void run() {
 			try {
-				model.read(new FileInputStream(file), null, "TURTLE");
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
+				// load data
+				System.out.println(Thread.currentThread().getId() + ":" + "Loading data for " + cls.toStringID() + "...");
+				long s = System.currentTimeMillis();
+				Model data = loadDataFromCacheOrCompute(cls, maxDepth, true);
+				System.out.println(Thread.currentThread().getId() + ":" + "Got " + data.size() + " triples for " + cls.toStringID() + "in " + (System.currentTimeMillis() - s) + "ms");
+				
+				// analyze
+				System.out.println(Thread.currentThread().getId() + ":" + "Analyzing " + cls.toStringID() + "...");
+				s = System.currentTimeMillis();
+				analyze(cls, data);
+				System.out.println(Thread.currentThread().getId() + ":" + "Analyzed " + cls.toStringID() + " in " + (System.currentTimeMillis() - s) + "ms");
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else {
-			if(singleQuery) {
-				model = loadDataFromEndpointBatch(cls, maxDepth);
+		}
+		
+		private Model loadDataFromCacheOrCompute(OWLClass cls, int maxDepth, boolean singleQuery) {
+			String filename = UrlEscapers.urlFormParameterEscaper().escape(cls.toStringID()) + ".ttl";
+			File file = new File(dataDir, filename);
+			
+			Model model;
+			if(file.exists()) {
+				model = ModelFactory.createDefaultModel();
+				try {
+					model.read(new FileInputStream(file), null, "TURTLE");
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			} else {
-				model = loadDataFromEndpoint(cls, maxDepth);
+				if(singleQuery) {
+					model = loadDataFromEndpointBatch(cls, maxDepth);
+				} else {
+					model = loadDataFromEndpoint(cls, maxDepth);
+				}
+				try {
+					model.write(new FileOutputStream(file), "TURTLE");
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-			try {
-				model.write(new FileOutputStream(file), "TURTLE");
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			model.add(schema);
+			return model;
 		}
-		model.add(schema);
-		return model;
-	}
-	
-	private Model loadDataFromEndpoint(OWLClass cls, int maxDepth) {
-		Model data = ModelFactory.createDefaultModel();
 		
-		// get individuals
-		SortedSet<OWLIndividual> individuals = reasoner.getIndividuals(cls);
+		private Model loadDataFromEndpoint(OWLClass cls, int maxDepth) {
+			Model data = ModelFactory.createDefaultModel();
+			
+			// get individuals
+			SortedSet<OWLIndividual> individuals = reasoner.getIndividuals(cls);
+						
+			int cnt = 0;
+			Model cbd;
+			for (OWLIndividual ind : individuals) {
+				System.out.println(cnt++ + "/" + individuals.size());
+				cbd = cbdGen.getConciseBoundedDescription(ind.toStringID(), maxDepth);
+				data.add(cbd);
+			}
+			
+			return data;
+		}
+		
+		private Model loadDataFromEndpointBatch(OWLClass cls, int maxDepth) {
+			String filename = UrlEscapers.urlFormParameterEscaper().escape(cls.toStringID()) + ".ttl";
+			File file = new File(dataDir, filename);
+			
+			Model model;
+			if(file.exists()) {
+				model = ModelFactory.createDefaultModel();
+				try {
+					model.read(new FileInputStream(file), null, "TURTLE");
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				String query = "construct{?s ?p0 ?o0 . ?o0 ?p1 ?o1 .} "
+						+ "where {"
+						+ "?s a <" + cls.toStringID() + ">. "
+						+ "?s ?p0 ?o0 . "
+						+ "optional{?o0 ?p1 ?o1 .}}";
+				model = ks.getQueryExecutionFactory().createQueryExecution(query).execConstruct();
+			}
+			
+			return model;
+		}
+		
+		private void analyze(OWLClass cls, Model model) {
+			String filename = UrlEscapers.urlFormParameterEscaper().escape(cls.toStringID()) + ".log";
+			File file = new File(dataDir, filename);
+			
+			if(!file.exists()) {
+				
+				StringBuilder sb = new StringBuilder();
+				
+				ParameterizedSparqlString template = new ParameterizedSparqlString(
+						"SELECT DISTINCT ?p WHERE {"
+						+ "?s a ?cls . "
+						+ "?s ?p ?o . "
+						+ "?p a <http://www.w3.org/2002/07/owl#ObjectProperty> .}");
+				template.setIri("cls", cls.toStringID());
+				
+				ParameterizedSparqlString template2 = new ParameterizedSparqlString(
+						"SELECT DISTINCT ?p2 WHERE {"
+						+ "?s a ?cls . "
+						+ "?s ?p1 ?o1 . "
+						+ "?o1 ?p2 ?o2 . "
+						+ "?p2 a <http://www.w3.org/2002/07/owl#ObjectProperty> .}");
+				template2.setIri("cls", cls.toStringID());
+				
+				ParameterizedSparqlString template3 = new ParameterizedSparqlString(
+						"SELECT ?o (COUNT(DISTINCT ?s1) AS ?cnt) WHERE {"
+						+ "?s1 a ?cls . "
+						+ "?s2 a ?cls . "
+	//					+ "?p2 a <http://www.w3.org/2002/07/owl#ObjectProperty> ."
+						+ "?s1 ?p1 ?o1_1 . ?o1_1 ?p2 ?o ."
+						+ "?s2 ?p1 ?o2_1 . ?o2_1 ?p2 ?o ."
+						+ "FILTER(!sameterm(?s1, ?s2) && !sameterm(?o1_1, ?o2_1))"
+						+ "} GROUP BY ?o ORDER BY DESC(?cnt)");
+				template3 = new ParameterizedSparqlString(
+						"SELECT ?o (COUNT(DISTINCT ?s1) AS ?cnt) WHERE {"
+						+ "?s1 a ?cls . "
+						+ "?s1 ?p1 ?o1_1 . ?o1_1 ?p2 ?o ."
+						+ "FILTER EXISTS{?s2 a ?cls . ?s2 ?p1 ?o2_1 . ?o2_1 ?p2 ?o ."
+						+ "FILTER(!sameterm(?s1, ?s2) && !sameterm(?o1_1, ?o2_1))}"
+						+ "} GROUP BY ?o HAVING(?cnt >= 10) ORDER BY DESC(?cnt)");
+				template3.setIri("cls", cls.toStringID());
+				
+				
+				QueryExecution qe = new QueryExecutionFactoryModel(model).createQueryExecution(template.toString());
+				ResultSet rs = qe.execSelect();
+				
+				while(rs.hasNext()) {
+					String property1 = rs.next().getResource("p").getURI();
+					template2.setIri("p1", property1);
 					
-		int cnt = 0;
-		Model cbd;
-		for (OWLIndividual ind : individuals) {
-			System.out.println(cnt++ + "/" + individuals.size());
-			cbd = cbdGen.getConciseBoundedDescription(ind.toStringID(), maxDepth);
-			data.add(cbd);
-		}
-		
-		return data;
-	}
-	
-	private Model loadDataFromEndpointBatch(OWLClass cls, int maxDepth) {
-		String filename = UrlEscapers.urlFormParameterEscaper().escape(cls.toStringID()) + ".ttl";
-		File file = new File(dataDir, filename);
-		
-		Model model;
-		if(file.exists()) {
-			model = ModelFactory.createDefaultModel();
-			try {
-				model.read(new FileInputStream(file), null, "TURTLE");
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+					QueryExecution qe2 = new QueryExecutionFactoryModel(model).createQueryExecution(template2.toString());
+					ResultSet rs2 = qe2.execSelect();
+					
+					while(rs2.hasNext()) {
+						String property2 = rs2.next().getResource("p2").getURI();
+						template3.setIri("p1", property1);
+						template3.setIri("p2", property2);
+						
+	//					System.out.println(template3.asQuery());
+						QueryExecution qe3 = new QueryExecutionFactoryModel(model).createQueryExecution(template3.toString());
+						ResultSet rs3 = qe3.execSelect();
+						
+						String delimiter = "\t";
+						QuerySolution qs;
+						while(rs3.hasNext()) {
+							qs = rs3.next();
+							if(qs.get("o") != null) {
+								sb.append(property1).append(delimiter).
+								append(property2).append(delimiter).
+								append(qs.getResource("o").getURI()).append(delimiter).
+								append(qs.getLiteral("cnt").getInt()).
+								append("\n");
+							}
+						}
+	//					sb.append(ResultSetFormatter.asText(rs));
+	//					System.out.println(sb);
+						
+						qe3.close();
+					}
+					qe2.close();
+				}
+				qe.close();
+				
+				try {
+					Files.write(sb.toString(), file, Charsets.UTF_8);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-		} else {
-			String query = "construct{?s ?p0 ?o0 . ?o0 ?p1 ?o1 .} "
-					+ "where {"
-					+ "?s a <" + cls.toStringID() + ">. "
-					+ "?s ?p0 ?o0 . "
-					+ "optional{?o0 ?p1 ?o1 .}}";
-			model = ks.getQueryExecutionFactory().createQueryExecution(query).execConstruct();
 		}
-		
-		return model;
 	}
 	
 	public static void main(String[] args) throws Exception {
 		File dir = new File(args[0]);
-		new DBpediaLearningProblemsGenerator(dir).generateBenchmark(1, 2);
+		new DBpediaLearningProblemsGenerator(dir).generateBenchmark(10, 2);
 	}
 	
 
