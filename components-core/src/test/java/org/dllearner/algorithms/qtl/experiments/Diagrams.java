1ba96d9618bdb634c6164ebba24d655a26eb6299
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2015-05-08 22:35:06 +0200

    Diagrams.

--- components-core/src/test/java/org/dllearner/algorithms/qtl/experiments/Diagrams.java
+++ components-core/src/test/java/org/dllearner/algorithms/qtl/experiments/Diagrams.java
@@ -4,12 +4,14 @@
 package org.dllearner.algorithms.qtl.experiments;
 
 import java.io.File;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.dllearner.algorithms.qtl.heuristics.QueryTreeHeuristic;
@@ -18,6 +20,7 @@ import org.dllearner.learningproblems.Heuristics.HeuristicType;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Joiner;
+import com.google.common.collect.Maps;
 import com.google.common.io.Files;
 import com.google.common.primitives.Doubles;
 import com.google.common.primitives.Ints;
@@ -29,6 +32,9 @@ import com.google.common.primitives.Ints;
 public class Diagrams {
 	
 	public static void main(String[] args) throws Exception {
+		File dir = new File(args[0]);
+		dir.mkdirs();
+		
 		Properties config = new Properties();
 		config.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("org/dllearner/algorithms/qtl/qtl-eval-config.properties"));
 
@@ -36,6 +42,9 @@ public class Diagrams {
 		String username = config.getProperty("username");
 		String password = config.getProperty("password");
 		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		
+//		url = "jdbc:mysql://address=(protocol=tcp)(host=[2001:638:902:2010:0:168:35:138])(port=3306)(user=root)/qtl";
+		
 		Connection conn = DriverManager.getConnection(url, username, password);
 		
 		int[] nrOfExamplesIntervals = {
@@ -56,22 +65,27 @@ public class Diagrams {
 //				0.6
 				};
 		
+		Map<HeuristicType, String> measure2ColumnName = Maps.newHashMap();
+		measure2ColumnName.put(HeuristicType.FMEASURE, "avg_fscore_best_returned");
+		measure2ColumnName.put(HeuristicType.PRED_ACC, "avg_predacc_best_returned");
+		measure2ColumnName.put(HeuristicType.MATTHEWS_CORRELATION, "avg_mathcorr_best_returned");
 		HeuristicType[] measures = {
 				HeuristicType.PRED_ACC, 
 				HeuristicType.FMEASURE, 
 				HeuristicType.MATTHEWS_CORRELATION};
 		
-		File dir = new File("/tmp/qtl/");
-		dir.mkdirs();
+		// get distinct noise intervals
 		
 		// |E| vs fscore
-		PreparedStatement ps = conn.prepareStatement("SELECT nrOfExamples,avg_fscore_best_returned from eval_overall WHERE heuristic_measure = ? && noise = ? ORDER BY nrOfExamples");
+		String sql = "SELECT nrOfExamples,%s from eval_overall WHERE heuristic_measure = ? && noise = ? ORDER BY nrOfExamples";
+		PreparedStatement ps;
 		for (double noise : noiseIntervals) {
 			String s = "";
 			s += "\t";
 			s += Joiner.on("\t").join(Ints.asList(nrOfExamplesIntervals));
 			s += "\n";
 			for (HeuristicType measure : measures) {
+				ps = conn.prepareStatement(String.format(sql, measure2ColumnName.get(measure)));
 				ps.setString(1, measure.toString());
 				ps.setDouble(2, noise);
 				ResultSet rs = ps.executeQuery();
@@ -87,13 +101,14 @@ public class Diagrams {
 		}
 		
 		// noise vs fscore
-		ps = conn.prepareStatement("SELECT noise,avg_fscore_best_returned from eval_overall WHERE heuristic_measure = ? && nrOfExamples = ?");
+		sql = "SELECT noise,%s from eval_overall WHERE heuristic_measure = ? && nrOfExamples = ?";
 		for (int nrOfExamples : nrOfExamplesIntervals) {
 			String s = "";
 			s += "\t";
 			s += Joiner.on("\t").join(Doubles.asList(noiseIntervals));
 			s += "\n";
 			for (HeuristicType measure : measures) {
+				ps = conn.prepareStatement(String.format(sql, measure2ColumnName.get(measure)));
 				ps.setString(1, measure.toString());
 				ps.setInt(2, nrOfExamples);
 				ResultSet rs = ps.executeQuery();
