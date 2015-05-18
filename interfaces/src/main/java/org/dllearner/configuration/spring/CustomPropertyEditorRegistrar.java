f9354ab0765b456caec05196b707054b23e03b2e
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2015-05-18 11:45:43 +0200

    Set reasoner implementation by conf in lower case.

--- interfaces/src/main/java/org/dllearner/configuration/spring/CustomPropertyEditorRegistrar.java
+++ interfaces/src/main/java/org/dllearner/configuration/spring/CustomPropertyEditorRegistrar.java
@@ -2,6 +2,8 @@ package org.dllearner.configuration.spring;
 
 import org.dllearner.core.config.ClassExpressionPropertyEditor;
 import org.dllearner.core.config.IndividualEditor;
+import org.dllearner.core.config.ReasonerImplementationEditor;
+import org.dllearner.reasoning.ReasonerImplementation;
 import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLIndividual;
 import org.springframework.beans.PropertyEditorRegistrar;
@@ -24,6 +26,7 @@ public class CustomPropertyEditorRegistrar implements PropertyEditorRegistrar {
     	ClassExpressionPropertyEditor ce = new ClassExpressionPropertyEditor();
     	registry.registerCustomEditor(OWLClassExpression.class, ce);
     	registry.registerCustomEditor(OWLIndividual.class, new IndividualEditor());
+    	registry.registerCustomEditor(ReasonerImplementation.class, new ReasonerImplementationEditor());
     	
     }
 }
