f19016aa75030bc7918531a12001114aa7521e93
Simon Bin <sbin@informatik.uni-leipzig.de>
2015-08-07 19:39:17 +0200

    move "experiments" from core to scripts

--- scripts/src/main/java/org/dllearner/experiments/ExMakerCrossFolds.java
+++ scripts/src/main/java/org/dllearner/experiments/ExMakerCrossFolds.java
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.dllearner.utilities.experiments;
+package org.dllearner.experiments;
 
 import java.util.ArrayList;
 import java.util.Collections;
--- scripts/src/main/java/org/dllearner/experiments/FinalizedMonitor.java
+++ scripts/src/main/java/org/dllearner/experiments/FinalizedMonitor.java
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.dllearner.utilities.experiments;
+package org.dllearner.experiments;
 
 import java.io.Serializable;
 
