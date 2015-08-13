8949a32bc42d3b390b56bc268d4a39407acfb483
Simon Bin <sbin@informatik.uni-leipzig.de>
2015-08-13 11:46:12 +0200

    fix incorrect code duplication in Heuristic

--- components-core/src/main/java/org/dllearner/core/AbstractHeuristic.java
+++ components-core/src/main/java/org/dllearner/core/AbstractHeuristic.java
@@ -22,7 +22,6 @@ package org.dllearner.core;
 import java.util.Comparator;
 
 import org.dllearner.algorithms.celoe.OENode;
-import org.dllearner.core.config.ConfigOption;
 
 import com.google.common.collect.ComparisonChain;
 
@@ -34,19 +33,7 @@ import com.google.common.collect.ComparisonChain;
  * @author Jens Lehmann
  *
  */
-@ComponentAnn(name = "OEHeuristicRuntime", shortName = "celoe_heuristic", version = 0.5)
 public abstract class AbstractHeuristic extends AbstractComponent implements Heuristic, Comparator<OENode>{
-	
-	// strong penalty for long descriptions
-	private double expansionPenaltyFactor = 0.1;
-	// bonus for being better than parent node
-	private double gainBonusFactor = 0.3;
-	// penalty if a node OWLClassExpression has very many refinements since exploring 
-	// such a node is computationally very expensive
-	private double nodeRefinementPenalty = 0.0001;
-	
-	@ConfigOption(name = "startNodeBonus", defaultValue="0.1")
-	private double startNodeBonus = 0.1;
 	
 	public AbstractHeuristic() {
 
@@ -55,7 +42,7 @@ public abstract class AbstractHeuristic extends AbstractComponent implements Heu
 	@Override
 	public void init() throws ComponentInitException {
 
-	}		
+	}
 	
 	@Override
 	public int compare(OENode node1, OENode node2) {
@@ -67,35 +54,4 @@ public abstract class AbstractHeuristic extends AbstractComponent implements Heu
 
 	public abstract double getNodeScore(OENode node);
 
-	public double getExpansionPenaltyFactor() {
-		return expansionPenaltyFactor;
-	}
-
-	public double getGainBonusFactor() {
-		return gainBonusFactor;
-	}
-
-	public void setGainBonusFactor(double gainBonusFactor) {
-		this.gainBonusFactor = gainBonusFactor;
-	}
-
-	public double getNodeRefinementPenalty() {
-		return nodeRefinementPenalty;
-	}
-
-	public void setNodeRefinementPenalty(double nodeRefinementPenalty) {
-		this.nodeRefinementPenalty = nodeRefinementPenalty;
-	}
-
-	public void setExpansionPenaltyFactor(double expansionPenaltyFactor) {
-		this.expansionPenaltyFactor = expansionPenaltyFactor;
-	}
-
-	public double getStartNodeBonus() {
-		return startNodeBonus;
-	}
-
-	public void setStartNodeBonus(double startNodeBonus) {
-		this.startNodeBonus = startNodeBonus;
-	}
 }
