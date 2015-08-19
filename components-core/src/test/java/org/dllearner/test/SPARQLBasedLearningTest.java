2e2e7e3ecd883a48c30e038a88c390102bf6da03
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2015-08-19 14:15:23 +0200

    SPARQL learning test extended.

--- components-core/src/test/java/org/dllearner/test/SPARQLBasedLearningTest.java
+++ components-core/src/test/java/org/dllearner/test/SPARQLBasedLearningTest.java
@@ -4,24 +4,35 @@
 package org.dllearner.test;
 
 import java.util.NavigableSet;
-import java.util.TreeSet;
 
 import org.dllearner.algorithms.celoe.CELOE;
 import org.dllearner.core.AbstractKnowledgeSource;
+import org.dllearner.core.AbstractReasonerComponent;
 import org.dllearner.core.EvaluatedDescription;
+import org.dllearner.core.KnowledgeSource;
 import org.dllearner.kb.OWLAPIOntology;
 import org.dllearner.kb.SparqlEndpointKS;
 import org.dllearner.kb.sparql.ClassBasedSampleGenerator;
 import org.dllearner.kb.sparql.SparqlEndpoint;
 import org.dllearner.learningproblems.ClassLearningProblem;
-import org.dllearner.reasoning.SPARQLReasoner;
+import org.dllearner.reasoning.ClosedWorldReasoner;
+import org.dllearner.reasoning.OWLAPIReasoner;
+import org.dllearner.reasoning.ReasonerImplementation;
 import org.dllearner.refinementoperators.RhoDRDown;
+import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyChange;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.util.OWLOntologyMerger;
 
 import uk.ac.manchester.cs.owl.owlapi.OWLClassImpl;
 
+import com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory;
 import com.google.common.collect.Sets;
 
 /**
@@ -37,24 +48,40 @@ public class SPARQLBasedLearningTest {
 		
 		SparqlEndpoint endpoint = SparqlEndpoint.getEndpointDBpedia();
 //		endpoint = new SparqlEndpoint(new URL("http://sake.informatik.uni-leipzig.de:8890/sparql"), "http://dbpedia.org");
+		
+		// the class to describe
 		OWLClass cls = new OWLClassImpl(IRI.create("http://dbpedia.org/ontology/SoccerClub"));
 		
-		SparqlEndpointKS ks = new SparqlEndpointKS(endpoint);
+		// (optionally) provide the schema
+		String ontologyURL = "http://downloads.dbpedia.org/2015-04/dbpedia_2015-04.owl";
+		OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+		OWLOntology schemaOntology = man.loadOntology(IRI.create(ontologyURL));
+		KnowledgeSource schemaKS = new OWLAPIOntology(schemaOntology);
+		schemaKS.init();
+		
+		// setup the dataset
+		SparqlEndpointKS ks = new SparqlEndpointKS(endpoint, schemaKS);
 		ks.init();
 		
 		// extract sample of the knowledge base
-		ClassBasedSampleGenerator sampleGen = new ClassBasedSampleGenerator(ks.getQueryExecutionFactory());
+		ClassBasedSampleGenerator sampleGen = new ClassBasedSampleGenerator(ks);
 		sampleGen.addAllowedPropertyNamespaces(Sets.newHashSet("http://dbpedia.org/ontology/"));
 		sampleGen.addAllowedObjectNamespaces(Sets.newHashSet("http://dbpedia.org/ontology/", "http://dbpedia.org/resource/"));
-		OWLOntology sample = sampleGen.getSample(cls);
+		OWLOntology sampleOntology = sampleGen.getSample(cls);
+		
+		// add schema axioms to the sample
+		man.addAxioms(sampleOntology, schemaOntology.getLogicalAxioms());
 		
 		// setup knowledge source
-		AbstractKnowledgeSource sampleKS = new OWLAPIOntology(sample);
+		AbstractKnowledgeSource sampleKS = new OWLAPIOntology(sampleOntology);
 		ks.init();
 		
 		// setup reasoner
-		SPARQLReasoner reasoner = new SPARQLReasoner(ks);
-//		ClosedWorldReasoner reasoner = new ClosedWorldReasoner(sampleKS);
+//		SPARQLReasoner reasoner = new SPARQLReasoner(ks);
+		OWLAPIReasoner baseReasoner = new OWLAPIReasoner(sampleKS);
+		baseReasoner.setReasonerImplementation(ReasonerImplementation.HERMIT);
+		baseReasoner.init();
+		ClosedWorldReasoner reasoner = new ClosedWorldReasoner(baseReasoner);
 		reasoner.init();
 		
 		// setup learning problem
