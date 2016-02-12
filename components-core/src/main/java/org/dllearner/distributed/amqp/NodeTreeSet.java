c9109cb379b434e7714415e4b058f08f28d17927
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2016-02-12 11:34:56 +0100

    added special node TreeSet and tests

---
+++ components-core/src/main/java/org/dllearner/distributed/amqp/NodeTreeSet.java
@@ -0,0 +1,333 @@
+package org.dllearner.distributed.amqp;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NavigableSet;
+import java.util.SortedSet;
+import java.util.Spliterator;
+import java.util.TreeSet;
+import java.util.UUID;
+
+import org.apache.commons.lang.NotImplementedException;
+
+public class NodeTreeSet extends TreeSet<OENode> implements Iterable<OENode> {
+
+	private static final long serialVersionUID = 25182147062840258L;
+
+	private HashMap<UUID, OENode> uuid2Node;
+	private HashMap<Double, List<UUID>> score2UUID;
+	private ArrayList<OENode> nodes;
+	private SortedSet<Double> scores;
+	private OEHeuristicRuntime comparator;
+
+	public NodeTreeSet() {
+		this(new OEHeuristicRuntime());
+	}
+
+	public NodeTreeSet(OEHeuristicRuntime comparator) {
+		this.comparator = comparator;
+		uuid2Node = new HashMap<UUID, OENode>();
+		score2UUID = new HashMap<Double, List<UUID>>();
+
+		scores = new TreeSet<Double>(new Comparator<Double>() {
+			@Override
+			public int compare(Double o1, Double o2) {
+				return Double.compare(o1, o2);
+			}
+		});
+		nodes = new ArrayList<OENode>();
+	}
+
+	public NodeTreeSet(NodeTreeSet c) {
+		this(c.comparator);
+		addAll(c);
+	}
+
+	public NodeTreeSet(Collection<OENode> c) {
+		this();
+		addAll(c);
+	}
+
+	@Override
+	public NodeTreeSetIterator iterator() {
+		return new NodeTreeSetIterator();
+	}
+
+	@Override
+	public NodeTreeSetIterator descendingIterator() {
+		return iterator();
+	}
+
+	@Override
+	public NavigableSet<OENode> descendingSet() {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public int size() {
+		return nodes.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return nodes.isEmpty();
+	}
+
+	@Override
+	public boolean contains(Object object) {
+		OENode node = (OENode) object;
+
+		return uuid2Node.containsKey(node.getUUID());
+	}
+
+	@Override
+	public boolean add(OENode node) {
+		// do nothing if node is already in the set
+		if (uuid2Node.containsKey(node.getUUID())) return false;
+
+		// update uid2Node
+		uuid2Node.put(node.getUUID(), node);
+
+		// update score2UUID
+		double score = comparator.getNodeScore(node);
+
+		if (!score2UUID.containsKey(score))
+			score2UUID.put(score, new ArrayList<UUID>());
+
+		score2UUID.get(score).add(node.getUUID());
+
+		// update nodes
+		nodes.add(node);
+
+		// update scores
+		scores.add(score);
+
+		return true;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		OENode node = (OENode) o;
+
+		// do nothing if node is not in the set
+		if (!uuid2Node.containsKey(node.getUUID())) return false;
+
+		boolean success = true;
+		UUID uuid = node.getUUID();
+
+		// update uid2Node
+		uuid2Node.remove(uuid);
+
+		// update score2UUID
+		double score = comparator.getNodeScore(node);
+
+		if (!score2UUID.containsKey(score)) {
+			/* this should only happen if the score changes based on e.g. the
+			 * parent's or children's score. */
+
+			Double formerScore = null;
+			// find score entry for UUID
+			for (Double s : score2UUID.keySet()) {
+				List<UUID> uuids = score2UUID.get(s);
+
+				for (UUID u : uuids) {
+					if (node.getUUID().equals(u)) {
+						formerScore = s;
+						uuids.remove(u);
+						break;
+					}
+				}
+			}
+
+			if (formerScore == null) success = false;
+
+			else {
+				// remove entry for the found score
+				if (score2UUID.get(formerScore).isEmpty()) {
+					score2UUID.remove(formerScore);
+					scores.remove(formerScore);
+				}
+			}
+
+		} else {
+			List<UUID> uuids = score2UUID.get(score);
+			uuids.remove(uuid);
+
+			if (uuids.isEmpty()) {
+				score2UUID.remove(score);
+				scores.remove(score);
+			}
+		}
+
+		// update nodes
+		nodes.remove(node);
+
+		return success;
+	}
+
+	@Override
+	public void clear() {
+		uuid2Node = new HashMap<UUID, OENode>();
+		score2UUID = new HashMap<Double, List<UUID>>();
+		nodes = new ArrayList<OENode>();
+		scores = new TreeSet<Double>();
+	}
+
+	@Override
+	public  boolean addAll(Collection<? extends OENode> c) {
+		boolean success = true;
+
+		for (OENode e : c) {
+			success = success && add(e);
+		}
+		return success;
+	}
+
+	@Override
+	public NavigableSet<OENode> subSet(OENode fromElement, boolean fromInclusive,
+			OENode toElement, boolean toInclusive) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public NavigableSet<OENode> headSet(OENode toElement, boolean inclusive) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public NavigableSet<OENode> tailSet(OENode fromElement, boolean inclusive) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public SortedSet<OENode> subSet(OENode fromElement, OENode toElement) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public SortedSet<OENode> headSet(OENode toElement) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public SortedSet<OENode> tailSet(OENode fromElement) {
+		throw new NotImplementedException();
+	}
+
+	@Override
+	public Comparator<? super OENode> comparator() { return comparator; }
+
+	@Override
+	public OENode first() {
+		OENode first = nodes.get(0);
+
+		for (OENode n : nodes) {
+			//                     __n_<_first__
+			if (comparator.compare(n, first) < 0) first = n;
+		}
+		return first;
+	}
+
+	@Override
+	public OENode last() {
+		OENode last = nodes.get(0);
+
+		for (OENode n : nodes) {
+			//                     __last_<_n__
+			if (comparator.compare(last, n) < 0) last = n;
+		}
+		return last;
+	}
+
+	@Override
+	public OENode lower(OENode e) { throw new NotImplementedException(); }
+
+	@Override
+	public OENode floor(OENode e) { throw new NotImplementedException(); }
+
+	@Override
+	public OENode ceiling(OENode e) { throw new NotImplementedException(); }
+
+	@Override
+	public OENode higher(OENode e) { throw new NotImplementedException(); }
+
+	@Override
+	public OENode pollFirst() { throw new NotImplementedException(); }
+
+	@Override
+	public OENode pollLast() { throw new NotImplementedException(); }
+
+	@Override
+	public Object clone() {
+		NodeTreeSet clone;
+		clone = (NodeTreeSet) super.clone();
+
+		return clone;
+	}
+
+	@Override
+	public Spliterator<OENode> spliterator() {
+		throw new NotImplementedException();
+	}
+
+	public OENode getNode(UUID uuid) {
+		return uuid2Node.get(uuid);
+	}
+
+	// ------------------------------------------------------------------------
+
+	class NodeTreeSetIterator implements Iterator<OENode> {
+		/** points to the *current* index position, so the one that corresponds
+		 * to the UUID that might have been returned already. Thus it has to be
+		 * incremented to get the next UUID. */
+		private int uuidIdx;
+		private Double currScore;
+		private Iterator<Double> scoresIt;
+
+		public NodeTreeSetIterator() {
+			uuidIdx = -1;
+			scoresIt = scores.iterator();
+			currScore = null;
+		}
+
+		@Override
+		public boolean hasNext() {
+			//              evaluation can be skipped if currScope not set, yet
+			boolean isLastUUID = ((currScore == null) ||
+					/* for each score we get from the score iterator we have to
+					 * check whether we are at the end of the UUID list
+					 * already. The current list pos is held in uuidIdx; the
+					 * considered UUID list is score2UUID.get(currScore) */
+					(uuidIdx < score2UUID.get(currScore).size()));
+
+			return !isLastUUID || scoresIt.hasNext();
+		}
+
+		@Override
+		public OENode next() {
+			if (currScore == null) currScore = scoresIt.next();
+
+			List<UUID> uuids = score2UUID.get(currScore);
+			UUID uuid = uuids.get(++uuidIdx);
+
+			if (uuidIdx == (uuids.size()-1)) {
+				// then we just got the last UUID value in the list
+
+				// reset uuid index and current score
+				uuidIdx = -1;
+				currScore = null;
+			}
+
+			return uuid2Node.get(uuid);
+		}
+	}
+
+	public OENode get(UUID uuid) {
+		return uuid2Node.get(uuid);
+	}
+}
+
---
+++ components-core/src/test/java/org/dllearner/distributed/amqp/NodeTreeSetTest.java
@@ -0,0 +1,279 @@
+package org.dllearner.distributed.amqp;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.dllearner.distributed.amqp.NodeTreeSet.NodeTreeSetIterator;
+import org.junit.Before;
+import org.junit.Test;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+
+import uk.ac.manchester.cs.owl.owlapi.OWLClassImpl;
+
+/**
+ * Tests copied from TreeSet tests and adapted to work with NodeTreeSet
+ */
+public class NodeTreeSetTest {
+
+	public static class ReversedOENodeComparator extends OEHeuristicRuntime {
+		private static final long serialVersionUID = 6570683860440308707L;
+
+		public boolean equals(OENode node1, OENode node2) {
+			return Double.compare(node1.getAccuracy(), node2.getAccuracy()) == 0;
+		}
+
+		@Override
+		public int compare(OENode node1, OENode node2) {
+			return -(Double.compare(node1.getAccuracy(), node2.getAccuracy()));
+		}
+	}
+
+	NodeTreeSet nts;
+	OENode nodeArray[] = new OENode[1000];
+	String prefix = "http://dl-learner.org/ont/";
+
+	@Test
+	public void test_Constructor() {
+		assertTrue("Did not construct correct NodeTreeSet", new NodeTreeSet().isEmpty());
+	}
+
+	@Test
+	public void test_ConstructorLjava_util_Collection() {
+		NodeTreeSet myNodeTreeSet = new NodeTreeSet(Arrays.asList(nodeArray));
+
+		assertTrue("NodeTreeSet incorrect size",
+				myNodeTreeSet.size() == nodeArray.length);
+
+		for (int counter = 0; counter < nodeArray.length; counter++)
+			assertTrue("NodeTreeSet does not contain correct elements", myNodeTreeSet
+					.contains(nodeArray[counter]));
+	}
+
+	@Test
+	public void test_ConstructorLjava_util_Comparator() {
+		NodeTreeSet myNodeTreeSet = new NodeTreeSet(new ReversedOENodeComparator());
+
+		assertTrue("Did not construct correct NodeTreeSet", myNodeTreeSet.isEmpty());
+
+		OENode node1 = new OENode(cls(prefix + "XXX23"), 0.234);
+		OENode node2 = new OENode(cls(prefix + "XXX23"), 0.567);
+		OENode node3 = new OENode(cls(prefix + "XXX42"), 0.890);
+		myNodeTreeSet.add(node1);
+		myNodeTreeSet.add(node2);
+		myNodeTreeSet.add(node3);
+
+		assertTrue(
+				"Answered incorrect first element--did not use custom comparator ",
+				myNodeTreeSet.first().equals(node3));
+
+		assertTrue(
+				"Answered incorrect last element--did not use custom comparator ",
+				myNodeTreeSet.last().equals(node1));
+	}
+
+	@Test
+	public void test_ConstructorLjava_util_SortedSet() {
+		ReversedOENodeComparator comp = new ReversedOENodeComparator();
+		NodeTreeSet myNodeTreeSet = new NodeTreeSet(comp);
+
+		for (int i = 0; i < nodeArray.length; i++)
+			myNodeTreeSet.add(nodeArray[i]);
+
+		NodeTreeSet anotherTreeSet = new NodeTreeSet(myNodeTreeSet);
+
+		assertTrue("NodeTreeSet is not correct size",
+				anotherTreeSet.size() == nodeArray.length);
+
+		for (int counter = 0; counter < nodeArray.length; counter++)
+			assertTrue("NodeTreeSet does not contain correct elements",
+					anotherTreeSet.contains(nodeArray[counter]));
+
+		assertTrue("NodeTreeSet does not answer correct comparator", anotherTreeSet
+				.comparator() == comp);
+		assertTrue("NodeTreeSet does not use comparator",
+				anotherTreeSet.first() == nodeArray[nodeArray.length - 1]);
+	}
+
+	@Test
+	public void test_addLjava_lang_Object() {
+		OENode node = new OENode(cls("ClsSmalles"), 0.);
+		nts.add(node);
+		assertTrue("Failed to add Object", nts.contains(node));
+
+		nts.add(nodeArray[0]);
+		assertTrue("Added existing element", nts.size() == nodeArray.length + 1);
+	}
+
+	@Test
+	public void test_addAllLjava_util_Collection() {
+		NodeTreeSet s = new NodeTreeSet();
+		s.addAll(nts);
+
+		assertTrue("Incorrect size after add", s.size() == nts.size());
+
+		NodeTreeSetIterator i = nts.iterator();
+		while (i.hasNext())
+			assertTrue("Returned incorrect set", s.contains(i.next()));
+	}
+
+	@Test
+	public void test_clear() {
+		nts.clear();
+		assertEquals("Returned non-zero size after clear", 0, nts.size());
+		assertTrue("Found element in cleared set", !nts.contains(nodeArray[0]));
+	}
+
+	@Test
+	public void test_clone() {
+		NodeTreeSet s = (NodeTreeSet) nts.clone();
+		NodeTreeSetIterator i = nts.iterator();
+		while (i.hasNext())
+			assertTrue("Clone failed to copy all elements", s
+					.contains(i.next()));
+	}
+
+	@Test
+	public void test_comparator() {
+		ReversedOENodeComparator comp = new ReversedOENodeComparator();
+		NodeTreeSet myNodeTreeSet = new NodeTreeSet(comp);
+		assertTrue("Answered incorrect comparator",
+				myNodeTreeSet.comparator() == comp);
+	}
+
+	@Test
+	public void test_containsLjava_lang_Object() {
+		assertTrue("Returned false for valid Object", nts
+				.contains(nodeArray[nodeArray.length / 2]));
+		assertTrue("Returned true for invalid Object", !nts
+				.contains(new OENode(cls("SomeCls"), .234)));
+		try {
+			nts.contains(new Object());
+		} catch (ClassCastException e) {
+			// Correct
+			return;
+		}
+		fail("Failed to throw exception when passed invalid element");
+	}
+
+	@Test
+	public void test_first() {
+		assertTrue("Returned incorrect first element",
+				nts.first() == nodeArray[0]);
+	}
+
+	// TODO: implement
+//	public void test_headSetLjava_lang_Object() {
+//		// Test for method java.util.SortedSet
+//		// java.util.TreeSet.headSet(java.lang.Object)
+//		Set s = nts.headSet(new Integer(100));
+//		assertEquals("Returned set of incorrect size", 100, s.size());
+//		for (int i = 0; i < 100; i++)
+//			assertTrue("Returned incorrect set", s.contains(nodeArray[i]));
+//	}
+
+	@Test
+	public void test_isEmpty() {
+		assertTrue("Empty set returned false", new NodeTreeSet().isEmpty());
+		assertTrue("Non-Empty returned true", !nts.isEmpty());
+	}
+
+	@Test
+	public void test_iterator() {
+		NodeTreeSet s = new NodeTreeSet();
+		s.addAll(nts);
+
+		Set<OENode> as = new HashSet<OENode>(Arrays.asList(nodeArray));
+		NodeTreeSetIterator i = nts.iterator();
+
+		while (i.hasNext())
+			as.remove(i.next());
+
+		assertEquals("Returned incorrect iterator", 0, as.size());
+	}
+
+	@Test
+	public void test_last() {
+		assertTrue("Returned incorrect last element",
+				nts.last() == nodeArray[nodeArray.length - 1]);
+	}
+
+	@Test
+	public void test_removeLjava_lang_Object() {
+		nts.remove(nodeArray[0]);
+		assertTrue("Failed to remove object", !nts.contains(nodeArray[0]));
+
+		assertTrue("Failed to change size after remove",
+				nts.size() == nodeArray.length - 1);
+
+		try {
+			nts.remove(new Object());
+		} catch (ClassCastException e) {
+			// Correct
+			return;
+		}
+		fail("Failed to throw exception when past uncomparable value");
+	}
+
+	@Test
+	public void test_size() {
+		assertTrue("Returned incorrect size", nts.size() == nodeArray.length);
+	}
+
+	// TODO: implement
+//	public void test_subSetLjava_lang_ObjectLjava_lang_Object() {
+//		// Test for method java.util.SortedSet
+//		// java.util.TreeSet.subSet(java.lang.Object, java.lang.Object)
+//		final int startPos = nodeArray.length / 4;
+//		final int endPos = 3 * nodeArray.length / 4;
+//		SortedSet aSubSet = nts.subSet(nodeArray[startPos], nodeArray[endPos]);
+//		assertTrue("Subset has wrong number of elements",
+//				aSubSet.size() == (endPos - startPos));
+//		for (int counter = startPos; counter < endPos; counter++)
+//			assertTrue("Subset does not contain all the elements it should",
+//					aSubSet.contains(nodeArray[counter]));
+//
+//		int result;
+//		try {
+//			nts.subSet(nodeArray[3], nodeArray[0]);
+//			result = 0;
+//		} catch (IllegalArgumentException e) {
+//			result = 1;
+//		}
+//		assertEquals("end less than start should throw", 1, result);
+//	}
+
+	// TODO: implement
+//	public void test_tailSetLjava_lang_Object() {
+//		// Test for method java.util.SortedSet
+//		// java.util.TreeSet.tailSet(java.lang.Object)
+//		Set s = nts.tailSet(new Integer(900));
+//		assertEquals("Returned set of incorrect size", 100, s.size());
+//		for (int i = 900; i < nodeArray.length; i++)
+//			assertTrue("Returned incorrect set", s.contains(nodeArray[i]));
+//	}
+
+	@Before
+	public void setUp() {
+		nts = new NodeTreeSet();
+
+		double accuracyStep = 1. / nodeArray.length;
+		double accuracy = 0;
+
+		for (int i = 0; i < nodeArray.length; i++) {
+			accuracy += accuracyStep;
+			OENode n = new OENode(cls("Cls" + i), accuracy);
+			nodeArray[i] = n;
+			nts.add(n);
+		}
+	}
+
+	private OWLClass cls(String localPart) {
+		return new OWLClassImpl(IRI.create(prefix + localPart));
+	}
+}
