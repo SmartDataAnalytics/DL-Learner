14348e5c23c752fc9e30c88dba60b445fc421877
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-08-05 10:31:56 +0200

    Initial implementation of disjunctive meta algorithm.

---
+++ components-core/src/test/java/org/dllearner/algorithms/meta/DisjunctiveCELATest.java
@@ -0,0 +1,16 @@
+package org.dllearner.algorithms.meta;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Lorenz Buehmann
+ */
+public class DisjunctiveCELATest {
+    @Test
+    public void start() throws Exception {
+
+    }
+
+}
