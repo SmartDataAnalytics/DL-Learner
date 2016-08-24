28544708b2923cc92a496f2cfc140e9fab315660
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-08-24 09:27:51 +0200

    QALD dataset filters.

---
+++ components-core/src/test/java/org/dllearner/algorithms/qtl/qald/QALDPredicates.java
@@ -0,0 +1,29 @@
+package org.dllearner.algorithms.qtl.qald;
+
+import org.apache.jena.query.QueryFactory;
+import org.dllearner.algorithms.qtl.qald.schema.Question;
+import org.dllearner.utilities.QueryUtils;
+
+import java.util.function.Predicate;
+
+public class QALDPredicates {
+
+    private static QueryUtils utils = new QueryUtils();
+
+    public static Predicate<Question> isUnion() {
+        return q -> q.getQuery().getSparql().toLowerCase().contains(" union");
+    }
+
+    public static Predicate<Question> hasFilter() {
+        return q -> q.getQuery().getSparql().toLowerCase().contains(" filter");
+    }
+
+    public static Predicate<Question> isSubjectTarget() {
+        return q -> utils.extractIncomingTriplePatterns(
+                        QueryFactory.create(q.getQuery().getSparql()),
+                        QueryFactory.create(q.getQuery().getSparql()).getProjectVars().get(0).asNode()
+                    ).isEmpty();
+
+    }
+
+}
