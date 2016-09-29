217e45ce3a74b1aab0ffb2a1ece930479b60734e
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2016-09-29 13:04:04 +0200

    Fixed compiler error.

--- interfaces/src/main/java/org/dllearner/cli/Enrichment.java
+++ interfaces/src/main/java/org/dllearner/cli/Enrichment.java
@@ -417,7 +417,7 @@ public class Enrichment {
 		// use own implementation of negative example finder
 		System.out.print("finding negatives ... ");
 		startTime = System.currentTimeMillis();
-		AutomaticNegativeExampleFinderSPARQL2 finder = new AutomaticNegativeExampleFinderSPARQL2(ks.getEndpoint(), reasoner);
+		AutomaticNegativeExampleFinderSPARQL2 finder = new AutomaticNegativeExampleFinderSPARQL2(reasoner);
 		SortedSet<OWLIndividual> negExamples = finder.getNegativeExamples(nc, posExamples, maxNrOfNegativeExamples);
 		SortedSetTuple<OWLIndividual> examples = new SortedSetTuple<>(posExamples, negExamples);
 		runTime = System.currentTimeMillis() - startTime;
