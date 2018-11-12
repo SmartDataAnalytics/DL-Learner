15bc422a2b71068d374569b78b531c481d6c47f0
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2018-11-12 19:33:45 +0100

    Added new test scenario for spatial learning

--- components-ext/src/test/java/org/dllearner/reasoning/spatial/SpatialLearningTestScenario.java
+++ components-ext/src/test/java/org/dllearner/reasoning/spatial/SpatialLearningTestScenario.java
@@ -62,7 +62,61 @@ public class SpatialLearningTestScenario {
         alg.init();
         alg.start();
     }
+
+    /**
+     * Target class expression: Feature and (isNear some AreaFeature)
+     */
+    private static void runScenario2() throws ComponentInitException {
+        String exampleFilePath =
+                SpatialLearningTestScenario.class.getClassLoader()
+                        .getResource("example_data.owl").getFile();
+        KnowledgeSource ks = new OWLFile(exampleFilePath);
+        ClosedWorldReasoner cwr = new ClosedWorldReasoner(ks);
+        SpatialReasonerPostGIS spatialReasoner = new SpatialReasonerPostGIS(
+                cwr, new DBConnectionSetting(
+                "localhost",5432, "dllearner",
+                "postgres", "postgres"));
+        spatialReasoner.addGeometryPropertyPath(Lists.newArrayList(
+                new OWLObjectPropertyImpl(IRI.create(
+                        "http://www.opengis.net/ont/geosparql#hasGeometry")),
+                new OWLDataPropertyImpl(IRI.create(
+                        "http://www.opengis.net/ont/geosparql#asWKT"))
+        ));
+        spatialReasoner.init();
+        spatialReasoner.setNearRadiusInMeters(85);
+//        spatialReasoner.setIsContainmentRelationReflexive(true);
+
+        SpatialRhoDRDown refinementOperator = new SpatialRhoDRDown();
+        refinementOperator.setReasoner((SpatialReasoner) spatialReasoner);
+        refinementOperator.init();
+
+        PosNegLPStandard lp = new PosNegLPStandard();
+        lp.setPositiveExamples(Sets.newHashSet(
+                new OWLNamedIndividualImpl(IRI.create(
+                        "http://dl-learner.org/ont/spatial#pos_inside_bhf_neustadt")),
+                new OWLNamedIndividualImpl(IRI.create(
+                        "http://dl-learner.org/ont/spatial#pos_outside_bhf_neustadt_1"))));
+
+        lp.setNegativeExamples(Sets.newHashSet(
+                new OWLNamedIndividualImpl(IRI.create(
+                        "http://dl-learner.org/ont/spatial#pos_on_turnerweg")),
+                new OWLNamedIndividualImpl(IRI.create(
+                        "http://dl-learner.org/ont/spatial#turnerweg"
+                ))));
+        lp.setReasoner(spatialReasoner);
+        lp.init();
+
+        SpatialLearningAlgorithm alg = new SpatialLearningAlgorithm();
+        alg.setLearningProblem(lp);
+        alg.setOperator(refinementOperator);
+        alg.setMaxExecutionTimeInSeconds(20);
+        alg.setExpandAccuracy100Nodes(true);
+        alg.init();
+        alg.start();
+    }
+
     public static void main(String[] args) throws ComponentInitException {
-        runScenario1();
+//        runScenario1();
+        runScenario2();
     }
 }
