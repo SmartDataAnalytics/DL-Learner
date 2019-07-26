3fca7eacf3db138b3061d2350395b9a735138a7e
Lorenz Buehmann <buehmann@informatik.uni-leipzig.de>
2019-07-26 13:49:52 +0200

    Started generic ML structures

---
+++ components-core/src/main/java/org/dllearner/core/Example.java
@@ -0,0 +1,9 @@
+package org.dllearner.core;
+
+/**
+ * Marker interface for examples in ILP setting.
+ *
+ * @author Lorenz Buehmann
+ */
+public interface Example {
+}
---
+++ components-core/src/main/java/org/dllearner/core/SequentialCovering.java
@@ -0,0 +1,47 @@
+package org.dllearner.core;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import com.google.common.collect.Sets;
+
+/**
+ * @author Lorenz Buehmann
+ */
+public abstract class SequentialCovering<E extends Example, H extends EvaluatedHypothesis> {
+
+    private final Set<E> examples;
+
+    public SequentialCovering(Set<E> examples) {
+        this.examples = examples;
+    }
+
+    public abstract H learnOneHypothesis(Set<E> examples);
+
+    public abstract boolean goodPerformance(H hypothesis);
+
+    public abstract Set<E> coveredExamples(H hypothesis);
+
+    public void run() {
+        Set<H> learnedHypotheses = new TreeSet<>();
+
+        Set<E> currentExamples = Sets.newHashSet(examples);
+
+        // learn a single hypothesis
+        H hypothesis = learnOneHypothesis(currentExamples);
+
+
+        while(goodPerformance(hypothesis)) {
+            learnedHypotheses.add(hypothesis);
+
+            // removed covered examples from examples
+            currentExamples = Sets.difference(currentExamples, coveredExamples(hypothesis));
+
+            // learn another hypothesis on remaining examples
+            hypothesis = learnOneHypothesis(currentExamples);
+        }
+    }
+
+
+
+}
