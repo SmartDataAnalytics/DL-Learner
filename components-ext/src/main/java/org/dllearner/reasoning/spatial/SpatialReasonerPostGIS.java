e6c508fa7e91efd59d74bb72d0aecc44c08f751d
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2020-05-03 15:03:49 +0200

    Implemented getPartiallyOverlapsWithMembers
    ...in SpatialReasonerPostGIS

--- components-ext/src/main/java/org/dllearner/reasoning/spatial/SpatialReasonerPostGIS.java
+++ components-ext/src/main/java/org/dllearner/reasoning/spatial/SpatialReasonerPostGIS.java
@@ -662,7 +662,9 @@ public class SpatialReasonerPostGIS extends AbstractReasonerComponent implements
             } else if (objectProperty.equals(SpatialVocabulary.hasProperPart)) {
                 return getHasProperPartMembers();
 
-            // TODO: partiallyOverlapsWith
+            // partiallyOverlapsWith
+            } else if (objectProperty.equals(SpatialVocabulary.partiallyOverlapsWith)) {
+                return getPartiallyOverlapsWithMembers();
             // TODO: isTangentialProperPartOf
             // TODO: isNonTangentialProperPartOf
             // TODO: isSpatiallyIdenticalWith
@@ -2657,6 +2659,80 @@ public class SpatialReasonerPostGIS extends AbstractReasonerComponent implements
         } catch (SQLException | ExecutionException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    protected Map<OWLIndividual, SortedSet<OWLIndividual>> getPartiallyOverlapsWithMembers() {
+        String queryStr =
+                "SELECT " +
+                    "l.iri l_iri, " +
+                    "r.iri r_iri " +
+                "FROM " +
+                    lineFeatureTableName + " l, " +
+                    lineFeatureTableName + " r " +
+                "WHERE " +
+                    "ST_Overlaps(l.the_geom, r.the_geom) " +
+                "UNION " +
+                "SELECT " +
+                    "l.iri l_iri, " +
+                    "r.iri r_iri " +
+                "FROM " +
+                    lineFeatureTableName + " l, " +
+                    areaFeatureTableName + " r " +
+                "WHERE " +
+                    "ST_Overlaps(l.the_geom, ST_Boundary(r.the_geom)) " +
+                "OR " +
+                    "(" +
+                        "ST_Intersects(l.the_geom, r.the_geom) " +
+                    "AND " +
+                        "NOT ST_Contains(r.the_geom, l.the_geom)" +
+                    ") " +
+                "UNION " +
+                "SELECT " +
+                    "l.iri l_iri, " +
+                    "r.iri r_iri " +
+                "FROM " +
+                    areaFeatureTableName + " l, " +
+                    areaFeatureTableName + " r " +
+                "WHERE " +
+                    "ST_Overlaps(l.the_geom, r.the_geom) ";
+
+        Map<OWLIndividual, SortedSet<OWLIndividual>> members = new HashMap<>();
+
+        try {
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(queryStr);
+
+            while (resultSet.next()) {
+                String geomIRI1 = resultSet.getString("l_iri");
+                String geomIRI2 = resultSet.getString("r_iri");
+
+                OWLIndividual geomIndividual1 =
+                        new OWLNamedIndividualImpl(IRI.create(geomIRI1));
+                OWLIndividual geomIndividual2 =
+                        new OWLNamedIndividualImpl(IRI.create(geomIRI2));
+
+                // convert geometries to features
+                OWLIndividual featureIndividual1 =
+                        geom2feature.get(geomIndividual1);
+                OWLIndividual featureIndividual2 =
+                        geom2feature.get(geomIndividual2);
+
+                if (!members.containsKey(featureIndividual1)) {
+                    members.put(featureIndividual1, new TreeSet<>());
+                }
+                members.get(featureIndividual1).add(featureIndividual2);
+
+                if (!members.containsKey(featureIndividual2)) {
+                    members.put(featureIndividual2, new TreeSet<>());
+                }
+                members.get(featureIndividual2).add(featureIndividual1);
+            }
+
+        } catch (SQLException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        return members;
     }
 
     @Override
