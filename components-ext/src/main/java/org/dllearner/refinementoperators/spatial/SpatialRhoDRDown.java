77d272df8d9ea28d4927f2dabb331c3eb8abc486
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2020-05-05 18:44:36 +0200

    Added endsNear handling to spatial ref.op

--- components-ext/src/main/java/org/dllearner/refinementoperators/spatial/SpatialRhoDRDown.java
+++ components-ext/src/main/java/org/dllearner/refinementoperators/spatial/SpatialRhoDRDown.java
@@ -327,7 +327,21 @@ public class SpatialRhoDRDown extends RhoDRDown {
                                     SpatialVocabulary.startsNear,
                                     SpatialVocabulary.SpatialFeature))));
 
-            // TODO: endsNear
+            // endsNear -------------------------------------------------------
+            refinements.add(
+                    new OWLObjectIntersectionOfImpl(Sets.newHashSet(
+                            cls,
+                            new OWLObjectSomeValuesFromImpl(
+                                    SpatialVocabulary.endsNear,
+                                    SpatialVocabulary.SpatialFeature))));
+
+            refinements.add(
+                    new OWLObjectIntersectionOfImpl(Sets.newHashSet(
+                            cls,
+                            new OWLObjectAllValuesFromImpl(
+                                    SpatialVocabulary.endsNear,
+                                    SpatialVocabulary.SpatialFeature))));
+
             // TODO: crosses
             // TODO: runsAlong
         }
@@ -533,7 +547,19 @@ public class SpatialRhoDRDown extends RhoDRDown {
                             SpatialVocabulary.startsNear,
                             SpatialVocabulary.SpatialFeature))));
 
-            // TODO: endsNear
+            // endsNear -------------------------------------------------------
+            refinements.add(new OWLObjectIntersectionOfImpl(Sets.newHashSet(
+                    intersection,
+                    new OWLObjectSomeValuesFromImpl(
+                            SpatialVocabulary.endsNear,
+                            SpatialVocabulary.SpatialFeature))));
+
+            refinements.add(new OWLObjectIntersectionOfImpl(Sets.newHashSet(
+                    intersection,
+                    new OWLObjectAllValuesFromImpl(
+                            SpatialVocabulary.endsNear,
+                            SpatialVocabulary.SpatialFeature))));
+
             // TODO: crosses
             // TODO: runsAlong
         }
@@ -831,7 +857,19 @@ public class SpatialRhoDRDown extends RhoDRDown {
                             SpatialVocabulary.startsNear,
                             SpatialVocabulary.SpatialFeature))));
 
-            // TODO: endsNear
+            // endsNear -------------------------------------------------------
+            refinements.add(new OWLObjectIntersectionOfImpl(Sets.newHashSet(
+                    new OWLObjectUnionOfImplExt(unionOperands),
+                    new OWLObjectSomeValuesFromImpl(
+                            SpatialVocabulary.endsNear,
+                            SpatialVocabulary.SpatialFeature))));
+
+            refinements.add(new OWLObjectIntersectionOfImpl(Sets.newHashSet(
+                    new OWLObjectUnionOfImplExt(unionOperands),
+                    new OWLObjectAllValuesFromImpl(
+                            SpatialVocabulary.endsNear,
+                            SpatialVocabulary.SpatialFeature))));
+
             // TODO: crosses
             // TODO: runsAlong
         }
