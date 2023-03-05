1646ade2ee0d1d312e6e9ad9e13eacf522be762d
Tomáš Bisták <tom.bistak@gmail.com>
2023-03-05 11:55:34 +0100

    useAllConstructor, useExistsConstructor, applyAllFilter, applyExistsFilter ignored when refining upwards

--- components-core/src/main/java/org/dllearner/refinementoperators/RhoDRDownConfigOptionsMemento.java
+++ components-core/src/main/java/org/dllearner/refinementoperators/RhoDRDownConfigOptionsMemento.java
@@ -1,18 +1,6 @@
 package org.dllearner.refinementoperators;
 
-import org.dllearner.utilities.owl.OWLClassExpressionLengthMetric;
-import org.semanticweb.owlapi.model.*;
-
-import java.util.*;
-
-// does not store the reasoner used by the originator
 public class RhoDRDownConfigOptionsMemento {
-
-    private final boolean applyAllFilter;
-    private final boolean applyExistsFilter;
-
-    private final boolean useAllConstructor;
-    private final boolean useExistsConstructor;
 
     private final boolean useNegation;
     private final boolean useDisjunction;
@@ -22,12 +10,6 @@ public class RhoDRDownConfigOptionsMemento {
     private final int maxCardinalityLimit;
 
     public RhoDRDownConfigOptionsMemento(RhoDRDown originator) {
-        applyAllFilter = originator.isApplyAllFilter();
-        applyExistsFilter = originator.isApplyExistsFilter();
-
-        useAllConstructor = originator.isUseAllConstructor();
-        useExistsConstructor = originator.isUseExistsConstructor();
-
         useNegation = originator.isUseNegation();
         useDisjunction = originator.isUseDisjunction();
         useSomeOnly = originator.isUseSomeOnly();
@@ -37,12 +19,6 @@ public class RhoDRDownConfigOptionsMemento {
     }
 
     public void restore(RhoDRDown originator) {
-        originator.setApplyAllFilter(applyAllFilter);
-        originator.setApplyExistsFilter(applyExistsFilter);
-
-        originator.setUseAllConstructor(useAllConstructor);
-        originator.setUseExistsConstructor(useExistsConstructor);
-
         originator.setUseNegation(useNegation);
         originator.setUseDisjunction(useDisjunction);
         originator.setUseSomeOnly(useSomeOnly);
